cf63c9dad23631a190e96cd270e38406a238a088
Eric Schultz <eric@wwahammy.com>
2018-06-05 16:06:47 -0500

    Remove commitchange specific postgres restore script

--- script/pg_restore_local_from_production.sh
+++ script/pg_restore_local_from_production.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -o latest.dump `heroku pg:backups public-url -a commitchange`
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -U admin -d commitchange_development latest.dump
+pg_restore --verbose --clean --no-acl --no-owner -h db -U admin -d commitchange_development latest.dump

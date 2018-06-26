ef9551fbf9cc0493ae9c76bd6f7a8855ccfc06d5
Eric Schultz <eric@commitchange.com>
2018-06-26 16:03:24 -0500

    Set up staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,6 +4,7 @@ Commitchange::Application.configure do
 
 	# Code is not reloaded between requests
 	config.cache_classes = true
+	config.cache_store = Settings.default.cache_store.to_sym
 
 	# Full error reports are disabled and caching is turned on
 	config.consider_all_requests_local = false
@@ -17,19 +18,20 @@ Commitchange::Application.configure do
 
 	# Generate digests for assets URLs
 	config.assets.digest = true
+	config.assets.compile = false
 
 	# Defaults to nil and saved in location specified by config.assets.prefix
 	# config.assets.manifest = YOUR_PATH
 
 	# Specifies the header that your server uses for sending files
 	# config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-	# config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+	config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
 	# Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
 	config.force_ssl = true
 
-  # See everything in the log (default is :info)
-	config.log_level = :info
+	# See everything in the log (default is :info)
+	config.log_level = :debug
 
 	# Prepend all log lines with the following tags
 	# config.log_tags = [ :subdomain, :uuid ]
@@ -41,18 +43,29 @@ Commitchange::Application.configure do
 	# config.cache_store = :mem_cache_store
 
 	# Enable serving of images, stylesheets, and JavaScripts from an asset server
-	# cdn_url = "https://d2e5we1j08b82a.cloudfront.net"
-	# config.action_controller.asset_host = cdn_url
-	# config.action_mailer.asset_host = cdn_url
+
+	cdn_url= URI(Settings.cdn.url)
+	cdn_url.port = Settings.cdn.port if Settings.cdn.port
+	cdn_url = cdn_url.to_s
+	config.action_controller.asset_host = cdn_url
+	config.action_mailer.asset_host = cdn_url
 	config.font_assets.origin = '*'
 
 	# Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-	# config.assets.precompile = ['application', 'manifests/*']
 
 	# Disable delivery errors, bad email addresses will be ignored
 	# config.action_mailer.raise_delivery_errors = false
 	config.action_mailer.delivery_method = Settings.mailer.delivery_method.to_sym
 	config.action_mailer.default_url_options = { host: Settings.mailer.host }
+	# Precompile all "page" files, it needs to be set here so the proper env is setup
+	config.assets.precompile << Proc.new do |path|
+		if path =~ /.*page\.(css|js)/
+			puts "Compiling asset: " + path
+			true
+		else
+			false
+		end
+	end
 
 	# Enable threaded mode
 	# config.threadsafe!
@@ -68,4 +81,8 @@ Commitchange::Application.configure do
 	# with SQLite, MySQL, and PostgreSQL)
 	# config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+	config.assets.compile = false
+
+	# Compress json
+	# config.middleware.use Rack::Deflater
 end

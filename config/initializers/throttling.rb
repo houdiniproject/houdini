119245580edc0d3aec45462833995dbd5c4648b6
Eric Schultz <eric@commitchange.com>
2018-08-02 14:45:12 -0500

    Make the throttling safer on cards

--- config/initializers/throttling.rb
+++ config/initializers/throttling.rb
@@ -4,7 +4,7 @@ def run_throttle?
   Rails.env != 'test' || (defined? FORCE_THROTTLE && FORCE_THROTTLE)
 end
 
-Rack::Attack.throttle('post to add card by supporter', limit:3, period: 60) do |req|
+Rack::Attack.throttle('post to add card by supporter', limit:4, period: 60) do |req|
   ret = nil
   if run_throttle? && req.path == '/cards' && req.post?
     begin

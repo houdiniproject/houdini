f1efaaa8b4307a289cae14b04217899011662a33
Eric Schultz <eric@wwahammy.com>
2018-08-21 14:31:48 -0500

    WIP

--- app/api/houdini/v1/nonprofit.rb
+++ app/api/houdini/v1/nonprofit.rb
@@ -1,10 +1,10 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 class Houdini::V1::Nonprofit < Houdini::V1::BaseAPI
-   helpers Houdini::V1::Helpers::ApplicationHelper, Houdini::V1::Helpers::RescueHelper
+  helpers Houdini::V1::Helpers::ApplicationHelper, Houdini::V1::Helpers::RescueHelper
 
-   before do
-     protect_against_forgery
-   end
+  before do
+    protect_against_forgery
+  end
 
   desc 'Return a nonprofit.' do
     success Houdini::V1::Entities::Nonprofit
@@ -18,74 +18,52 @@ class Houdini::V1::Nonprofit < Houdini::V1::BaseAPI
       present np, as: Houdini::V1::Entities::Nonprofit
     end
   end
-   
+
   desc 'Register a nonprofit' do
     success Houdini::V1::Entities::Nonprofit
 
     #this needs to be a validation an array
-    failure [{code:400, message:'Validation Errors',  model: Houdini::V1::Entities::ValidationErrors},
-            {code:401, message: 'Not authorized or authenticated'}]
+    failure [{code: 400, message: 'Validation Errors', model: Houdini::V1::Entities::ValidationErrors},
+             {code: 401, message: 'Not authorized or authenticated'}]
   end
 
   params do
 
-    requires :nonprofit, type: Hash  do
-      requires :name, type:String, desc: 'Organization Name', allow_blank: false, documentation: { param_type: 'body' }
-      optional :website, type:String, desc: 'Organization website URL', allow_blank:true, regexp: URI::regexp, documentation: { param_type: 'body' }, coerce_with: ->(url) {
+    requires :nonprofit, type: Hash do
+      requires :name, type: String, desc: 'Organization Name', allow_blank: false, documentation: {param_type: 'body'}
+      optional :website, type: String, desc: 'Organization website URL', allow_blank: true, regexp: URI::regexp, documentation: {param_type: 'body'}, coerce_with: ->(url) {
         coerced_url = url
         unless (url =~ /\Ahttp:\/\/.*/i || url =~ /\Ahttps:\/\/.*/i)
-          coerced_url = 'http://'+ coerced_url
+          coerced_url = 'http://' + coerced_url
         end
         coerced_url
       }
-      requires :zip_code, type:String, allow_blank: false, desc: "Organization Address ZIP Code", documentation: { param_type: 'body' }
-      requires :state_code, type:String, allow_blank: false, desc: "Organization Address State Code", documentation: { param_type: 'body' }
-      requires :city, type:String, allow_blank: false, desc: "Organization Address City", documentation: { param_type: 'body' }
-      optional :email, type:String, desc: 'Organization email (public)', regexp: Email::Regex, documentation: { param_type: 'body' }
-      optional :phone, type:String, desc: 'Organization phone (public)', documentation: { param_type: 'body' }
+      requires :zip_code, type: String, allow_blank: false, desc: "Organization Address ZIP Code", documentation: {param_type: 'body'}
+      requires :state_code, type: String, allow_blank: false, desc: "Organization Address State Code", documentation: {param_type: 'body'}
+      requires :city, type: String, allow_blank: false, desc: "Organization Address City", documentation: {param_type: 'body'}
+      optional :email, type: String, desc: 'Organization email (public)', regexp: Email::Regex, documentation: {param_type: 'body'}
+      optional :phone, type: String, desc: 'Organization phone (public)', documentation: {param_type: 'body'}
     end
 
     requires :user, type: Hash do
-      requires :name, type:String, desc: 'Full name', allow_blank:false, documentation: { param_type: 'body' }
-      requires :email, type:String, desc: 'Username', allow_blank: false, documentation: { param_type: 'body' }
-      requires :password, type:String, desc: 'Password', allow_blank: false, is_equal_to: :password_confirmation, documentation: { param_type: 'body' }
-      requires :password_confirmation, type:String, desc: 'Password confirmation', allow_blank: false, documentation: { param_type: 'body' }
+      requires :name, type: String, desc: 'Full name', allow_blank: false, documentation: {param_type: 'body'}
+      requires :email, type: String, desc: 'Username', allow_blank: false, documentation: {param_type: 'body'}
+      requires :password, type: String, desc: 'Password', allow_blank: false, is_equal_to: :password_confirmation, documentation: {param_type: 'body'}
+      requires :password_confirmation, type: String, desc: 'Password confirmation', allow_blank: false, documentation: {param_type: 'body'}
     end
 
 
   end
   post do
+    if current_user
+      error!('Unauthorized', 401)
+    end
+
     declared_params = declared(params)
     np = nil
     u = nil
     Qx.transaction do
-      begin
-        np = Nonprofit.new(OnboardAccounts.set_nonprofit_defaults(declared_params[:nonprofit]))
-
-        begin
-          np.save!
-        rescue ActiveRecord::RecordInvalid => e
-          if (e.record.errors[:slug])
-            begin
-              slug = SlugNonprofitNamingAlgorithm.new(np.state_code_slug, np.city_slug).create_copy_name(np.slug)
-              np.slug = slug
-              np.save!
-            rescue UnableToCreateNameCopyError
-              raise Grape::Exceptions::ValidationErrors.new(errors:[Grape::Exceptions::Validation.new(
-
-                  params: ["nonprofit[name]"],
-                  message: "has an invalid slug. Contact support for help."
-              )])
-            end
-          else
-            raise e
-          end
-        end
-
-        u = User.new(declared_params[:user])
-        u.save!
-
-      np = Nonprofit.new(OnboardAccounts.set_nonprofit_defaults(params[:nonprofit]))
+      np = Nonprofit.new(OnboardAccounts.set_nonprofit_defaults(declared_params[:nonprofit]))
 
       begin
         np.save!
@@ -96,7 +74,7 @@ class Houdini::V1::Nonprofit < Houdini::V1::BaseAPI
             np.slug = slug
             np.save!
           rescue UnableToCreateNameCopyError
-            raise Grape::Exceptions::ValidationErrors.new(errors:[Grape::Exceptions::Validation.new(
+            raise Grape::Exceptions::ValidationErrors.new(errors: [Grape::Exceptions::Validation.new(
 
                 params: ["nonprofit[name]"],
                 message: "has an invalid slug. Contact support for help."
@@ -107,7 +85,7 @@ class Houdini::V1::Nonprofit < Houdini::V1::BaseAPI
         end
       end
 
-      u = User.new(params[:user])
+      u = User.new(declared_params[:user])
       u.save!
 
       role = u.roles.build(host: np, name: 'nonprofit_admin')
@@ -120,7 +98,6 @@ class Houdini::V1::Nonprofit < Houdini::V1::BaseAPI
     #onboard callback
     present np, with: Houdini::V1::Entities::Nonprofit
   end
-
 
 
 end

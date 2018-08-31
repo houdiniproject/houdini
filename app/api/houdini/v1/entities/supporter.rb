051bf2b514ade150486b4437b39e3d960e9a0292
Eric Schultz <eric@wwahammy.com>
2018-08-31 17:03:44 -0500

    donation_spec works for api

--- app/api/houdini/v1/entities/supporter.rb
+++ app/api/houdini/v1/entities/supporter.rb
@@ -3,3 +3,7 @@ class Houdini::V1::Entities::Supporter < Grape::Entity
   expose :id, documentation: {type: Integer}
   expose :default_address, using: Houdini::V1::Entities::Address, documentation: {type: 'Houdini::V1::Entities::Address'}
 end
+
+class Houdini::V1::Entities::SupporterStub < Grape::Entity
+  expose :id, documentation: {type: Integer}
+end
--- spec/api/houdini/donation_spec.rb
+++ spec/api/houdini/donation_spec.rb
@@ -83,15 +83,22 @@ describe Houdini::V1::Donation, :type => :request do
 
   describe :put do
     it '404s on invalid address' do
-      xhr :put, '/api/v1/donation/410595'
+      input = {donation: {address:{address: "heothwohtw"}}}
+      xhr :put, '/api/v1/donation/410595', input
       expect(response.code).to eq "404"
     end
 
     describe 'authorize properly' do
 
       it '401s properly' do
+        nonprofit.miscellaneous_np_info = MiscellaneousNpInfo.new(supporter_default_address_strategy: :manual)
+        nonprofit.miscellaneous_np_info.save!
+        nonprofit.save!
+        #nonprofit.miscellaneous_np_info.supporter_default_address_strategy = :manual
         run_authorization_tests({method: :put, action: "/api/v1/donation/#{donation.id}",
-                                 successful_users:  roles__open_to_np_associate})
+                                 successful_users:  roles__open_to_np_associate}) do |u|
+          {donation: {address:{address: "heothwohtw"}}}
+        end
 
       end
 
@@ -160,22 +167,128 @@ describe Houdini::V1::Donation, :type => :request do
 
 
     describe 'update donations' do
+
+      before(:each) { nonprofit.miscellaneous_np_info = MiscellaneousNpInfo.new(supporter_default_address_strategy: :manual)
+      nonprofit.miscellaneous_np_info.save!
+      nonprofit.save!
+      sign_in user_as_np_admin
+      }
+      let(:input) do
+        {donation: {address: {
+            address: 'adddress',
+            city: 'cityeee',
+            state_code:"state code",
+            zip_code: "532525",
+            country: 'coutnwet'
+        }}}
+      end
+
+
+      let(:input_address) { input[:donation][:address]}
+
+      def generate_expected()
+          return h({id: donation.id,
+             address:
+                 h({
+                       id: TransactionAddress.last.id,
+                       address: input_address[:address],
+                       city: input_address[:city],
+                       state_code: input_address[:state_code],
+                       zip_code: input_address[:zip_code],
+                       country: input_address[:country],
+                       fingerprint: AddressComparisons.calculate_hash(donation.supporter.id, input_address[:address],
+                                                                      input_address[:city],
+                                                                      input_address[:state_code],
+                                                                      input_address[:zip_code],
+                                                                      input_address[:country]),
+                       type: "TransactionAddress",
+                       name: nil,
+                       supporter: h({id: donation.supporter.id})
+                   })
+            })
+
+
+      end
+
+      def make_call_and_verify_response
+        xhr :put, "/api/v1/donation/#{donation.id}", input, format: :json
+        expect(response.status).to eq 200
+        json_response = JSON.parse(response.body)
+
+
+        expected = generate_expected
+
+
+        expect(json_response).to eq expected
+      end
+
       it 'no address already' do
-        donation.address = transaction_address
+
+        # make sure on add is called
+        expect_any_instance_of(DefaultAddressStrategies::ManualStrategy).to receive(:on_add)
+        make_call_and_verify_response()
+
+      end
+
+
+      describe 'an identical address is in the db' do
+
+        let(:address_matching_input) { TransactionAddress.create!({supporter: donation.supporter}.merge(input_address))}
+
+        let(:pre_input_address) {TransactionAddress.create!({supporter: donation.supporter}.merge(input_address).merge({country: 'ehtwetioh'})) }
+
+        before(:each) do
+          # just some address we're not going to have any more
+          donation.address = pre_input_address
+          donation.address.save!
+          donation.save!
+          address_matching_input
+        end
+
+        it 'but its not used by anything else' do
+
+          expect_any_instance_of(DefaultAddressStrategies::ManualStrategy).to receive(:on_use)
+          expect_any_instance_of(DefaultAddressStrategies::ManualStrategy).to receive(:on_remove).with(donation.supporter, pre_input_address)
+          make_call_and_verify_response()
+
+
+          donation.reload
+          expect(donation.address).to eq address_matching_input
+          expect(Address.where(id: pre_input_address.id).any?).to be_falsey
+        end
+
+        it 'used by another transaction so we dont delete the original address' do
+
+          AddressToTransactionRelation.create!(address: pre_input_address)
+          expect_any_instance_of(DefaultAddressStrategies::ManualStrategy).to receive(:on_use)
+          expect_any_instance_of(DefaultAddressStrategies::ManualStrategy).to_not receive(:on_remove)
+          make_call_and_verify_response()
+
+
+          donation.reload
+          expect(donation.address).to eq address_matching_input
+          expect(Address.where(id: pre_input_address.id).any?).to be_truthy
+        end
+      end
+      it 'address already set exists but identical' do
+
+
+        original_address = TransactionAddress.create!({supporter: donation.supporter}.merge(input_address))
+        donation.address = original_address
+        donation.address.save!
         donation.save!
 
-        # donation: {
-        #     address: {}
-        sign_in user_as_np_admin
-        xhr :put, "/api/v1/donation/#{donation.id}", format: :json
+        # make sure on add is called
+        expect_any_instance_of(DefaultAddressStrategies::ManualStrategy).to receive(:on_use)
+        make_call_and_verify_response()
+
+
+        donation.reload
+        expect(donation.address).to eq original_address
+
       end
     end
 
-    it 'updates donation' do
 
-
-      expect(response.status).to eq 200
-      json_response = JSON.parse(response.body)
-    end
   end
 end

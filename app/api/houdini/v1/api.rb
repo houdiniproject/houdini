faf1d7d84a12206c1ceab528499e88e030f39d35
Eric Schultz <eric@wwahammy.com>
2018-09-06 12:03:42 -0500

    Add api_domain to configuration

--- app/api/houdini/v1/api.rb
+++ app/api/houdini/v1/api.rb
@@ -14,9 +14,9 @@ class Houdini::V1::API < Grape::API
 	mount Houdini::V1::Nonprofit => '/nonprofit'
 	# Additional mounts are added via generators above this line
   # DON'T REMOVE THIS OR THE PREVIOUS LINES!!!
-	uriForHost = URI.parse(Settings.cdn.url)
+	uri_for_host = URI.parse(Settings.api_domain&.url || Settings.cdn.url)
 	add_swagger_documentation \
-		host: "#{uriForHost.host}#{Settings.cdn.port ? ":#{Settings.cdn.port}" : ""}",
-		schemes: [uriForHost.scheme],
+		host: "#{uri_for_host.host}#{uri_for_host.port ? ":#{uri_for_host.port}" : ""}",
+		schemes: [uri_for_host.scheme],
 		base_path: '/api/v1'
 end

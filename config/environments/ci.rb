edb8920f278cb375c3ec30b1cbb1bc48835c1a1b
Eric Schultz <eric@wwahammy.com>
2018-09-07 13:09:20 -0500

    Updatery

---
+++ config/environments/ci.rb
@@ -0,0 +1,8 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+require 'config/environments/test.rb'
+
+Commitchange::Application.configure do
+
+  config.log_level = :info
+
+end

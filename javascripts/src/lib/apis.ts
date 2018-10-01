6f94e3c7248b9596682c21b18fd82aac0f26d9d8
Eric Schultz <eric@wwahammy.com>
2018-10-01 15:31:57 -0500

    Add legacy put donation api

--- javascripts/src/lib/apis.ts
+++ javascripts/src/lib/apis.ts
@@ -1,4 +1,5 @@
 // License: LGPL-3.0-or-later
 import {WebUserSignInOut} from "./api/sign_in";
+import {PutDonation} from './api/put_donation';
 
-export const APIS = [WebUserSignInOut]
+export const APIS = [WebUserSignInOut, PutDonation]

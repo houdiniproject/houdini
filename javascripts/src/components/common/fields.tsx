6607f1e9e7b2532bcf8c0e4538bccc7b2c1d19a3
Eric Schultz <eric@wwahammy.com>
2018-10-09 11:50:51 -0500

    Correct missing inputClassName property

--- javascripts/src/components/common/fields.tsx
+++ javascripts/src/components/common/fields.tsx
@@ -8,13 +8,13 @@ import {HoudiniField} from "../../lib/houdini_form";
 import ReactInput from "./form/ReactInput";
 
 
-export const BasicField = observer((props:{field:Field, placeholder?:string, label?:string, wrapperClassName?:string}) =>{
+export const BasicField = observer((props:{field:Field, placeholder?:string, label?:string, wrapperClassName?:string, inputClassNames?:string}) =>{
     let field = props.field as HoudiniField
     return <LabeledFieldComponent
         inputId={props.field.id} labelText={field.label} inError={field.hasError} error={field.error}
         inStickyError={field.hasServerError} stickyError={field.serverError}
         className={props.wrapperClassName} >
 
-        <ReactInput field={field} label={props.label} placeholder={props.placeholder} className="form-control"/>
+        <ReactInput field={field} label={props.label} placeholder={props.placeholder} className={`form-control ${props.inputClassNames || ''}`}/>
     </LabeledFieldComponent>
 })

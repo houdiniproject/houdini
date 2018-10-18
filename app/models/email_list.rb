ef8159064ee6d8da47dc316480aadff92228cf31
Eric Schultz <eric@commitchange.com>
2018-10-17 20:47:26 -0500

    Fix some bugs for finding list subscribers and setting atributes on EmailList

--- app/models/email_list.rb
+++ app/models/email_list.rb
@@ -1,5 +1,5 @@
 class EmailList < ActiveRecord::Base
-  attr_accessible :list_name, :mailchimp_list_id
+  attr_accessible :list_name, :mailchimp_list_id, :nonprofit, :tag_master
   belongs_to :nonprofit
   belongs_to :tag_master
 end

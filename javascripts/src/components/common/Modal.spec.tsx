5781c55b1be0532f3028b912f544e8296c4d85db
Eric Schultz <eric@commitchange.com>
2018-10-19 12:42:50 -0500

    Add Modal tests

--- javascripts/src/components/common/Modal.spec.tsx
+++ javascripts/src/components/common/Modal.spec.tsx
@@ -1,10 +1,28 @@
 // License: LGPL-3.0-or-later
 import * as React from 'react';
 import 'jest';
-import Modal from './Modal'
+import Modal, {ModalProps} from './Modal'
+import {shallow} from "enzyme";
+import {toJS} from "mobx";
+import toJson from "enzyme-to-json";
 
 describe('Modal', () => {
-  test('your test here', () => {
-    expect(false).toBe(true)
+  test('nothing displayed if inactive', () => {
+    let modal = shallow(<Modal childGenerator={() => <div/>}/>)
+
+    expect(toJson(modal)).toMatchSnapshot()
+  })
+
+  test('active modal displays', () => {
+    let onCloseWasCalled = false
+    let modal = shallow(<Modal titleText={"title text"}
+                               focusDialog={true}
+                               modalActive={true}
+                               onClose={() => { onCloseWasCalled = true}}
+                               childGenerator={() => <div/>}/>)
+    expect(toJson(modal)).toMatchSnapshot()
+    let modalComponent = modal.instance() as React.Component<ModalProps, {}> //casting to modal didn't work for reasons?
+    modalComponent.props.onClose()
+    expect(onCloseWasCalled).toBeTruthy()
   })
 })
--- javascripts/src/components/common/Modal.tsx
+++ javascripts/src/components/common/Modal.tsx
@@ -1,12 +1,11 @@
 // License: LGPL-3.0-or-later
 import * as React from 'react';
 import { observer } from 'mobx-react';
-import {InjectedIntlProps, injectIntl} from 'react-intl';
 import AriaModal = require('react-aria-modal');
 
 export interface ModalProps
 {
-  onClose?: () => void
+  onClose?: () => void // if you want your modal to close, this needs to set modalActive to false
   modalActive?: boolean
   titleText?: string
   focusDialog?:boolean
@@ -14,11 +13,16 @@ export interface ModalProps
   childGenerator:() => JSX.Element
 }
 
-class Modal extends React.Component<ModalProps & InjectedIntlProps, {}> {
+class Modal extends React.Component<ModalProps, {}> {
+
+  static defaultProps = {
+    dialogStyle: {minWidth:'768px'}
+  }
+
   render() {
    const modal = this.props.modalActive ?
       <AriaModal mounted={this.props.modalActive} titleText={this.props.titleText} focusDialog={this.props.focusDialog}
-                 onExit={this.props.onClose} dialogStyle={this.props.dialogStyle || {minWidth:'768px'}}>
+                 onExit={this.props.onClose} dialogStyle={this.props.dialogStyle}>
         <header className='modal-header'>
           <h4 className='modal-header-title'>{this.props.titleText}</h4>
         </header>
@@ -31,7 +35,7 @@ class Modal extends React.Component<ModalProps & InjectedIntlProps, {}> {
   }
 }
 
-export default injectIntl(observer(Modal))
+export default observer(Modal)
 
 
 

168584682881671100df1a27529c1be18f5126df
Eric Schultz <eric@wwahammy.com>
2018-11-02 16:23:34 -0500

    LESS LOGGING

--- config/environments/ci.rb
+++ config/environments/ci.rb
@@ -1,5 +1,6 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 Commitchange::Application.configure do
+<<<<<<< HEAD
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on

55171efbc42044256918852d3c8cda7f4d402ab6
Eric Schultz <eric@wwahammy.com>
2018-11-05 15:53:42 -0600

    Add missing license headers

--- app/uploaders/campaign_template_main_image_uploader.rb
+++ app/uploaders/campaign_template_main_image_uploader.rb
@@ -1,5 +1,4 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-
 class CampaignTemplateMainImageUploader < CarrierWave::Uploader::Base
 	include CarrierWave::MiniMagick
 

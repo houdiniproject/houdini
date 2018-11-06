d71140367427bfe03c4da79e2e0f3741912c9b58
Eric Schultz <eric@wwahammy.com>
2018-11-06 13:03:00 -0600

    WIP

--- spec/lib/insert/insert_duplicate_spec.rb
+++ spec/lib/insert/insert_duplicate_spec.rb
@@ -114,7 +114,9 @@ describe InsertDuplicate do
         expect(result.attributes.with_indifferent_access).to eq(common_result_attributes.merge(
             {
                 id: result.id,
-                end_datetime: nil
+                end_datetime: nil,
+                banner_image: nil,
+                campaign_template_id: nil
             }).with_indifferent_access)
         validate_cgo(result)
       end

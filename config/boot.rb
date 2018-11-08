3291392c80b17389f22f8f3132a8a2f60c1bc933
Eric Schultz <eric@wwahammy.com>
2018-11-08 16:29:53 -0600

    Boot  update

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,5 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-require 'rubygems'
-
+#
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 

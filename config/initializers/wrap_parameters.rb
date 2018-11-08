6fa8bd5a627b1934e7fabf21268f0d295dd5aed1
Eric Schultz <eric@wwahammy.com>
2018-11-08 16:40:50 -0600

    Fix wrap params

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -1,15 +1,15 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 # Be sure to restart your server when you modify this file.
-#
+
 # This file contains settings for ActionController::ParamsWrapper which
 # is enabled by default.
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
 ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json]
+  wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
 end
 
-# Disable root element in JSON by default.
+# To enable root element in JSON for ActiveRecord objects.
 ActiveSupport.on_load(:active_record) do
-  self.include_root_in_json = false
+ self.include_root_in_json = true
 end

34ef64179bfa2e79650499d0601dd6a3c509ee8a
Eric Schultz <eric@wwahammy.com>
2018-12-10 14:17:08 -0600

    Address hashing now uses JSON

--- lib/address_comparisons.rb
+++ lib/address_comparisons.rb
@@ -1,17 +1,10 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 module AddressComparisons
-  # we pick a really weird character that could never be used in an address
-  # The winner is Cuneiform Sign a Times A (U+12001) - https://unicode-table.com/en/12001/
-  DELIMITER = "𒀁"
-
   def self.calculate_hash(supporter_id, address, city, state_code, zip_code, country)
-    OpenSSL::Digest::SHA224.new(safely_delimited_address_string(supporter_id, address, city, state_code, zip_code, country)).hexdigest
+    OpenSSL::Digest::SHA224.new(safely_delimited_address_string(supporter_id: supporter_id, address: address, city: city, state_code: state_code, zip_code: zip_code, country: country)).hexdigest
   end
 
-  def self.safely_delimited_address_string(*address_parts)
-    address_parts.map do |i|
-      j = i&.to_s || ""
-      j.strip.upcase.gsub(/[[:punct:]]/, '').gsub(DELIMITER, '')
-    end.join("𒀁")
+  def self.safely_delimited_address_string(address_parts)
+    JSON::generate(address_parts)
   end
 end

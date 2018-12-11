21d2b7e11455e0ac4fa509c52455d3112bc86ab9
Eric Schultz <eric@wwahammy.com>
2018-12-11 15:20:27 -0600

    Add spec for CalculateDefaultAddressStrategy

--- spec/lib/calculate/calculate_default_address_strategy_spec.rb
+++ spec/lib/calculate/calculate_default_address_strategy_spec.rb
@@ -2,7 +2,21 @@
 require 'rails_helper'
 
 describe CalculateDefaultAddressStrategy do
-  it 'is untested' do
-    pending 'add tests here'
+  it 'returns ManualStrategy on manual' do
+    default_result = CalculateDefaultAddressStrategy.find_strategy()
+    expect(default_result).to be_an_instance_of DefaultAddressStrategies::ManualStrategy
+
+    result = CalculateDefaultAddressStrategy.find_strategy(:manual)
+    expect(result).to be_an_instance_of DefaultAddressStrategies::ManualStrategy
+  end
+
+  it 'returns AlwaysFirstStrategy on always_first' do
+    result = CalculateDefaultAddressStrategy.find_strategy(:always_first)
+    expect(result).to be_an_instance_of DefaultAddressStrategies::AlwaysFirstStrategy
+  end
+
+  it 'returns AlwaysLastStrategy on always_last' do
+    result = CalculateDefaultAddressStrategy.find_strategy(:always_last)
+    expect(result).to be_an_instance_of DefaultAddressStrategies::AlwaysLastStrategy
   end
 end

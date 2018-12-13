238e80f04d486f86012433d33c7eae321465c36a
Eric Schultz <eric@wwahammy.com>
2018-12-13 16:03:09 -0600

    Supporter paging arguments now validated

--- app/api/houdini/v1/validators/less_than_or_equal.rb
+++ app/api/houdini/v1/validators/less_than_or_equal.rb
@@ -2,7 +2,19 @@
 class Houdini::V1::Validators::LessThanOrEqual < Grape::Validations::Base
   def validate_param!(attr_name, params)
     unless params[attr_name] <= @option
-      fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: 'MESSAGE'
+      fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: translate(:less_than_or_equal, value: @option)
     end
+  end
+
+
+  def translate(key, **options)
+    if key.is_a? Symbol
+      key = Grape::Exceptions::Base.BASE_MESSAGES_KEY + "." + key
+    end
+
+    options = options.dup
+    options[:default] &&= options[:default].to_s
+    message = ::I18n.translate(key, **options)
+    message.present? ? message : ::I18n.translate(key, locale: FALLBACK_LOCALE, **options)
   end
 end

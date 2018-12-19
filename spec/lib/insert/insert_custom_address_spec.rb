bc2e6fec5fadd3af0e0ba1fdc185a03897d345fa
Eric Schultz <eric@wwahammy.com>
2018-12-19 13:00:08 -0600

    InsertCustomAddress

--- spec/lib/insert/insert_custom_address_spec.rb
+++ spec/lib/insert/insert_custom_address_spec.rb
@@ -2,7 +2,47 @@
 require 'rails_helper'
 
 describe InsertCustomAddress do
-  it 'is untested' do
-    pending 'add tests here'
+  include_context :shared_rd_donation_value_context
+  let(:address_data) {
+    {
+      'address': 'address',
+      'city': 'city',
+      'state_code': 'state',
+      'zip_code': 'zipppy zip',
+      'country': 'country code'
+    }
+  }
+  describe '.create' do
+    it 'should create custom address and notify of creation' do
+      address_strategy = double('address_strategy')
+      expect(address_strategy).to receive(:on_add).once
+      expect(supporter).to receive(:default_address_strategy).and_return(address_strategy)
+
+      result = InsertCustomAddress::create(supporter, address_data)
+      expect(result).to eq CustomAddress.last
+    end
+  end
+
+  describe '.find_or_create' do
+    it 'should create new address when none in system already' do
+      address_strategy = double('address_strategy')
+      expect(address_strategy).to receive(:on_add).once
+      expect(supporter).to receive(:default_address_strategy).and_return(address_strategy)
+
+      result = InsertCustomAddress::find_or_create(supporter, address_data)
+      expect(result).to eq CustomAddress.last
+    end
+
+    it 'should get old address' do
+      address_strategy = double('address_strategy')
+      expect(address_strategy).to_not receive(:on_add)
+      allow(supporter).to receive(:default_address_strategy).and_return(address_strategy)
+
+      address = CustomAddress.create!({supporter:supporter}.merge(address_data))
+      result = InsertCustomAddress::find_or_create(supporter, address_data)
+
+      expect(result).to eq address
+
+    end
   end
 end

7dbdff007350c6e03ef3d267937d9071dfd00150
Eric Schultz <eric@wwahammy.com>
2018-12-19 13:00:08 -0600

    InsertCustomAddress

--- lib/insert/insert_custom_address.rb
+++ lib/insert/insert_custom_address.rb
@@ -1,16 +1,14 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 module InsertCustomAddress
-
-  def create(supporter, address_data)
+  def self.create(supporter, address_data)
     address = CustomAddress.create!({supporter:supporter}.merge(address_data))
     supporter.default_address_strategy.on_add(supporter, address)
+    address
   end
 
-  def find_or_create(supporter, address_data)
+  def self.find_or_create(supporter, address_data)
     address = CustomAddress.find_via_fingerprint(supporter, address_data[:address], address_data[:city], address_data[:state_code], address_data[:zip_code], address_data[:country])
-    if (address)
-      supporter.default_address_strategy.on_use(supporter, address)
-    else
+    unless address
       address = create(supporter, address_data)
     end
     address

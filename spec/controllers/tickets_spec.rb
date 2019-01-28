b4ebaa81024e843b870ff7fc94e5f75359700b0b
Eric Schultz <eric@wwahammy.com>
2019-01-28 14:17:10 -0600

    Fix routing bugs in TicketsController spec

--- spec/controllers/tickets_spec.rb
+++ spec/controllers/tickets_spec.rb
@@ -6,32 +6,31 @@ describe TicketsController, :type => :controller do
   describe 'authorization' do
     include_context :shared_user_context
     describe 'rejects unauthorized users' do
-      describe 'update' do
-          include_context :open_to_event_editor, :put, :update, nonprofit_id: :__our_np, event_id: :__our_event
-      end
+
       describe 'index' do
-          include_context :open_to_event_editor, :get, :index, nonprofit_id: :__our_np, event_id: :__our_event
+        include_context :open_to_event_editor, :get, :index, nonprofit_id: :__our_np, event_id: :__our_event
       end
 
+      describe 'update' do
+          include_context :open_to_event_editor, :put, :update, nonprofit_id: :__our_np, event_id: :__our_event, id: 1111
+      end
 
       describe 'destroy' do
-          include_context :open_to_event_editor, :delete, :destroy, nonprofit_id: :__our_np, event_id: :__our_event
+          include_context :open_to_event_editor, :delete, :destroy, nonprofit_id: :__our_np, event_id: :__our_event, id: 1111
       end
 
       describe 'delete_card_for_ticket' do
           include_context :open_to_np_associate, :post, :delete_card_for_ticket, nonprofit_id: :__our_np, event_id: :__our_event, id: 11111
       end
-
-
     end
 
     describe 'open to all' do
       describe 'create' do
-          include_context :open_to_all, :post, :create, nonprofit_id: :__our_np
+          include_context :open_to_all, :post, :create, nonprofit_id: :__our_np, event_id: :__our_event
       end
 
       describe 'add_note' do
-        include_context :open_to_all, :put, :add_note, nonprofit_id: :__our_np, event_id: :__our_event
+        include_context :open_to_all, :put, :add_note, nonprofit_id: :__our_np, event_id: :__our_event, id: 1111
       end
 
     end

0e38e4133ada6e246cdd8a86504fdb7aac2b43f8
Eric Schultz <eric@wwahammy.com>
2019-02-01 13:15:35 -0600

    Add Grape Devise into our gems because it's unsupported

---
+++ gems/grape_devise/spec/dummy/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable
+end

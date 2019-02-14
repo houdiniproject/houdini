b471adb0ef64266a03f618c752b9c09226062da2
Eric Schultz <eric@wwahammy.com>
2019-02-14 16:16:26 -0600

    Add Javascript animation to modal opening and closing

--- javascripts/src/components/common/Modal.tsx
+++ javascripts/src/components/common/Modal.tsx
@@ -2,37 +2,73 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import AriaModal = require('react-aria-modal');
+import { VelocityTransitionGroup } from 'velocity-react';
+import 'velocity-animate';
+import 'velocity-animate/velocity.ui';
 
-export interface ModalProps
-{
+export interface ModalProps {
   onClose?: () => void // if you want your modal to close, this needs to set modalActive to false
   modalActive?: boolean
   titleText?: string
-  focusDialog?:boolean
-  dialogStyle?:any
-  childGenerator:() => any
+  focusDialog?: boolean
+  dialogStyle?: any
+  childGenerator: () => any
 }
 
 class Modal extends React.Component<ModalProps, {}> {
 
   static defaultProps = {
-    dialogStyle: {minWidth:'768px'}
+    dialogStyle: { minWidth: '768px' }
   }
 
   render() {
-   const modal = this.props.modalActive ?
-      <AriaModal mounted={this.props.modalActive} titleText={this.props.titleText} focusDialog={this.props.focusDialog}
-                 onExit={this.props.onClose} dialogStyle={this.props.dialogStyle}>
-        <header className='modal-header'>
-          <h4 className='modal-header-title'>{this.props.titleText}</h4>
-        </header>
-        <div className="modal-body">
-          {this.props.childGenerator()}
-        </div>
-      </AriaModal> : false;
 
-      return modal
+    const innerModal = this.props.modalActive ? <AriaModal mounted={this.props.modalActive} titleText={this.props.titleText} focusDialog={this.props.focusDialog}
+      onExit={this.props.onClose} dialogStyle={this.props.dialogStyle}>
+      <header className='modal-header'>
+        <h4 className='modal-header-title'>{this.props.titleText}</h4>
+      </header>
+      <div className="modal-body">
+        {this.props.childGenerator()}
+      </div>
+    </AriaModal> : false
+
+    const modal =
+      <VelocityTransitionGroup
+        enter={
+          {
+            animation: 'fadeIn',
+            /* These styles are needed because, for some reason, we're 
+            not able to cover the sidebar otherwise. Why? *shrug*
+            */
+            style: {
+              position: 'fixed',
+              top: '0px',
+              left: '0px',
+              zIndex: '5000'
+            }
+          }
+        }
+
+        leave={
+          {
+            animation: 'fadeOut',
+            style: {
+              position: 'fixed',
+              top: '0px',
+              left: '0px',
+              zIndex: '5000'
+            }
+          }
+        }
+        
+        runOnMount={true}>
+        {innerModal}
+      </VelocityTransitionGroup>;
+
+    return modal
   }
+
 }
 
 export default observer(Modal)

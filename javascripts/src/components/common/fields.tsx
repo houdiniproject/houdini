dd5fcce79bfdfa62b6a80b0ecf0349fe51ac7432
Eric Schultz <eric@wwahammy.com>
2019-04-18 13:38:37 -0500

    Add support for input addon (from Bootstrap)

--- javascripts/src/components/common/fields.tsx
+++ javascripts/src/components/common/fields.tsx
@@ -1,9 +1,9 @@
 // License: LGPL-3.0-or-later
 import * as React from 'react';
-import {observer} from "mobx-react";
-import {Field} from "../../../../types/mobx-react-form";
+import { observer } from "mobx-react";
+import { Field } from "../../../../types/mobx-react-form";
 import LabeledFieldComponent from "./LabeledFieldComponent";
-import {HoudiniField} from "../../lib/houdini_form";
+import { HoudiniField } from "../../lib/houdini_form";
 import ReactInput from "./form/ReactInput";
 import ReactSelect from './form/ReactSelect';
 import ReactTextarea from "./form/ReactTextarea";
@@ -11,77 +11,101 @@ import ReactMaskedInput from "./form/ReactMaskedInput";
 import createNumberMask from "../../lib/createNumberMask";
 
 export interface ClassNameable {
-  className?:string
+  className?: string
 }
 
-interface FieldProps extends ClassNameable{
-  field:Field,
-  placeholder?:string,
-  label?:string
-  inputClassNames?:string
+const InputGroupAddon: React.StatelessComponent<{ children: JSX.Element }> = (props) => {
+  return <span className="input-group-addon">{props.children}</span>
+}
+
+InputGroupAddon.displayName = "InputGroupAddon"
+
+function wrapInInputGroupWhenNeeded({ input, prefixInputAddon, postfixInputAddon }: { input: JSX.Element; prefixInputAddon?: JSX.Element; postfixInputAddon?: JSX.Element; }): JSX.Element {
+  const prefix = prefixInputAddon ? <InputGroupAddon>{prefixInputAddon}</InputGroupAddon> : false;
+
+  const postfix = postfixInputAddon ? <InputGroupAddon>{postfixInputAddon}</InputGroupAddon> : false;
+
+  if (prefix || postfix) {
+    return <div className="input-group">
+      {prefix}
+      {input}
+      {postfix}
+    </div>
+  }
+  else {
+    return input
+  }
+}
+
+interface FieldProps extends ClassNameable {
+  field: Field,
+  placeholder?: string,
+  label?: string
+  inputClassNames?: string
 }
 
 interface BasicFieldProps extends FieldProps {
-
+  prefixInputAddon?: JSX.Element
+  postfixInputAddon?: JSX.Element
 }
 
-export const BasicField = observer((props:BasicFieldProps) =>{
-    let field = props.field as HoudiniField
-    return <LabeledFieldComponent
-        inputId={props.field.id} labelText={field.label} inError={field.hasError} error={field.error}
-        inStickyError={field.hasServerError} stickyError={field.serverError}
-        className={props.className} >
-        <ReactInput field={field} label={props.label} placeholder={props.placeholder} className={`form-control ${props.inputClassNames || ''}`}/>
-    </LabeledFieldComponent>
+export const BasicField = observer((props: BasicFieldProps) => {
+  let field = props.field as HoudiniField
+  return <LabeledFieldComponent
+    inputId={props.field.id} labelText={field.label} inError={field.hasError} error={field.error}
+    inStickyError={field.hasServerError} stickyError={field.serverError}
+    className={props.className} >
+    {wrapInInputGroupWhenNeeded({ input: <ReactInput field={field} label={props.label} placeholder={props.placeholder} className={`form-control ${props.inputClassNames || ''}`} />, prefixInputAddon: props.prefixInputAddon, postfixInputAddon: props.postfixInputAddon })}
+  </LabeledFieldComponent>
 })
 
 interface SelectFieldProps extends FieldProps {
-  options?:Array<{id:any, name:string}>
+  options?: Array<{ id: any, name: string }>
 }
 
-export const SelectField = observer((props:SelectFieldProps) =>{
+export const SelectField = observer((props: SelectFieldProps) => {
   let field = props.field as HoudiniField
   return <LabeledFieldComponent
     inputId={props.field.id} labelText={field.label} inError={field.hasError} error={field.error}
     inStickyError={field.hasServerError} stickyError={field.serverError}
     className={props.className} >
 
-    <ReactSelect field={field} label={props.label} placeholder={props.placeholder} className={`form-control ${props.inputClassNames}`} options={props.options}/>
+    <ReactSelect field={field} label={props.label} placeholder={props.placeholder} className={`form-control ${props.inputClassNames}`} options={props.options} />
 
   </LabeledFieldComponent>
 })
 
 interface TextareaFieldProps extends FieldProps {
-    rows?:number
+  rows?: number
 }
 
-export const TextareaField = observer((props:TextareaFieldProps) =>{
+export const TextareaField = observer((props: TextareaFieldProps) => {
   let field = props.field as HoudiniField
   return <LabeledFieldComponent
     inputId={props.field.id} labelText={field.label} inError={field.hasError} error={field.error}
     inStickyError={field.hasServerError} stickyError={field.serverError}
     className={props.className} >
 
-    <ReactTextarea field={field} label={props.label} placeholder={props.placeholder} className={`form-control ${props.inputClassNames}`} rows={props.rows}/>
+    <ReactTextarea field={field} label={props.label} placeholder={props.placeholder} className={`form-control ${props.inputClassNames}`} rows={props.rows} />
 
   </LabeledFieldComponent>
 })
 
 interface CurrencyFieldProps extends FieldProps {
-  currencySymbol?:string, 
-  mustBeNegative?:boolean, 
-  allowNegative?:boolean
+  currencySymbol?: string,
+  mustBeNegative?: boolean,
+  allowNegative?: boolean
 }
 
 
-export const CurrencyField = observer((props:CurrencyFieldProps) => {
+export const CurrencyField = observer((props: CurrencyFieldProps) => {
   let field = props.field as HoudiniField
   let currencySymbol = props.mustBeNegative ? "-$" : "$"
   let allowNegative = props.allowNegative || !props.mustBeNegative
   return <LabeledFieldComponent
-  inputId={props.field.id} labelText={field.label} inError={field.hasError} error={field.error}
-  inStickyError={field.hasServerError} stickyError={field.serverError}
-  className={props.className} >
+    inputId={props.field.id} labelText={field.label} inError={field.hasError} error={field.error}
+    inStickyError={field.hasServerError} stickyError={field.serverError}
+    className={props.className}>
 
       <ReactMaskedInput field={field} label={props.label} placeholder={props.placeholder}
                         className={`form-control ${props.inputClassNames}`} guide={true}

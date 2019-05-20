d4c5780444f371f6afbce869a79d9dcc453601be
Eric Schultz <eric@wwahammy.com>
2019-05-20 13:23:32 -0500

    Fix bug where lack of user confirmation causes some bugs to occur

--- client/js/common/ajax/get_campaign_and_event_names_and_ids.js
+++ client/js/common/ajax/get_campaign_and_event_names_and_ids.js
@@ -6,21 +6,31 @@ module.exports = function(npo_id) {
   var eventsPath = '/nonprofits/' + npo_id + '/events/name_and_id'
 
   request.get(campaignsPath).end(function(err, resp){
-    resp.body.unshift(false)
-    let campaign_id_names = resp.body.map((i) => {
-      if (i.isChildCampaign)
-      {
-        return {id: i.id, name: i.name + " - " + i.creator}
-      }
-      else
-      {
-        return {id: i.id, name: i.name}
-      }
-    })
-    appl.def('campaigns.data', campaign_id_names)
+    var dataResponse = []
+
+    if (!err) {
+      resp.body.unshift(false)
+      dataResponse = resp.body.map((i) => {
+        if (i.isChildCampaign)
+        {
+          return {id: i.id, name: i.name + " - " + i.creator}
+        }
+        else
+        {
+          return {id: i.id, name: i.name}
+        }
+      })
+    }
+    appl.def('campaigns.data', dataResponse)
   })
+
   request.get(eventsPath).end(function(err, resp){
-    resp.body.unshift(false)
-    appl.def('events.data', resp.body)
+    var dataResponse = []
+    if(!err) {
+      resp.body.unshift(false)
+      dataResponse = resp.body
+    }
+
+    appl.def('events.data', dataResponse)
   })
 }

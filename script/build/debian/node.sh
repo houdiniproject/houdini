c0eb3f40bd8543e22b55f1acf363bc6c64b9eec0
Eric Schultz <eric@commitchange.com>
2019-05-31 17:19:51 -0500

    REBUILD WITH  ./dc build -- Add full-icu to docker containers because Node doesn't include it

--- script/build/debian/node.sh
+++ script/build/debian/node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-
-curl -sL https://deb.nodesource.com/setup_9.x | bash -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get update -qq && apt-get install -y nodejs
 npm install npm@^6 -g
+npm install -g icu4c-data@64l

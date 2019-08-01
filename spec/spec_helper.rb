2552e831b32a12ed7cff919aee7c19ce8e2b54ee
Luis Castro <luiscastroem@gmail.com>
2019-08-01 15:49:38 +0200

    chore(rspec): supress warnings on false positives

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,7 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.on_potential_false_positives = :nothing
   end
 
   # rspec-mocks config goes here. You can use an alternate test double

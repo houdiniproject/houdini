0cf21bb1c93849237daf606861ef65d7338b56f9
Luis Castro <luiscastroem@gmail.com>
2019-08-06 16:06:14 +0200

    feat(email_list): add strong params

--- app/controllers/nonprofits/email_lists_controller.rb
+++ app/controllers/nonprofits/email_lists_controller.rb
@@ -8,12 +8,18 @@ module Nonprofits
     before_action :authenticate_nonprofit_user!
 
     def index
-      render_json { Qx.fetch(:email_lists, nonprofit_id: params[:nonprofit_id]) }
+      render_json { Qx.fetch(:email_lists, nonprofit_id: email_list_params[:nonprofit_id]) }
     end
 
     def create
-      tag_master_ids = params['tag_masters'].values.map(&:to_i)
-      render_json { InsertEmailLists.for_mailchimp(params[:nonprofit_id], tag_master_ids) }
+      tag_master_ids = email_list_params[:tag_masters].values.map(&:to_i)
+      render_json { InsertEmailLists.for_mailchimp(email_list_params[:nonprofit_id], tag_master_ids) }
+    end
+
+    private
+
+    def email_list_params
+      params.permit(:nonprofit_id, :tag_masters)
     end
   end
 end
--- app/models/email_list.rb
+++ app/models/email_list.rb
@@ -2,7 +2,10 @@
 
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 class EmailList < ApplicationRecord
-  # attr_accessible :list_name, :mailchimp_list_id, :nonprofit, :tag_master
+  # :list_name,
+  # :mailchimp_list_id,
+  # :nonprofit,
+  # :tag_master
   belongs_to :nonprofit
   belongs_to :tag_master
 end

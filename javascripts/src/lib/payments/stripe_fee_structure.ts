3f5896423425863c2a88304661bcfd256cd25bd3
Eric Schultz <eric@commitchange.com>
2019-08-08 16:35:16 -0500

    Typescript class for calculating a Stripe fee

---
+++ javascripts/src/lib/payments/stripe_fee_structure.ts
@@ -0,0 +1,49 @@
+// License: LGPL-3.0-or-later
+import {boundMethod} from 'autobind-decorator'
+import { Money } from "../money";
+import _ = require('lodash');
+
+type FeeStructureProps = {flatFee?:number, percentFee?:number}
+
+export class StripeFeeStructure {
+
+    constructor(readonly props:FeeStructureProps)
+    {
+        if (!props.flatFee && !props.percentFee) {
+            throw Error(`flatFee or percentFee must be passed`)
+        }
+
+        const def:FeeStructureProps = {
+            flatFee: 0,
+            percentFee: 0
+        }
+
+        props = {...def, ...props}
+
+        if (props.percentFee < 0 || props.percentFee > 1)
+        {
+            throw Error(`percentFee must be less or equal to 1 and greater then or equal to 0`)
+        }
+
+        if (props.flatFee < 0) {
+            throw Error(`flatFee must be 0 or greater`)
+        }
+
+        if (!_.isInteger(props.flatFee)){
+            throw Error(`flatFee must be an integer as its always in the smallest currency unit`)
+        }
+
+        this.props = props;
+        Object.freeze(this.props)
+    }
+    
+    @boundMethod
+    calculateFee(x:Money) : Money {
+        return Money.fromCents(Math.round(x.amountInCents - (x.amountInCents - this.props.flatFee - (x.amountInCents * this.props.percentFee))), x.currency);
+    }
+
+    @boundMethod
+    reverseCalculateFee(x:Money):Money {
+        return Money.fromCents(Math.round(((x.amountInCents + this.props.flatFee) / (1 - this.props.percentFee) - x.amountInCents)), x.currency)
+    }
+}

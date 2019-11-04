a85235a75f51e44797bb496a89ab9bdbc7ce2c56
Eric Schultz <eric@commitchange.com>
2019-11-04 14:41:36 -0600

    Update throttling to prevent so many card attempts

--- config/initializers/throttling.rb
+++ config/initializers/throttling.rb
@@ -4,20 +4,41 @@ def run_throttle?
   Rails.env != 'test' || (defined? FORCE_THROTTLE && FORCE_THROTTLE)
 end
 
-Rack::Attack.throttle('post to add card by supporter', limit:4, period: 60) do |req|
-  ret = nil
-  if run_throttle? && req.path == '/cards' && req.post?
-    begin
-      json = JSON.parse(req.body.string)
-      if json['card']['holder_type'] == 'Supporter'
-        ret = json['card']['holder_id']
-      end
-    rescue
-      req.body.rewind
-    end
-  end
 
-  ret
+if ENV['THROTTLE_CARD_L1_LIMIT'] && ENV['THROTTLE_CARD_L1_PERIOD']
+  Rack::Attack.throttle('post to add card by supporter LEVEL 1', limit:ENV['THROTTLE_CARD_L1_LIMIT'].to_i, period: ENV['THROTTLE_CARD_L1_PERIOD'].to_i) do |req|
+    ret = nil
+    if run_throttle? && req.path == '/cards' && req.post?
+      begin
+        json = JSON.parse(req.body.string)
+        if json['card']['holder_type'] == 'Supporter'
+          ret = json['card']['holder_id']
+        end
+      rescue
+        req.body.rewind
+      end
+    end
+
+    ret
+  end
+end
+
+if ENV['THROTTLE_CARD_L2_LIMIT'] && ENV['THROTTLE_CARD_L2_PERIOD']
+  Rack::Attack.throttle('post to add card by supporter LEVEL 2', limit:ENV['THROTTLE_CARD_L2_LIMIT'].to_i, period: ENV['THROTTLE_CARD_L2_PERIOD'].to_i) do |req|
+    ret = nil
+    if run_throttle? && req.path == '/cards' && req.post?
+      begin
+        json = JSON.parse(req.body.string)
+        if json['card']['holder_type'] == 'Supporter'
+          ret = json['card']['holder_id']
+        end
+      rescue
+        req.body.rewind
+      end
+    end
+
+    ret
+  end
 end
 
 
@@ -35,4 +56,9 @@ Rack::Attack.throttle('post to supporter', limit:10, period: 60) do |req|
   end
 
   ret
+end
+
+
+Rack::Attack.throttled_response = lambda do |env|
+  [ 429, {'Content-Type' => 'application/json'}, [JSON.generate({'error': "I'm sorry; you're not allowed to add a card that often to a single supporter. Please contact support@commitchange.com for help."})]]
 end

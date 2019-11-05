e4722ec3cac3aeb3e30aff614f64a46a330707fb
Eric Schultz <eric+this+is+ridiculous@commitchange.com>
2019-11-05 15:37:47 -0600

    Update Node 13 (simplifies ICU)

--- script/debian_setup.sh
+++ script/debian_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-curl -sL https://deb.nodesource.com/setup_9.x | bash -
+curl -sL https://deb.nodesource.com/setup_13.x | bash -
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs postgresql-9.6 default-jre

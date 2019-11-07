e3a10f0980cd4a532e44ed1c248a71838eef6950
Eric Schultz <eric+this+is+ridiculous@commitchange.com>
2019-11-07 14:33:51 -0600

    Fix typo

--- app/jobs/refund_notification_job.rb
+++ app/jobs/refund_notification_job.rb
@@ -2,7 +2,7 @@ class RefundNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(refund)
-    RefundNotificationJobDonorEmail.perform_later(refund)
+    RefundNotificationDonorEmailJob.perform_later(refund)
     RefundNotificationNonprofitEmailJob.perform_later(refund)
   end
 end

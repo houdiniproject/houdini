28d1c11af4b8dcd1759ae6947c738afb503d21ad
Eric Schultz <eric+this+is+ridiculous@commitchange.com>
2019-11-08 11:25:53 -0600

    Add test queue adapter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,7 +43,8 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  
+  ActiveJob::Base.queue_adapter = :test
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.

567549c85cd95e5833835cdca002ed3da139b263
Eric Schultz <eric+this+is+ridiculous@commitchange.com>
2019-11-21 15:57:39 -0600

    Everything works except fonts

--- app/javascript/packs/registration_page.js
+++ app/javascript/packs/registration_page.js
@@ -1,4 +1,5 @@
 // License: LGPL-3.0-or-later
-
+require('../../../client/css/global/page.css')
+require('bootstrap-loader');
 require('../../../javascripts/app/registration_page')
 
--- app/javascript/packs/session_login_page.js
+++ app/javascript/packs/session_login_page.js
@@ -1,3 +1,3 @@
 // License: LGPL-3.0-or-later
-
+require('bootstrap-loader');
 require('../../../javascripts/app/session_login_page')

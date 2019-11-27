2889c7376cacb9314000aa7a4a0af7c54f7e63c9
Eric Schultz <eric@commitchange.com>
2019-11-27 10:37:22 -0600

    Extend session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 # Be sure to restart your server when you modify this file.
 
-Commitchange::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 4.hours
+Commitchange::Application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 4.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

6f087c76b84289a389e83bde7eb1f4539038d770
Eric Schultz <eric@commitchange.com>
2020-01-07 11:08:54 -0600

    Remove unused PaymentNotificationJob

--- app/jobs/payment_notification_job.rb
+++
@@ -1,8 +0,0 @@
-class PaymentNotificationJob < ApplicationJob
-  queue_as :default
-
-  def perform(donation, locale, user=nil)
-    PaymentNotificationEmailDonorJob.perform_later donation, locale
-    PaymentNotificationEmailNonprofitJob.perform_later donation, user
-  end
-end

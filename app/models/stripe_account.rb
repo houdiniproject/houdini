4f896a0926094b5f88e0bc7c9dc1ea8a354b5fb5
Eric Schultz <eric@commitchange.com>
2020-02-06 11:35:24 -0600

    Update StripeAccount to latest

--- app/models/stripe_account.rb
+++ app/models/stripe_account.rb
@@ -1,6 +1,9 @@
 class StripeAccount < ActiveRecord::Base
-  attr_accessible  :object
+  attr_accessible  :object, :stripe_account_id
   has_one :nonprofit, primary_key: :stripe_account_id
+  has_one :nonprofit_verification_process_status, primary_key: :stripe_account_id
+
+  after_save
 
   def object=(input)
     serialize_on_update(input)
@@ -23,10 +26,10 @@ class StripeAccount < ActiveRecord::Base
   end
 
   def verification_status
-    if eventually_due.any? || currently_due.any? || past_due.any?
-      result = :unverified
-    elsif (pending_verification.any?)
+    if pending_verification.any?
       result = :pending
+    elsif needs_more_validation_info
+      result = :unverified
     else
       result = :verified
     end
@@ -34,7 +37,7 @@ class StripeAccount < ActiveRecord::Base
   end
 
   def needs_more_validation_info
-    [self.currently_due, self.past_due, self.eventually_due].map{|i| i || []}.any?{|i| i.all?{|j| j.starts_with('external_account')}}
+    ![self.currently_due, self.past_due, self.eventually_due].map{|i| i || []}.any?{|i| i.all?{|j| j.starts_with?('external_account')}}
   end
 
   private 
@@ -51,10 +54,9 @@ class StripeAccount < ActiveRecord::Base
       write_attribute(:object, input)
       object_json = JSON::parse(input)
     end
-
     self.charges_enabled = !!object_json['charges_enabled']
     self.payouts_enabled = !!object_json['payouts_enabled']
-    requirements = object_json['requirements'] || []
+    requirements = object_json['requirements'] || {}
     self.disabled_reason =  requirements['disabled_reason']
     self.currently_due = JSON.generate(requirements['currently_due'] || [])
     self.past_due =  JSON.generate(requirements['past_due'] || [])

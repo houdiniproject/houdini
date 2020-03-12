c0c643c2eb9b60eeb37209894a050105cb2d23da
Eric Schultz <eric@commitchange.com>
2020-03-12 14:47:11 -0500

    Initial NonprofitCreateJob

---
+++ lib/job_types/nonprofit_create_job.rb
@@ -0,0 +1,14 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+module JobTypes
+  class NonprofitCreateJob < GenericJob
+    attr_reader :nonprofit_id
+
+    def initialize(nonprofit_id)
+      @nonprofit_id = nonprofit_id
+    end
+
+    def perform
+      fail
+    end
+  end
+end

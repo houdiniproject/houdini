334d5422a33d6d3baa2ef42f15013532b09559c8
Eric Schultz <eric@commitchange.com>
2020-03-18 16:29:42 -0500

    Fix reference bug in ticket mailers

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -8,7 +8,7 @@ class PaymentMailer < BaseMailer
     if payment.kind == 'Donation' || payment.kind == 'RecurringDonation'
       return Delayed::Job.enqueue JobTypes::NonprofitPaymentNotificationJob.new(payment.donation.id, user_id)
     elsif payment.kind == 'Ticket'
-      return TicketMailer.receipt_admin(payment.donation.id, user_id).deliver
+      return TicketMailer.receipt_admin(payment.tickets.pluck(:id), user_id).deliver
     elsif payment.kind == 'Refund'
       return Delayed::Job.enqueue JobTypes::NonprofitRefundNotificationJob.new(payment.refund.id, user_id)
     end

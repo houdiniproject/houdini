b1b9968b8e93955870a42dd7708fbd58c24c0b35
Eric Schultz <eric@commitchange.com>
2020-03-25 16:34:40 -0500

    Simplify btn so it can be cached properly

--- client/js/nonprofits/btn/page.js
+++ client/js/nonprofits/btn/page.js
@@ -1,19 +1,18 @@
 // License: LGPL-3.0-or-later
  var Font = require('../../common/brand-fonts'),
      utils = require('../../common/utilities'),
-	$brandedButton = $('.branded-donate-button')
+	$brandedButton = document.querySelector('.branded-donate-button')
 
   if(utils.get_param('fixed')){
-	$brandedButton.addClass('is-fixed')
-	$('.centered').css('padding-top', '5px')
+	$brandedButton.classList.add('is-fixed')
+	var element = document.querySelector('.centered')
+	element.style.paddingTop = '5px'
   }
 
   var $logoBlue = '#42B3DF',
 	brandColor = app.nonprofit.brand_color || $logoBlue,
 	brandFont = Font[app.nonprofit.brand_font] || Font.bitter
 
-  $brandedButton.css({
-	'background-color': brandColor,
-	'font-family': brandFont 
-	}
-  )
+$brandedButton.style.backgroundColor = brandColor;
+$brandedButton.style.fontFamily = brandFont;
+  

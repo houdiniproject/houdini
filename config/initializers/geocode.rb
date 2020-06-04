5ceb05e6c3c8fbc5f64a125102383e01255b3654
Eric <eric@commitchange.com>
2020-06-04 11:21:02 -0500

    Set Geocoder to use the test provider on tests

--- config/initializers/geocode.rb
+++ config/initializers/geocode.rb
@@ -3,7 +3,7 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 Geocoder.configure(
   cache: Rails.cache,
-  lookup: :google,
+  lookup: Rails.env == 'test' ? :test : :google,
   use_https: true,
   api_key: ENV['GOOGLE_API_KEY'],
   timeout: 10

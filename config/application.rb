595b39d3ed67e02804faa85b52439f2265f866dc
Eric <eric@commitchange.com>
2020-06-08 16:43:02 -0500

    Mirror Bess as Houdini

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ require "action_view/railtie"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+# Mirror Bess as Houdini
+Houdini = Bess
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)

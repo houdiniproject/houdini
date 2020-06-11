ded3ef302d58ba574c4cece7287d9e530cdc43be
Eric <eric@commitchange.com>
2020-06-11 14:22:44 -0500

    Put most of bess into the Houdini module

--- gems/bess/lib/houdini.rb
+++ gems/bess/lib/houdini.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-require_relative './houdini'
+require "houdini/railtie"
+
+module Houdini
+  # Your code goes here...
+end

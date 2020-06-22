27a37aca3db599c7a285421b2a431f0dec78205e
Eric <eric@commitchange.com>
2020-06-22 14:47:10 -0500

    Correct the Wisper listeners

--- app/listeners/campaign_listener.rb
+++ app/listeners/campaign_listener.rb
@@ -3,7 +3,7 @@
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
 class CampaignListener < ApplicationListener
-    def campaign_create(campaign)
+    def self.campaign_create(campaign)
       if campaign.child_campaign?
         CampaignCreationFederatedEmailJob.perform_later(campaign)
       else
--- app/listeners/credit_card_payment_listener.rb
+++ app/listeners/credit_card_payment_listener.rb
@@ -3,32 +3,32 @@
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
 class CreditCardPaymentListener < ApplicationListener
-    def donation_create(donation, locale, user=nil)
+    def self.donation_create(donation, locale, user=nil)
       if donation.payment_provider == :credit_card
         PaymentNotificationEmailDonorJob.perform_later donation, locale
         PaymentNotificationEmailNonprofitJob.perform_later donation, user
       end
     end
 
-    def recurring_donation_create(donation, locale, user=nil)
+    def self.recurring_donation_create(donation, locale, user=nil)
       if donation.payment_provider == :credit_card
         PaymentNotificationEmailDonorJob.perform_later donation, locale
         PaymentNotificationEmailNonprofitJob.perform_later donation, user
       end
     end
 
-    def refund_create(refund)
+    def self.refund_create(refund)
       RefundNotificationJob.perform_later refund
     end
 
-    def recurring_donation_payment_succeeded(donation, locale, user=nil)
+    def self.recurring_donation_payment_succeeded(donation, locale, user=nil)
       if donation.payment_provider == :credit_card
         PaymentNotificationEmailDonorJob.perform_later donation, locale
         PaymentNotificationEmailNonprofitJob.perform_later donation, user
       end
     end
 
-    def recurring_donation_payment_failed(donation, locale)
+    def self.recurring_donation_payment_failed(donation, locale)
       FailedRecurringDonationPaymentDonorEmailJob.perform_later(donation)
       if (donation.recurring_donation.n_failures >= 3)
         FailedRecurringDonationPaymentNonprofitEmailJob.perform_later(donation)

6c492bb202593bfea091e6df69208257fdb754da
Eric <eric@commitchange.com>
2020-06-22 17:44:19 -0500

    Add react-rails

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,4 @@
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);

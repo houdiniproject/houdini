2c8da33706dab16affe89c9ca4be42ba00567d2c
Eric <eric@commitchange.com>
2020-06-22 17:47:42 -0500

    Remove legacy react generators

--- lib/generators/react/lib/lib_generator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class React::LibGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('templates', __dir__)
-  def copy_file_to_lib
-    template 'module.ts.erb', File.join('javascripts/src/lib/', *(class_path + ["#{file_name.underscore}.ts"]))
-    template 'module.spec.ts.erb', File.join('javascripts/src/lib/', *(class_path + ["#{file_name.underscore}.spec.ts"]))
-  end
-end

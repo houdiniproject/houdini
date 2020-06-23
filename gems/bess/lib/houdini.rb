6dfaf27f2ecc7faec6f88f7bbff80a80ddb3bfe4
Eric <eric@commitchange.com>
2020-06-23 17:23:08 -0500

    Turn bess into an engine so we can share generators

--- gems/bess/lib/houdini.rb
+++ gems/bess/lib/houdini.rb
@@ -2,7 +2,7 @@
 
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
-require "houdini/railtie"
+require "houdini/engine"
 
 module Houdini
   extend ActiveSupport::Autoload

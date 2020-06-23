3e2c6980c580757af23b42d1d000a47e7fc9b649
Eric <eric@commitchange.com>
2020-06-23 16:53:28 -0500

    Update fork-ts-checkout-webpack-plugin to 5.0.4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,12 +5,12 @@ const erb = require('./loaders/erb')
 
 environment.loaders.prepend('erb', erb)
 environment.plugins.append(
-    "ForkTsCheckerWebpackPlugin",
-    new ForkTsCheckerWebpackPlugin({
-      // this is a relative path to your project's TypeScript config
+  "ForkTsCheckerWebpackPlugin",
+  new ForkTsCheckerWebpackPlugin({
+    typescript: {
       tsconfig: path.resolve(__dirname, "../../tsconfig.json"),
-      // non-async so type checking will block compilation
-      async: false,
-    })
-  );
+    },
+    async: false,
+  })
+);
 module.exports = environment

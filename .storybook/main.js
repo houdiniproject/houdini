0b02f646937eff7c39b0b430554ce46bc5273615
Eric <eric@commitchange.com>
2020-06-23 16:49:46 -0500

    Add example storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,28 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: async config => {
-    // do mutation to the config
+// License: LGPL-3.0-or-later
+// Based on https://github.com/rails/webpacker/issues/1004#issuecomment-628377930
+process.env.NODE_ENV = "development";
+const railsWebpackEnv = require("../config/webpack/environment");
 
-    return config;
-  },
+module.exports = {
+  stories: ["../app/javascript/stories/*.[tj]s?(x)"],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: (config) => ({
+    // do mutation to the config
+    ...config,
+    resolve: {
+      ...config.resolve,
+      ...railsWebpackEnv.config.resolve,
+      modules: railsWebpackEnv.resolvedModules.map((i) => i.value),
+    },
+    module: {
+      ...config.module,
+      rules: railsWebpackEnv.loaders
+        .filter((i) => i.key !== "nodeModules")
+        .map((i) => i.value),
+    },
+    plugins: [
+      ...config.plugins,
+      ...railsWebpackEnv.plugins.map((i) => i.value),
+    ],
+  }),
 };

daec5dd3fc3d5d6150281d0b6db097dac30b7241
Eric <eric@commitchange.com>
2020-06-26 16:50:19 -0500

    Change the eslintrc.js file for our Typescript work

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,31 @@
 // License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 // Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+	root: true,
+
+	overrides: [
+		{
+			"files": ['**/*.ts'],
+			parser: '@typescript-eslint/parser',
+			plugins: [
+				'@typescript-eslint',
+			],
+			extends: [
+				'eslint:recommended',
+				'plugin:@typescript-eslint/recommended',
+			]
+		}
+	],
+	"rules": {
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"semi": [
+			"error",
+			"never"
+		]
+	}
 };

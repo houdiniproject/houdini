eb4385ec173bed9c44d6d6bf2190c8e617a32566
Eric <eric@commitchange.com>
2020-06-29 15:54:35 -0500

    Setup linting of javascript used by node based build tools (eslint, babel, webpack, etc)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,38 @@
 // License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 // Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+const _ = require('lodash');
+
+const tsSpecBase = {
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+		'jest'
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jest/all'
+	],
+	rules:{
+		"jest/lowercase-name": ["error", { "ignore": ["describe"]}]
+	}
+};
+
+const tsSpec = _.cloneDeep(tsSpecBase);
+tsSpec['files'] = ['**/*.spec.ts'];
 
 
 module.exports = {
 	root: true,
 
 	overrides: [
+		{
+			"files": ['*.js', 'config/webpack/**/*.js'],
+			extends: [
+				'eslint:recommended',
+				'plugin:node/recommended'
+			],
+		},
 		{
 			"files": ['**/*.ts'],
 			parser: '@typescript-eslint/parser',
@@ -17,22 +44,7 @@ module.exports = {
 				'plugin:@typescript-eslint/recommended',
 			]
 		},
-		{
-			"files": ['**/*.spec.ts'],
-			parser: '@typescript-eslint/parser',
-			plugins: [
-				'@typescript-eslint',
-				'jest'
-			],
-			extends: [
-				'eslint:recommended',
-				'plugin:@typescript-eslint/recommended',
-				'plugin:jest/all'
-			],
-			rules:{
-				"jest/lowercase-name": ["error", { "ignore": ["describe"]}]
-			}
-		}
+		tsSpec
 	],
 	"rules": {
 		"linebreak-style": [

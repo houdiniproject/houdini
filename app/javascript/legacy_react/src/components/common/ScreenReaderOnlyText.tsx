23df9a53098596a691d8b8bf10069f71534f4a9f
Eric <eric@commitchange.com>
2020-06-29 16:01:52 -0500

    Add additional react eslint checks. Illustrate its use on a single file.

--- app/javascript/legacy_react/src/components/common/ScreenReaderOnlyText.tsx
+++ app/javascript/legacy_react/src/components/common/ScreenReaderOnlyText.tsx
@@ -1,26 +1,22 @@
 // License: LGPL-3.0-or-later
 import * as React from 'react';
 
-export interface ScreenReaderOnlyTextProps
-{
-}
 
-class ScreenReaderOnlyText extends React.Component<ScreenReaderOnlyTextProps, {}> {
+class ScreenReaderOnlyText extends React.Component<Record<string,unknown>, Record<string,unknown>> {
 
-
-  render() {
-    const style:React.CSSProperties = {
-      position: 'absolute',
-      width: '1px',
-      height: '1px',
-      padding: 0,
-      margin: '-1px',
-      overflow: 'hidden',
-      clip: 'rect(0,0,0,0)',
-      border: 0
-    }
-    return <span style={style}>{this.props.children}</span>
-  }
+	render() : JSX.Element {
+		const style:React.CSSProperties = {
+			position: 'absolute',
+			width: '1px',
+			height: '1px',
+			padding: 0,
+			margin: '-1px',
+			overflow: 'hidden',
+			clip: 'rect(0,0,0,0)',
+			border: 0
+		};
+		return <span style={style}>{this.props.children}</span>;
+	}
 }
 
 export default ScreenReaderOnlyText;

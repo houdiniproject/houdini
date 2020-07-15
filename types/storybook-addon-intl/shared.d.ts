063e736ed3ac7d6078571da5f24564d82245cbdc
Eric <eric@commitchange.com>
2020-07-15 12:41:20 -0500

    Add storybook-addon-intl

---
+++ types/storybook-addon-intl/shared.d.ts
@@ -0,0 +1,5 @@
+declare module 'storybook-addon-intl/shared' {
+  export declare const EVENT_SET_CONFIG_ID:string;
+  export declare const EVENT_GET_LOCALE_ID:string;
+  export declare const EVENT_SET_LOCALE_ID:string;
+}

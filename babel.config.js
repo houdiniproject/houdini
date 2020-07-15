4f0d907442a554e5c3e8ac631e3ab03ec0eedba3
Eric <eric@commitchange.com>
2020-07-15 12:44:39 -0500

    Make sure @material-ui/core and @material-ui/icons aren't fully imported in babel

--- babel.config.js
+++ babel.config.js
@@ -88,9 +88,11 @@ module.exports = function(api) {
         'babel-plugin-transform-imports',
         {
           '@material-ui/core': {
+            'transform': '@material-ui/core/esm/${member}',
             'preventFullImport': true
           },
           '@material-ui/icons': {
+            'transform': '@material-ui/icons/esm/${member}',
             'preventFullImport': true
           }
         }

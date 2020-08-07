233e16799dc351738ed00783f371683e440a3259
Eric Schultz <eric@commitchange.com>
2020-08-07 16:01:48 -0500

    Webpack copies the NOTICE-js to the output folder and puts a header with url to the notice in every webpack file

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,35 @@
-const { environment } = require('@rails/webpacker')
+const { environment, config } = require('@rails/webpacker')
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const webpack = require('webpack');
 const path = require("path");
 const erb = require('./loaders/erb')
+const fs = require('fs')
+const hash = require('object-hash')
+
+function getTerser() {
+  if(environment.config && 
+    environment.config.optimization && 
+    environment.config.optimization.minimizer && 
+    environment.config.optimization.minimizer instanceof Array &&
+    environment.config.optimization.minimizer.length === 1)
+    return environment.config.optimization.minimizer[0]
+  else 
+    return null;
+}
+
+function outputLicenseFile(file, outputDir) {
+	const contents = fs.readFileSync(file);
+	const name =  path.basename(file) + "-" + hash(contents) + ".txt";
+	if (!fs.existsSync(outputDir)) {
+		fs.mkdirSync(outputDir, {recursive: true});
+	}
+	fs.copyFileSync(file, path.join(outputDir, name));
+
+	return name;
+
+}
+
+
 
 environment.loaders.prepend('erb', erb)
 environment.plugins.append(
@@ -13,4 +41,21 @@ environment.plugins.append(
     async: false,
   })
 );
+
+environment.plugins.prepend("BannerPlugin", new webpack.BannerPlugin(
+  {banner: `@hlicense License information is available at ${config.publicPath}${outputLicenseFile('NOTICE-js', config.outputPath)}`,
+entryOnly: false}))
+
+
+const terser = getTerser()
+if (terser) {
+  terser.options.terserOptions = terser.options.terserOptions || {}
+  terser.options.terserOptions.output = terser.options.terserOptions.output || {} 
+  terser.options.terserOptions.output.comments = /@hlicense/i
+
+  // we don't want terser to print out license headers, we'll handle that ourselves
+  terser.options.extractComments = false;
+}
+
+
 module.exports = environment

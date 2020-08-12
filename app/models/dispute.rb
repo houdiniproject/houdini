86cb13c2a510a02b9942243a296eb5206f7d51fb
Eric Schultz <eric@commitchange.com>
2020-08-12 12:58:23 -0500

    Remove the unused :lost_and_paid status from Dispute

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -3,7 +3,7 @@ class Dispute < ActiveRecord::Base
 
   Reasons = [:unrecognized, :duplicate, :fraudulent, :subscription_canceled, :product_unacceptable, :product_not_received, :unrecognized, :credit_not_processed, :goods_services_returned_or_refused, :goods_services_cancelled, :incorrect_account_details, :insufficient_funds, :bank_cannot_process, :debit_not_authorized, :general]
 
-  Statuses = [:needs_response, :under_review, :won, :lost, :lost_and_paid]
+  Statuses = [:needs_response, :under_review, :won, :lost]
 
   attr_accessible \
     :gross_amount, # int

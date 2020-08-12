f3b73d06e8ba6c4d9576d9a16502260ddf0868fe
Eric Schultz <eric@commitchange.com>
2020-08-12 11:20:32 -0500

    Cleanup unused specs

--- spec/models/recurring_donation_hold_spec.rb
+++
@@ -1,6 +0,0 @@
-# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-require 'rails_helper'
-
-RSpec.describe RecurringDonationHold, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end

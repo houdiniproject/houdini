2456ccb9ebd8e2da4faf5d0172054fe202c49153
Eric Schultz <eric@commitchange.com>
2020-08-20 14:08:51 -0500

    Move dispute created and withdrawn at sametime in shared context

--- spec/support/contexts/disputes_context.rb
+++ spec/support/contexts/disputes_context.rb
@@ -461,3 +461,106 @@ RSpec.shared_context :dispute_won_specs do
     specify { expect(subject.date).to eq DateTime.new(2019,10,29,20,43,10)}
   end
 end
+
+
+RSpec.shared_context :dispute_created_and_withdrawn_at_same_time_context do 
+  include_context :disputes_context
+  let(:event_json_created) do
+    StripeMock.mock_webhook_event('charge.dispute.created-with-one-withdrawn')
+  end
+
+  let(:json_created) { event_json_created['data']['object']}
+
+  let(:json_funds_withdrawn) {event_json_funds_withdrawn['data']['object']}
+
+  let(:event_json_funds_withdrawn) do
+    json = StripeMock.mock_webhook_event('charge.dispute.funds_withdrawn')
+    stripe_helper.upsert_stripe_object(:dispute, json['data']['object'])
+    json
+  end
+
+  let!(:charge) { force_create(:charge, supporter: supporter, 
+    stripe_charge_id: 'ch_1Y7zzfBCJIIhvMWmSiNWrPAC', nonprofit: nonprofit, payment:force_create(:payment,
+       supporter:supporter,
+      nonprofit: nonprofit,
+      gross_amount: 80000))}
+end
+
+RSpec.shared_context :dispute_created_and_withdrawn_at_same_time_specs do
+  include_context :dispute_created_and_withdrawn_at_same_time_context
+
+  it 'has status of needs_response' do 
+    expect(obj.status).to eq 'needs_response'
+  end
+
+  it 'has reason of duplicate' do 
+    expect(obj.reason).to eq 'duplicate'
+  end
+
+  it 'has 1 balance transactions' do 
+    expect(obj.balance_transactions.count).to eq 1
+  end
+
+  it 'has a net_change of -81500' do
+    expect(obj.net_change).to eq -81500
+  end
+
+  it 'has an amount of 80000' do
+    expect(obj.amount).to eq 80000
+  end
+
+  it 'has a correct charge id ' do 
+    expect(obj.stripe_charge_id).to eq "ch_1Y7zzfBCJIIhvMWmSiNWrPAC"
+  end
+
+  it 'has a correct dispute id' do 
+    expect(obj.stripe_dispute_id).to eq "dp_05RsQX2eZvKYlo2C0FRTGSSA"
+  end
+
+  it 'has a correct charge id ' do 
+    expect(obj.stripe_charge_id).to eq "ch_1Y7zzfBCJIIhvMWmSiNWrPAC"
+  end
+
+  describe "dispute" do
+    subject { dispute }
+    specify {expect(subject).to be_persisted }
+    specify {expect(subject.gross_amount).to eq 80000 }
+    specify {expect(subject.status).to eq "needs_response" }
+    specify { expect(subject.reason).to eq 'duplicate' }
+  end
+
+  it 'has one dispute transaction' do
+    expect(dispute_transactions.count).to eq 1
+  end
+
+  describe 'has a withdrawal_transaction' do
+    subject{ withdrawal_transaction }
+    specify {  expect(subject).to be_persisted }
+    specify {  expect(subject.gross_amount).to eq -80000 }
+    specify {  expect(subject.fee_total).to eq -1500 }
+    specify { expect(subject.net_amount).to eq -81500}
+    specify {  expect(subject.stripe_transaction_id).to eq 'txn_1Y7pdnBCJIIhvMWmJ9KQVpfB' }
+    specify {  expect(subject).to be_persisted }
+    specify { expect(subject.disbursed).to eq false }
+  end
+
+  describe 'has a withdrawal_payment' do
+    subject { withdrawal_payment}
+    specify { expect(subject).to be_persisted }
+    specify { expect(subject.gross_amount).to eq -80000}
+    specify { expect(subject.fee_total).to eq -1500}
+    specify { expect(subject.kind).to eq 'Dispute'}
+    specify { expect(subject.nonprofit).to eq supporter.nonprofit}
+    specify { expect(subject.date).to eq DateTime.new(2020, 8, 3, 4, 55, 55)}
+  end
+
+  it 'has only added one payment' do
+    obj
+    expect(Payment.count).to eq 2 #one for charge, one for DisputeTransaction
+  end
+
+  it 'has only one dispute transaction' do 
+    obj
+    expect(DisputeTransaction.count).to eq 1
+  end
+end

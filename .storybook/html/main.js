3aeae309577db7f5a755e1b9dbf26aaceb61205c
Eric Schultz <eric@commitchange.com>
2020-09-23 13:41:25 -0500

    Add support for html stories

---
+++ .storybook/html/main.js
@@ -0,0 +1,11 @@
+// License: LGPL-3.0-or-later
+module.exports = {
+  "stories": [
+    "../../app/javascript/**/*--html.stories.mdx",
+    "../../app/javascript/**/*--html.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}

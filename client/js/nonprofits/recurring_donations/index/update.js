169637fe9b92baa0b1c86b6fb403f882e7edc15b
Eric Schultz <eric@commitchange.com>
2020-09-29 11:46:03 -0500

    Fix an update bug for the amount of recurring donations. Fixes commitchange/tix#2717

--- client/js/nonprofits/recurring_donations/index/update.js
+++ client/js/nonprofits/recurring_donations/index/update.js
@@ -1,9 +1,16 @@
 // License: LGPL-3.0-or-later
 
+function prepareFormObject(form_obj) {
+	form_obj['donation']['dollars'] = form_obj['donation']['dollars'].replace(',', '')
+	return form_obj
+}
+
+
 appl.def('ajax_details', {
 	update: function(id, form_obj, node) {
 		appl.def('loading', true)
 		delete form_obj.dollars
+		form_obj = prepareFormObject(form_obj)
 		appl.ajax.update('recurring_donation_details', id, form_obj).then(function(resp) {
 			appl.def('loading', false)
 			appl.ajax.index('recurring_donations')

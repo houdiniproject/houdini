fb4e38fa4c209bc67ba76fcd70bda7f701489660
Eric Schultz <eric@commitchange.com>
2020-10-07 12:39:08 -0500

    Update everything to postgres 12

--- script/build/debian/postgres.sh
+++ script/build/debian/postgres.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
-
-apt-get -yy install postgresql-client-9.6
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt update
+apt-get -yy install postgresql-client-12

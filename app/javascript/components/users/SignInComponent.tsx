66d1539b08ec2e050a8b4ad2947012e445e698a4
Gina  Torres <ginatorres@Ginas-MacBook-Pro.local>
2020-10-29 15:37:01 -0500

    Made Material-Iu changes

--- app/javascript/components/users/SignInComponent.tsx
+++ app/javascript/components/users/SignInComponent.tsx
@@ -1,12 +1,16 @@
 // License: LGPL-3.0-or-later
 import React, {useEffect, useState} from "react";
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import { createStyles, Theme, makeStyles, useTheme } from '@material-ui/core/styles';
 import {Formik, Form, Field} from 'formik';
 import Button from '@material-ui/core/Button';
 import noop from "lodash/noop";
 import usePrevious from 'react-use/esm/usePrevious';
-import './SignInStyles.css';
 import Typography from '@material-ui/core/Typography';
+import {spacing}  from '@material-ui/system';
+import MuiButton from "@material-ui/core/Button";
+import { styled } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+import { shadows } from '@material-ui/system';
 
 
 import { CardContent, Link } from '@material-ui/core';
@@ -17,9 +21,8 @@ import LockOpenIcon from '@material-ui/icons/LockOpen';
 import Input from '@material-ui/core/Input';
 import Card from '@material-ui/core/Card';
 import CardMedia from "@material-ui/core/CardMedia";
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import logo from './Images/logo.png';
+// import logo from './Images/logo.png';
 
 
 import useCurrentUserAuth from "../../hooks/useCurrentUserAuth";
@@ -27,6 +30,9 @@ import { SignInError } from "../../legacy_react/src/lib/api/errors";
 import { useIntl } from "../../components/intl";
 import * as yup from '../../common/yup';
 import { Email } from '../../legacy_react/src/lib/regex';
+import { autorun } from "mobx";
+import Box from '@material-ui/core/Box';
+import { FormatAlignCenter } from "@material-ui/icons";
 
 
 export interface SignInComponentProps {
@@ -80,97 +86,127 @@ function SignInComponent(props:SignInComponentProps) : JSX.Element {
 
 	const { formatMessage } = useIntl();
 
+	//Yup validation
 	const validationSchema= yup.object({
 		email: yup.string().required(),
 		password: yup.string()
 		  .required()
-    });
+	});
 
-	return (
-		<Formik initialValues={{email: ""}}  onSubmit={async (_values, formikHelpers) => {
-			try {
-				await signIn({email: 'email@ema.com', password: "password"});
+	//Styling 
+	const useStyles = makeStyles((theme: Theme) => createStyles ({
+		textField: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+			},
+		},
+		card: {
+			borderRadius: 15,
+			boxShadow: 'rgb(192,192,192) 0px 1px 6px, rgba(255, 0, 0, 0.117647) 0px 1px 4px',
+		 }
+		}),
+		);
+		
+	const Button = styled(MuiButton)(spacing);
+
+	const classes = useStyles();
+		
+		return (
+			<Formik initialValues={{email: ""}}  onSubmit={async (_values, formikHelpers) => {
+				try {
+					await signIn({email: 'email@ema.com', password: "password"});
+				}
+				catch (e:unknown) {
+					// NOTE: We're just swallowing the exception here for now. Might we need to do
+					// something different? Don't know!
+				}
+				finally {
+					formikHelpers.setSubmitting(false);
+				}
 			}
-			catch (e:unknown) {
-				// NOTE: We're just swallowing the exception here for now. Might we need to do
-				// something different? Don't know!
-			}
-			finally {
-				formikHelpers.setSubmitting(false);
-			}
-		}
-		}>{(props) => {
-				useEffect(() => {
-					setIsValid(props.isValid);
-				}, [props.isValid]);
+			}>{(props) => {
+					useEffect(() => {
+						setIsValid(props.isValid);
+					}, [props.isValid]);
 
-				return <Form>
-					{/* NOTE: if a Button should submit a form, mark it as type="submit". Otherwise pressing Enter won't submit form*/}
-					<Card 
-          className="card" 
-          variant="outlined" 
-          style={{ borderRadius: 25}} 
-          >
-				
-		<CardMedia 
-			className="media"
-        	image={logo}
-			title="Houdini"
-		/>
-
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							Login
-						</Typography>
-					</CardContent>
-					<br />
-					<br />
-
-					<InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel>
-						<Input
-						id="input-with-icon-adornment"
-						startAdornment={
-							<InputAdornment position="start">
-							<AccountCircle className="icon" />
-							</InputAdornment>
-						}
+			return <Form>
+						{/* NOTE: if a Button should submit a form, mark it as type="submit". Otherwise pressing Enter won't submit form*/}
+			<Grid container
+				direction="column"
+				alignItems="center"
+				justify="center"
+			>
+			
+				<Card classes={{ root: classes.card }}
+					variant="outlined"
+				>
+				<Box p={10}>
+					<Grid container
+						direction="column"
+						alignItems="center"
+						justify="center"
+					>
+							
+						<CardMedia 
+							// className="media"
+							// image={logo}
+							// title="Houdini"
 						/>
-					<br />
-          <br />
-          <InputLabel htmlFor="input-with-icon-adornment">Password</InputLabel>
-						<Input
-						id="input-with-icon-adornment"
-						startAdornment={
-							<InputAdornment position="start">
-							<LockOpenIcon className="icon"/>
-							</InputAdornment>
-						}
-						/>
-					<br />
+						<CardContent>
+							<Typography gutterBottom variant="h5" component="h2">
+								Login
+							</Typography>
+						</CardContent>
 
-					<br />
-					<br />
-				<div className="container">
-          <Button data-testid="signInButton" type="submit"
-						variant={'contained'}
-						color={'primary'}
-						style={{ borderRadius: 25}} 
-						>
-						{formatMessage({id: 'submit'})}
-          </Button>
-          <br />
-          <br />
-          <Link 
-						component="button"
-						variant="body2"
-						onClick={() => {
-							console.info("I'm a button.");
-						}}
+						<Box p={1.5}>
+							<InputLabel htmlFor="input-with-icon-adornment">Email</InputLabel>
+								<Input 
+									id="input-with-icon-adornment"
+									startAdornment={
+								<InputAdornment position="start">
+								<AccountCircle fontSize="small"/>
+								</InputAdornment>
+								}
+											/> 
+						</Box>
+						
+						<Box p={1.5}>
+							<InputLabel htmlFor="input-with-icon-adornment">Password</InputLabel>
+								<Input 
+									id="input-with-icon-adornment"
+									startAdornment={
+									<InputAdornment position="start">
+										<LockOpenIcon fontSize="small"/>
+									</InputAdornment>
+								}/>
+						</Box>
+						<br />
+						<Box>
+							<Button
+								data-testid="signInButton" 
+								type="submit"
+								variant={'contained'}
+								color={'primary'}
+							>
+							{formatMessage({id: 'submit'})}
+							</Button>
+						</Box>
+						<br />
+						<Link
+							component="button"
+							variant="body2"
+							onClick={() => {
+								console.info("I'm a button.");
+							}}
 						>
 						Forgot Password
-          </Link>
-        </div>
-        </Card>
+						</Link>
+					</Grid>
+				</Box>
+			</Card>
+		</Grid>
+		
 
 
 					{componentState === 'submitting' ? "" : <>
@@ -180,9 +216,9 @@ function SignInComponent(props:SignInComponentProps) : JSX.Element {
 					}
 				</Form>;
 			}}
-
 		</Formik>
-	);
+		);
+	
 }
 
 SignInComponent.defaultProps = {

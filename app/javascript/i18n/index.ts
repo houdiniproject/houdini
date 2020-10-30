ac54c135b8a25f3e703b62e8a338673881def0af
Eric Schultz <eric@commitchange.com>
2020-10-30 12:59:41 -0500

    Autogenerate Javascript locales on webpack and jest builds

--- app/javascript/i18n/index.ts
+++ app/javascript/i18n/index.ts
@@ -1,10 +1,9 @@
 
 require('./regenerate.js');
 import I18n from 'i18n-js';
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const translations = require('./translations.js').default as Record<string, any>;
+const locales = require('./locales').default;
 
-I18n.translations = translations;
+I18n.translations = locales;
 
 export default I18n;
--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,23 @@
 module.exports =  {
 	"testEnvironment": "jest-environment-jsdom-fifteen",
 	"testEnvironmentOptions": {
-		"enzymeAdapter": "react16"
+		"enzymeAdapter": "react16",
 	},
 	"globals": {
 		"ts-jest": {
-			"babelConfig": false
-		}
+			"babelConfig": false,
+		},
 	},
 	"setupFiles": [
-		"<rootDir>/setupTests.js"
+		"<rootDir>/setupTests.js",
 	],
 	"transform": {
-		"^.+\\.tsx?$": "ts-jest"
+		"^.+\\.tsx?$": "ts-jest",
 	},
 	"testPathIgnorePatterns": [
 		"/node_modules/",
 		"/config/webpack/test.js",
-		"/vendor/"
+		"/vendor/",
 	],
 	"testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
 	"moduleFileExtensions": [
@@ -26,9 +26,13 @@ module.exports =  {
 		"js",
 		"jsx",
 		"json",
-		"node"
+		"node",
 	],
 	"snapshotSerializers": [
-		"enzyme-to-json/serializer"
-	]
-}
+		"enzyme-to-json/serializer",
+	],
+	"moduleNameMapper": {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+		"\\.erb.(js|ts)": "<rootDir>/__mocks__/erbMock.js",
+	},
+};

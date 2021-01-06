a7b0cf6bac1de20f8d029ff2d44a4674d4558c7d
Eric Schultz <eric@commitchange.com>
2021-01-06 16:08:46 -0600

    DirectUploadsController only accepts confirmed users

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
-#
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
 class DirectUploadsController < ActiveStorage::DirectUploadsController
 	include Controllers::Nonprofit::Authorization
 	skip_before_action :verify_authenticity_token, only: [:create]
-	before_action  do
-		authenticate_user!("You must be logged in to use this", :json)
+	before_action  :authenticate_user_with_json!
+
+private
+	def authenticate_confirmed_user_with_json!
+		authenticate_confirmed_user!("You must be logged in to use this", :json)
 	end
 end

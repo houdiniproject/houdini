c5cc0038be807ea899369b7729be5d914e0c6592
Eric Schultz <eric@commitchange.com>
2021-01-06 15:42:24 -0600

    Turn off drawing ActiveStorage routes so we put protection on /direct_uploads

--- config/application.rb
+++ config/application.rb
@@ -106,6 +106,8 @@ module Commitchange
 
     config.action_mailer.default_options = {from: "Default Org Team <hi@defaultorg.com>"}
 
+    config.active_storage.draw_routes = false
+
     # this works around a bug where the the webpacker proxy
     # only waits 60 seconds for a compilation to happen. That's not 
     # fast enough on startup and Webpacker doesn't allow us to override.

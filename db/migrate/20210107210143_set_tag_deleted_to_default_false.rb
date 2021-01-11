a6f8b5feb158f425d3d948bf150bf51a85a42279
Eric Schultz <eric@commitchange.com>
2021-01-11 12:44:07 -0600

    Set TagMaster.deleted to false

---
+++ db/migrate/20210107210143_set_tag_deleted_to_default_false.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+class SetTagDeletedToDefaultFalse < ActiveRecord::Migration[6.1]
+  def change
+    change_column_default :tag_masters, :deleted, from: nil, to: false
+    change_column_null :tag_masters, :deleted, false
+  end
+end

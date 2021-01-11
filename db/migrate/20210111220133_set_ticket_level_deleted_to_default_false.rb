4466df6b0dae19c74fa6238703a7b1bb584cbd47
Eric Schultz <eric@commitchange.com>
2021-01-11 16:03:11 -0600

    Set ticket_levels.deleted default to false

---
+++ db/migrate/20210111220133_set_ticket_level_deleted_to_default_false.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+class SetTicketLevelDeletedToDefaultFalse < ActiveRecord::Migration[6.1]
+  def change
+    change_column_default :ticket_levels, :deleted, from: nil, to: false
+    change_column_null :ticket_levels, :deleted, false
+  end
+end

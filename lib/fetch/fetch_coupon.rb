fae212b842a73ffb628f2409f1768a65b404fa32
Eric Schultz <eric@commitchange.com>
2021-02-04 11:32:33 -0600

    Remove unused coupon class

--- lib/fetch/fetch_coupon.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
-# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
-module FetchCoupon
-  def self.page(params)
-    return params[:name].tr('-', '_') if params[:name]
-  end
-end

75fac928d619b6e9a2a86fe9be82e3a9efec6e44
Eric Schultz <eric@commitchange.com>
2021-02-09 13:26:13 -0600

    Replace all uses of db:structure:load

--- script/test.sh
+++ script/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yarn ci && rake db:create db:structure:load db:migrate && RAILS_ENV=test rake db:create db:structure:load test:prepare && rake spec && yarn run build-all && yarn jest
+yarn ci && rake db:create db:schema:load db:migrate && RAILS_ENV=test rake db:create db:schema:load test:prepare && rake spec && yarn run build-all && yarn jest

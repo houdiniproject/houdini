9264297a513757d5dca16f79690c295ee02edf1f
Eric Schultz <eric@commitchange.com>
2021-02-18 18:05:05 -0600

    Add super_admin? method to User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'super_admin?' do 
+		let(:super_admin) { 
+			sa = create(:user)
+
+			sa.roles.create(name: 'super_admin')
+			sa
+		}
+
+		let(:not_super_admin) {
+			sa = create(:user)
+			sa.roles.create(name: 'nonprofit_admin')
+			sa.roles.create(name: 'nonprofit_associate')
+			sa
+		}
+		
+		it 'returns true for super admin' do
+			expect(super_admin).to be_super_admin
+		end
+
+		it 'returns false for not super admin' do
+			expect(not_super_admin).to_not be_super_admin
+		end
+	end
+end

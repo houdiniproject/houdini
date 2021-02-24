8c47573501e4f8e0cd8225ada9205d00cf530604
Eric Schultz <eric@commitchange.com>
2021-02-24 12:27:48 -0600

    Rubocop controller concerns and specs

--- app/controllers/concerns/controllers/locale.rb
+++ app/controllers/concerns/controllers/locale.rb
@@ -2,19 +2,22 @@
 
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+# rubocop:disable Style/ConditionalAssignment
 module Controllers::Locale
-    extend ActiveSupport::Concern
+	extend ActiveSupport::Concern
 
-    included do 
-        before_action :set_locale
+	included do
+		before_action :set_locale
 
-        private 
-        def set_locale
-            if params[:locale] && Houdini.intl.available_locales.include?(params[:locale])
-              I18n.locale = params[:locale]
-            else
-              I18n.locale = Houdini.intl.language
-            end
-        end
-    end
+		private
+
+		def set_locale
+			if params[:locale] && Houdini.intl.available_locales.include?(params[:locale])
+				I18n.locale = params[:locale]
+			else
+				I18n.locale = Houdini.intl.language
+			end
+		end
+	end
 end
+# rubocop:enable all

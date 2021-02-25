7ce86e2c8834d86fa17fe98f7067326fbbd13e94
Eric Schultz <eric@commitchange.com>
2021-02-25 12:44:07 -0600

    Remove unused spec files

--- spec/support/date_time.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
-# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
-class DateTime
-	def nsec
-		(sec_fraction * 1_000_000_000).to_i
-	end
-end

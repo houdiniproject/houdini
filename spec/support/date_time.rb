4c8544b5cdee78ef425612c1170099103ae32730
Eric Schultz <eric@commitchange.com>
2021-02-25 12:40:50 -0600

    Rubocop spec/support/date_time.rb

--- spec/support/date_time.rb
+++ spec/support/date_time.rb
@@ -3,7 +3,7 @@
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
 class DateTime
-  def nsec
-    (sec_fraction * 1_000_000_000).to_i
-  end
+	def nsec
+		(sec_fraction * 1_000_000_000).to_i
+	end
 end

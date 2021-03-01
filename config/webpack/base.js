a8cf29b11e987a189d2e0da9dc8b92f97bb0c968
Eric Schultz <eric@commitchange.com>
2021-03-01 11:32:39 -0600

    Update base for TSChecker

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,3 +1,6 @@
 const { webpackConfig } = require('@rails/webpacker')
+const ForkTSCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-module.exports = webpackConfig
+module.exports = merge(webpackConfig, {
+  plugins: [new ForkTSCheckerWebpackPlugin()],
+});

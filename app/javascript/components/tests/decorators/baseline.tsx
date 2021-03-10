1fa6d70cb08ce54e1a87dacd991e5d8b0b209d1e
Eric Schultz <eric@commitchange.com>
2021-03-09 19:08:06 -0600

    Add CssBaseline decorator and Roboto to Storybook

---
+++ app/javascript/components/tests/decorators/baseline.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import type {DecoratorFn} from '@storybook/react';
+import { StoryFn } from '@storybook/addons';
+
+
+function decorator(story:StoryFn<JSX.Element>): JSX.Element {
+	return (<>
+		<CssBaseline/>
+		{story()}
+	</>);
+}
+
+export default function decorate(): DecoratorFn {
+	return decorator;
+}

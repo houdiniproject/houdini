a47ae90bfc7ccd1a7a1550b79aa9f1921cef6b98
Eric Schultz <eric@commitchange.com>
2021-03-10 14:47:11 -0600

    Add AnimatedCheckmark

---
+++ app/javascript/components/common/progress/AnimatedCheckmark.stories.tsx
@@ -0,0 +1,25 @@
+// License: LGPL-3.0-or-later
+import * as React from 'react';
+import AnimatedCheckmark, {AnimatedCheckmarkProps} from './AnimatedCheckmark';
+
+export default {
+	title: 'users/AnimatedCheckmark',
+	component: AnimatedCheckmark,
+};
+
+type TemplateArgs = AnimatedCheckmarkProps;
+
+const CheckmarkTemplate = (args:TemplateArgs) => {
+
+
+	return <AnimatedCheckmark
+		{...args}
+		key={Math.random() /* so it reloads everytime props change */}></AnimatedCheckmark>;
+};
+
+export const Checkmark = CheckmarkTemplate.bind({});
+Checkmark.args = {
+};
+
+
+

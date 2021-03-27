d655e17f3758b0f0d93bfa82cc73fd97708e8fe4
Clarissa Borges <clarissalimab08@gmail.com>
2021-03-27 18:07:31 -0300

    Removes redundant stripe card

--- lib/stripe.rb
+++
@@ -1,6 +0,0 @@
-# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-
-# monkey patch stripe cards and sources to simplify checking some infomation about the cards and sources
-require 'stripe/mixin'
-require 'stripe/card'
-require 'stripe/source'

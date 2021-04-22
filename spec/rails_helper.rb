508164157940014cf0de3b38b0d40fa90f09b5ee
Eric Schultz <eric@commitchange.com>
2021-04-22 17:06:28 -0500

    Remove the no longer used Devise::TestHelpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,6 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
   config.include RSpec::Rails::RequestExampleGroup, type: :request, file_path: /spec\/api/
 end

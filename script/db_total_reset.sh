0129b0f2ac63074c65e55ad7e3706eb2a42b1cab
Clarissa Borges <clarissalimab08@gmail.com>
2021-05-04 12:20:04 -0300

    Replaces 'commitchange' prefix with 'houdini' from databases

--- script/db_total_reset.sh
+++ script/db_total_reset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dropdb commitchange_development ; dropdb commitchange_test ; rake db:drop ; rake db:setup ; rake db:migrate ; script/pg_restore_local_from_production.sh
+dropdb houdini_development ; dropdb houdini_test ; rake db:drop ; rake db:setup ; rake db:migrate ; script/pg_restore_local_from_production.sh
 

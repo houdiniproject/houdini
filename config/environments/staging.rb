5dbf8edccb2fde5d17b7900f4cf25341b9addda2
Clarissa Borges <clarissalimab08@gmail.com>
2021-05-05 10:35:00 -0300

    Removes font_assets from staging config file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,7 +16,7 @@ Commitchange::Application.configure do
 	config.action_controller.perform_caching = true
 
 	# Disable Rails's static asset server (Apache or nginx will already do this)
-	config.serve_static_assets = true
+	config.serve_static_files = true
 
 	# Compress JavaScripts and CSS
 	config.assets.compress = true
@@ -54,7 +54,6 @@ Commitchange::Application.configure do
 	cdn_url = cdn_url.to_s
 	config.action_controller.asset_host = cdn_url
 	config.action_mailer.asset_host = cdn_url
-	config.font_assets.origin = '*'
 
 	# Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
 

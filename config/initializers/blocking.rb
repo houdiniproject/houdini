9b95c0d45f9f3ad369c7ac3a611c186db24725de
Eric Schultz <eric@commitchange.com>
2021-05-05 11:25:19 -0500

    Correct the method name of Rack::Attack.blocklist

--- config/initializers/blocking.rb
+++ config/initializers/blocking.rb
@@ -1,6 +1,6 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 
-Rack::Attack.blacklist("block all icky method requests") do |request|
+Rack::Attack.blocklist("block all icky method requests") do |request|
   !(request.get? ||
       request.head? ||
       request.post? ||

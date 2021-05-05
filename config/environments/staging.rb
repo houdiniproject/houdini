896754774a6589ea3be5a9dad5239e9d1cd3e700
Clarissa Borges <clarissalimab08@gmail.com>
2021-05-05 11:03:20 -0300

    Keeping config.serve_static_assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,7 +16,7 @@ Commitchange::Application.configure do
 	config.action_controller.perform_caching = true
 
 	# Disable Rails's static asset server (Apache or nginx will already do this)
-	config.serve_static_files = true
+	config.serve_static_assets = true
 
 	# Compress JavaScripts and CSS
 	config.assets.compress = true
@@ -71,7 +71,6 @@ Commitchange::Application.configure do
 		end
 	end
 
-
 	# Enable locale fallbacks for I18n (makes lookups for any locale fall back to
 	# the I18n.default_locale when a translation can not be found)
 	config.i18n.fallbacks = true
@@ -85,6 +84,7 @@ Commitchange::Application.configure do
 	config.dependency_loading = true if $rails_rake_task
 	# Compress json
 	# config.middleware.use Rack::Deflater
+
 	config.middleware.insert_before 'Rack::Cache', Rack::Attack
 	NONPROFIT_VERIFICATION_SEND_EMAIL_DELAY = 5.minutes
 end

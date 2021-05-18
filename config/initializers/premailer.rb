5e2c8a14df5c9fa79850e3bb018cf58d6373d3dd
Eric Schultz <eric@commitchange.com>
2021-05-18 14:48:10 -0500

    Add premailer for inlining email CSS

---
+++ config/initializers/premailer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+
+# We want to keep any style attributes one elements already defined in the mailer html.
+Premailer::Rails.config.merge!(preserve_style_attribute: true)

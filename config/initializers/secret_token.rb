f0247f1078433ab7b161480678c195a6980528d3
Eric Schultz <eric@commitchange.com>
2021-05-24 13:36:03 -0500

    Add Secret Key Base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -6,3 +6,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Commitchange::Application.config.secret_token = ENV.fetch('SECRET_TOKEN')
+
+Commitchange::Application.config.secret_key_base = ENV.fetch('SECRET_KEY_BASE')

86408bada2cff4e9e7ff8213c21d81cdf4ba01b8
Clarissa Borges <clarissa@commitchange.com>
2021-05-24 21:21:43 -0300

    Fixes removing header image

--- client/js/nonprofits/edit/page.js
+++ client/js/nonprofits/edit/page.js
@@ -3,6 +3,9 @@ require('../../common/image_uploader')
 require('../../common/on-change-sanitize-slug')
 var url = "/nonprofits/" + app.nonprofit_id
 
-appl.def('remove_this_image', function() {
-	appl.remove_background_image(url, 'nonprofit')
+appl.def('remove_background_image', function () {
+	var url = '/nonprofits/' + app.nonprofit_id
+	var notification = 'Removing background image...'
+	var payload = { remove_background_image : true }
+	appl.remove_image(url, 'nonprofit', notification, payload)
 })

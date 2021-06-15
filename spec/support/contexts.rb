9a58b9f68461ee230ac4b3a22c79a2698c6c1fa6
Eric Schultz <eric@commitchange.com>
2021-06-15 14:06:28 -0500

    Automagically load all the context files for spec testing

--- spec/support/contexts.rb
+++ spec/support/contexts.rb
@@ -2,5 +2,7 @@
 
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
-require 'support/contexts/shared_donation_charge_context'
-require 'support/contexts/shared_rd_donation_value_context'
+
+ Dir["#{File.dirname (__FILE__)}/contexts/*"].each do |file|
+  require_relative "./contexts/#{File.basename(file, ".rb")}" 
+end

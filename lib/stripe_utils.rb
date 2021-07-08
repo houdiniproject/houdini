53e38a6eef62799dc1e9018b54dec855ba9acc68
Eric Schultz <eric@commitchange.com>
2021-07-08 13:11:33 -0500

    Remove unused StripeUtils.create_refund

--- lib/stripe_utils.rb
+++ lib/stripe_utils.rb
@@ -1,6 +1,5 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 require 'stripe'
-require 'calculate/calculate_fees'
 
 module StripeUtils
 
@@ -10,16 +9,6 @@ module StripeUtils
 			currency: currency || Settings.intntl.currencies[0]
 		}, {
 			stripe_account: stripe_account_id
-		})
-	end
-
-
-	def self.create_refund(stripe_charge, amount, reason)
-		stripe_charge.refunds.create({
-			amount: amount,
-			refund_application_fee: true,
-			reverse_transfer: true,
-			reason: reason
 		})
 	end
 end

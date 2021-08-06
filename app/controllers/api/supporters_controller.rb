4cc7a28de6eae379c1511155de936ab82606000b
Eric Schultz <eric@commitchange.com>
2021-08-06 12:32:14 -0500

    Add api/nonprofits/:nonprofit_id/supporters and api/nonprofits/:nonprofit_id/supporters/:id

---
+++ app/controllers/api/supporters_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+
+# A controller for interacting with a nonprofit's supporters
+class Api::SupportersController < Api::ApiController
+	include Controllers::Nonprofit::Current
+	include Controllers::Nonprofit::Authorization
+	before_action :authenticate_nonprofit_user!
+
+	# Gets the nonprofits supporters
+	# If not logged in, causes a 401 error
+	def index
+		@supporters = current_nonprofit.supporters
+	end
+
+	# Gets the a single nonprofit supporter
+	# If not logged in, causes a 401 error
+	def show
+		@supporter = current_nonprofit.supporters.find(params[:id])
+	end
+end

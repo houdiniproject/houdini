e1d83a1b21395b47a851f80e42175751f2d393b1
Eric Schultz <eric@commitchange.com>
2021-08-12 16:25:31 -0500

    Add /api/nonprofits/:nonprofit_id/supporters/:supporter_id/:supporter_id/supporter_notes and ./:id

---
+++ app/controllers/api/supporter_notes_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+
+# A controller for interacting with a nonprofit's supporters
+class Api::SupporterNotesController < Api::ApiController
+	include Controllers::Nonprofit::Current
+	include Controllers::Nonprofit::Authorization
+	before_action :authenticate_nonprofit_user!
+
+	# Gets the nonprofits supporters
+	# If not logged in, causes a 401 error
+	def index
+		@supporter_notes = current_nonprofit.supporters.find(params[:supporter_id]).supporter_notes
+	end
+
+	# Gets the a single nonprofit supporter
+	# If not logged in, causes a 401 error
+	def show
+		@supporter_note = current_nonprofit.supporters.find(params[:supporter_id]).supporter_notes.find(params[:id])
+	end
+end
---
+++ spec/views/api/supporter_notes/show.json.jbuilder_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+require 'rails_helper'
+
+RSpec.describe '/api/supporter_notes/show.json.jbuilder', type: :view do
+	subject(:json) do
+		assign(:supporter_note, supporter_note)
+		render
+		JSON.parse(rendered)
+	end
+
+	let(:supporter) { supporter_note.supporter }
+	let(:nonprofit) { supporter.nonprofit }
+	let(:user) { supporter_note.user }
+
+	let(:supporter_note) { create(:supporter_note_with_fv_poverty_with_user) }
+
+	it {
+		is_expected.to include('object' => 'supporter_note')
+	}
+
+	it {
+		is_expected.to include('id' => supporter_note.id)
+	}
+
+	it {
+		is_expected.to include('content' => 'Some content in our note')
+	}
+
+	it {
+		is_expected.to include('nonprofit' => nonprofit.id)
+	}
+
+	it {
+		is_expected.to include('deleted' => false)
+	}
+
+	it {
+		is_expected.to include('supporter' => supporter.id)
+	}
+
+	it {
+		is_expected.to include('user' => user.id)
+	}
+
+	it {
+		is_expected.to include('url' =>
+			a_string_matching(
+				%r{http://test\.host/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes/#{supporter_note.id}}
+			))
+	}
+end

b6348e4513dfaddb40ee45bff9225cc0c6fb22d1
Eric Schultz <eric@commitchange.com>
2021-08-26 18:53:43 -0500

    Add /api/nonprofits/:nonprofit_id/events/:id

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -14,4 +14,20 @@ FactoryBot.define do
     nonprofit_id { 55352 }
     profile
   end
+
+  factory :fv_poverty_fighting_event, class: 'Event' do
+    name { "Fighting Poverty 2021"}
+    
+    start_datetime { DateTime.new(2025, 5, 11, 4, 5, 6) }
+    end_datetime { DateTime.new(2025, 5, 11, 5, 1, 7) }
+    address { '100 N Appleton St' }
+    city { 'Appleton' }
+    state_code { 'WI' }
+    slug { 'fighting-poverty' }
+
+    factory :fv_poverty_fighting_event_with_nonprofit_and_profile do
+      nonprofit { create(:fv_poverty) }
+      profile
+    end
+  end
 end

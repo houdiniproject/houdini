018b77f51890456ff74dc23d2fb5571d63412686
Clarissa Borges <clarissa@commitchange.com>
2021-09-10 17:18:45 -0300

    Adds tests to useIsReady hook

--- app/javascript/hooks/users/useIsReady.ts
+++ app/javascript/hooks/users/useIsReady.ts
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { NetworkError } from "../../api/errors";
 
-export default function useIsReady(wasSubmitting: boolean, onFailure: (error: NetworkError) => void, failed: boolean, lastSignInAttemptError: NetworkError): boolean {
+export default function useIsReady(wasSubmitting: boolean, onFailure: (error: NetworkError) => void, failed: boolean, lastSignInAttemptError: NetworkError, submitting: boolean): boolean {
   const [state, setState] = useState(true);
 
   useEffect(() => {
     if (failed && wasSubmitting) {
       setState(true);
       onFailure(lastSignInAttemptError);
+    } else if (submitting) {
+      setState(false);
     }
-  }, [failed, wasSubmitting, lastSignInAttemptError, onFailure, setState]);
+    // when should it not be ready?
+  }, [failed, wasSubmitting, lastSignInAttemptError, onFailure, submitting, setState]);
 
   return state;
 }

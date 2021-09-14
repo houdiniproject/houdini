1d16b1e42043a35cf39c8cbfc628c11fcd4df703
Eric Schultz <eric@commitchange.com>
2021-09-14 13:48:22 -0500

    Add #supporter_cards to Nonprofit

--- app/models/nonprofit.rb
+++ app/models/nonprofit.rb
@@ -72,6 +72,7 @@ class Nonprofit < ActiveRecord::Base
   has_many :imports
   has_many :email_settings
   has_many :cards, as: :holder
+  has_many :supporter_cards, through: :supporters, source: :cards, class_name: 'Card'
 
   has_one :bank_account, -> { where("COALESCE(bank_accounts.deleted, false) = false") }, dependent: :destroy
   has_one :billing_subscription, dependent: :destroy

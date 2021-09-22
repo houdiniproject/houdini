e7861bd90c535b47e8cbb9d1c8a485b640a9a74f
Eric Schultz <eric@commitchange.com>
2021-09-22 14:23:47 -0500

    Paging for /api/nonprofit/:nonprofit_id/supporters/:id/supporter_notes

--- spec/requests/api/supporter_notes_controller_request_spec.rb
+++ spec/requests/api/supporter_notes_controller_request_spec.rb
@@ -22,58 +22,128 @@ RSpec.describe Api::SupporterNotesController, type: :request do
 				JSON.parse(response.body)
 			end
 
-			before do
-				sign_in user
-				get "/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes"
-			end
-
-			it {
-				expect(response).to have_http_status(:success)
-			}
-
-			it {
-				expect(json.count).to eq 1
-			}
-
-			context 'with first item' do
-				subject do
-					json[0]
+			describe 'with a response' do
+				before do
+					sign_in user
+					get "/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes"
 				end
 
 				it {
-					is_expected.to include('object' => 'supporter_note')
+					expect(response).to have_http_status(:success)
 				}
 
 				it {
-					is_expected.to include('id' => supporter_note.id)
+					is_expected.to include('data' => have_attributes(count: 1))
 				}
 
-				it {
-					is_expected.to include('content' => 'Some content in our note')
-				}
+				context 'with first item' do
+					subject do
+						json['data'][0]
+					end
 
-				it {
-					is_expected.to include('nonprofit' => nonprofit.id)
-				}
+					it {
+						is_expected.to include('object' => 'supporter_note')
+					}
 
-				it {
-					is_expected.to include('deleted' => false)
-				}
+					it {
+						is_expected.to include('id' => supporter_note.id)
+					}
 
-				it {
-					is_expected.to include('supporter' => supporter.id)
-				}
+					it {
+						is_expected.to include('content' => 'Some content in our note')
+					}
 
-				it {
-					is_expected.to include('user' => user.id)
-				}
+					it {
+						is_expected.to include('nonprofit' => nonprofit.id)
+					}
 
-				it {
-					is_expected.to include('url' =>
-						a_string_matching(
-							%r{http://www\.example\.com/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes/#{supporter_note.id}} # rubocop:disable Layout/LineLength
-						))
-				}
+					it {
+						is_expected.to include('deleted' => false)
+					}
+
+					it {
+						is_expected.to include('supporter' => supporter.id)
+					}
+
+					it {
+						is_expected.to include('user' => user.id)
+					}
+
+					it {
+						is_expected.to include('url' =>
+							a_string_matching(
+								%r{http://www\.example\.com/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes/#{supporter_note.id}} # rubocop:disable Layout/LineLength
+							))
+					}
+				end
+			end
+
+			context 'when paging' do
+				before do
+					(0..5).each do |i|
+						create(:supporter_note_with_fv_poverty_with_user, supporter: supporter, content: "content for #{i}")
+					end
+					sign_in user
+				end
+
+				context 'when on page 0' do
+					subject(:json) do
+						JSON.parse(response.body)
+					end
+
+					before do
+						get "/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes", params: { page: 0, per: 5 }
+					end
+
+					it { is_expected.to include('data' => have_attributes(count: 5)) }
+					it { is_expected.to include('first_page' => true) }
+					it { is_expected.to include('last_page' =>  false) }
+					it { is_expected.to include('current_page' => 1) }
+					it { is_expected.to include('requested_size' => 5) }
+					it { is_expected.to include('total_count' => 7) }
+				end
+
+				context 'when on page 1' do
+					subject(:json) do
+						JSON.parse(response.body)
+					end
+
+					before do
+						get "/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes", params: { page: 1, per: 5 }
+					end
+
+					it { is_expected.to include('data' => have_attributes(count: 5)) }
+					it { is_expected.to include('first_page' => true) }
+					it { is_expected.to include('last_page' =>  false) }
+					it { is_expected.to include('current_page' => 1) }
+					it { is_expected.to include('requested_size' => 5) }
+					it { is_expected.to include('total_count' => 7) }
+
+					it {
+						expect(json['data'].map { |i| i['id'] }).to eq SupporterNote.order('id DESC').limit(5).pluck(:id)
+					}
+				end
+
+				context 'when on page 2' do
+					subject(:json) do
+						JSON.parse(response.body)
+					end
+
+					before do
+						get "/api/nonprofits/#{nonprofit.id}/supporters/#{supporter.id}/supporter_notes", params: { page: 2, per: 5 }
+					end
+
+					it { is_expected.to include('data' => have_attributes(count: 2)) }
+					it { is_expected.to include('first_page' => false) }
+					it { is_expected.to include('last_page' =>  true) }
+					it { is_expected.to include('current_page' => 2) }
+					it { is_expected.to include('requested_size' => 5) }
+					it { is_expected.to include('total_count' => 7) }
+
+					it {
+						expect(json['data'].map { |i| i['id'] }).to eq SupporterNote.order('id DESC').limit(2).offset(5).pluck(:id)
+					}
+				end
 			end
 		end
 

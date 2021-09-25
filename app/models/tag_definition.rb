1d8d01082310551d2b32ba9acf534682c2268cb3
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-09-25 14:40:02 -0300

    Change all ocurrences of "tag master" to "tag definition"
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>

--- app/models/tag_definition.rb
+++ app/models/tag_definition.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
-# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
-class TagMaster < ApplicationRecord
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/master/LICENSE
+class TagDefinition < ApplicationRecord
   include Model::Eventable
   include Model::Jbuilder
 
@@ -29,7 +29,7 @@ class TagMaster < ApplicationRecord
   def no_dupes
     return self if nonprofit.nil?
 
-    errors.add(:base, 'Duplicate tag') if nonprofit.tag_masters.not_deleted.where(name: name).any?
+    errors.add(:base, 'Duplicate tag') if nonprofit.tag_definitions.not_deleted.where(name: name).any?
   end
 
 
@@ -40,7 +40,7 @@ class TagMaster < ApplicationRecord
       save!
     end
   end
-  
+
   def to_builder(*expand)
     init_builder(*expand) do |json|
       json.(self, :name, :deleted)

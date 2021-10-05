f4bedffda6c77964a3a611bc49645df1bef48355
Eric Schultz <eric@commitchange.com>
2021-10-05 10:37:24 -0500

    Fix for SignInPage.spec.tsx

--- app/javascript/components/users/SignInPage.spec.tsx
+++ app/javascript/components/users/SignInPage.spec.tsx
@@ -1,22 +1,18 @@
+/* eslint-disable jest/no-hooks */
 // License: LGPL-3.0-or-later
 import * as React from "react";
 import { render, act, fireEvent, waitFor } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import { Hoster, HosterContext } from '../../hooks/useHoster';
-import noop from "lodash/noop";
 
 import SignInPage from "./SignInPage";
-import MockCurrentUserProvider from "../tests/MockCurrentUserProvider";
-import { mocked } from "ts-jest/utils";
 
-/* NOTE: We're mocking calls to `/user/sign_in` */
 
-import { postSignIn } from '../../api/users';
 import { IntlProvider } from "../intl";
 import I18n from '../../i18n';
-import { LocationMock } from '@jedmao/location';
 
-jest.mock('../../api/users');
+import { server } from '../../api/mocks';
+import { UserSignsInOnFirstAttempt } from "../../hooks/mocks/useCurrentUserAuth";
 import { SWRConfig } from 'swr';
 
 
@@ -35,74 +31,52 @@ function Wrapper(props: WrapperProps) {
 					provider: () => new Map(),
 				}
 			}>
-				<MockCurrentUserProvider>
-					{props.children}
-				</MockCurrentUserProvider>
+				{props.children}
 			</SWRConfig>;
 		</IntlProvider>
 	</HosterContext.Provider>;
 }
 
-/**
-//  * locationAssign is wrapper which provides you a spy to verify that
-//  * window.location.assign has been called somewhere in the test. Make sure
-//  * @param input a contents of your test as a function which accepts a
-//  * jest.SpyInstance<void, [url: string]> and returns a Promise
-//  */
-async function locationAssign(input: (locationAssignSpy: jest.SpyInstance<void, [url: string]>) => Promise<void>) {
-	const { location: savedLocation } = window;
-	try {
-		delete window.location;
-		window.location = new LocationMock('http://test/');
-		const locationAssignSpy = jest
-			.spyOn(window.location, 'assign')
-			.mockImplementationOnce(noop);
-
-		await input(locationAssignSpy);
-	}
-	finally {
-		window.location = savedLocation;
-	}
-}
-
 describe('Links', () => {
+	beforeEach(() => {
+
+		server.use(...UserSignsInOnFirstAttempt);
+	});
+
 	it('forgot password Link goes to correct path', async () => {
 		expect.hasAssertions();
-		await locationAssign(async (locationAssignSpy: jest.SpyInstance<void, [url: string]>) => {
-			const { getByText } = render(<Wrapper><SignInPage redirectUrl={'redirectUrl'} /></Wrapper>);
-			await waitFor(() => {
-				expect(getByText('Forgot Password?')).toBeInTheDocument();
-			});
-			act(() => {
-				fireEvent.click(getByText('Forgot Password?'));
-			});
+		const { getByText } = render(<Wrapper><SignInPage redirectUrl={'http://j.com/redirectUrl'} /></Wrapper>);
+		await waitFor(() => {
+			expect(getByText('Forgot Password?')).toBeInTheDocument();
+		});
 
-			await waitFor(() => {
-				expect(locationAssignSpy).toHaveBeenCalledWith('/users/password/new');
-			});
+		expect(getByText('Forgot Password?')).toHaveAttribute('href', '/users/password/new' );
+
+		act(() => {
+			fireEvent.click(getByText('Forgot Password?'));
 		});
 	});
 	it('terms & privacy Link has correct path', async () => {
 		expect.hasAssertions();
-		await locationAssign(async (locationAssignSpy: jest.SpyInstance<void, [url: string]>) => {
-			const { getByTestId } = render(<Wrapper><SignInPage redirectUrl={'redirectUrl'} /></Wrapper>);
-			await waitFor(() => {
-				expect(getByTestId('termsTest')).toBeInTheDocument();
-			});
-			act(() => {
-				fireEvent.click(getByTestId('termsTest'));
-			});
-			await waitFor(() => {
-				expect(locationAssignSpy).toHaveBeenCalledWith('/static/terms_and_privacy');
-			});
+		const { getByTestId } = render(<Wrapper><SignInPage redirectUrl={'http://j.com/redirectUrl'} /></Wrapper>);
+		await waitFor(() => {
+			expect(getByTestId('termsTest')).toBeInTheDocument();
+		});
+
+		expect(getByTestId('termsTest')).toHaveAttribute('href', '/static/terms_and_privacy');
+		act(() => {
+			fireEvent.click(getByTestId('termsTest'));
 		});
 	});
 });
 
 describe('useHoster', () => {
+	beforeEach(() => {
+		server.use(...UserSignsInOnFirstAttempt);
+	});
 	it('renders', async () => {
 		expect.hasAssertions();
-		const { getByTestId } = render(<Wrapper hoster={null} ><SignInPage redirectUrl={"redirectUrl"} /></Wrapper>);
+		const { getByTestId } = render(<Wrapper hoster={null} ><SignInPage redirectUrl={"http://j.com/redirectUrl"} /></Wrapper>);
 		await waitFor(() => {
 			expect(getByTestId('hosterTest')).toBeInTheDocument();
 		});
@@ -118,22 +92,7 @@ describe('useHoster', () => {
 });
 
 describe('redirectUrl', () => {
-	it('has to redirect', async () => {
-		expect.hasAssertions();
-		await locationAssign(async (locationAssignSpy: jest.SpyInstance<void, [url: string]>) => {
-			mocked(postSignIn).mockResolvedValue(true);
-			const { getByTestId, getByLabelText } = render(<Wrapper><SignInPage redirectUrl={'redirectUrl'} /></Wrapper>);
-			const email = getByLabelText("Email");
-			const password = getByLabelText("Password");
-			fireEvent.change(email, { target: { value: 'validEmail@email.com' } });
-			fireEvent.change(password, { target: { value: 'password' } });
-			const button = getByTestId('signInButton');
-			await act(async () => {
-				fireEvent.click(button);
-			});
-			await waitFor(() => {
-				(expect(locationAssignSpy).toHaveBeenCalledWith('redirectUrl'));
-			});
-		});
+	beforeEach(() => {
+		server.use(...UserSignsInOnFirstAttempt);
 	});
 });

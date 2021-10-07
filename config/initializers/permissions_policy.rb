3d8d1fae3824cd9088d52e65de0a49358facb217
lucasgandace <lucasgandac@hotmail.com>
2021-10-06 22:12:24 -0300

    cleaning unused function

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -2,10 +2,4 @@
 # information see https://developers.google.com/web/updates/2018/06/feature-policy
 #
 # Rails.application.config.permissions_policy do |f|
-#   f.camera      :none
-#   f.gyroscope   :none
-#   f.microphone  :none
-#   f.usb         :none
-#   f.fullscreen  :self
-#   f.payment     :self, "https://secure.example.com"
 # end

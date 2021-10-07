68f8fd850119ee5bff09a72bfe79d951198def16
lucasgandace <lucasgandac@hotmail.com>
2021-10-06 22:22:21 -0300

    removing old function

--- app/javascript/components/tests/intl/index.tsx
+++ app/javascript/components/tests/intl/index.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import addons from '@storybook/addons';
 import omit  from 'lodash/omit';

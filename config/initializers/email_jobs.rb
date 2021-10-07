3058eb8662bab57a3a4258b56df3a6fe14fe80c5
lucasgandace <lucasgandac@hotmail.com>
2021-10-06 21:54:02 -0300

    removing old unused variable

--- config/initializers/email_jobs.rb
+++ config/initializers/email_jobs.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
 MAX_EMAIL_JOB_ATTEMPTS = Rails.env == 'production' ? 50 : 2

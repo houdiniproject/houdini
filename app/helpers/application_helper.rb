d6c7819fd21ae315fa2b146388a45a71d789e6b2
Heron Rodrigues <heronrs1@gmail.com>
2021-10-18 23:57:54 -0300

    Add comments and examples to describe methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,6 +28,7 @@ module ApplicationHelper
 
   ## Dates
 
+  # Format date to m/d/Y. Ex: 10/18/2021
   def format_date_to_mdY(date_object, timezone = nil)
     return '' if date_object.nil?
 
@@ -42,10 +43,12 @@ module ApplicationHelper
     time_object.strftime('%l:%M%P')
   end
 
+  # Format date with complete month name. Ex: "October 18, 2021"
   def format_date_with_month_name(date_object)
     date_object.strftime('%B %d, %Y')
   end
 
+  # Format date to datetime with timezone. Ex: 10/18/2021 11:00PM (+03:00)
   def format_date_to_datetime_timezone(date_object, timezone = nil)
     date_object = date_object.in_time_zone(timezone) if timezone
     date_object.strftime('%m/%d/%Y %I:%M%P (%Z)')
@@ -55,6 +58,7 @@ module ApplicationHelper
     [%w[Alabama AL], %w[Alaska AK], %w[Arizona AZ], %w[Arkansas AR], %w[California CA], %w[Colorado CO], %w[Connecticut CT], %w[Delaware DE], ['District of Columbia', 'DC'], %w[Florida FL], %w[Georgia GA], %w[Hawaii HI], %w[Idaho ID], %w[Illinois IL], %w[Indiana IN], %w[Iowa IA], %w[Kansas KS], %w[Kentucky KY], %w[Louisiana LA], %w[Maine ME], %w[Maryland MD], %w[Massachusetts MA], %w[Michigan MI], %w[Minnesota MN], %w[Mississippi MS], %w[Missouri MO], %w[Montana MT], %w[Nebraska NE], %w[Nevada NV], ['New Hampshire', 'NH'], ['New Jersey', 'NJ'], ['New Mexico', 'NM'], ['New York', 'NY'], ['North Carolina', 'NC'], ['North Dakota', 'ND'], %w[Ohio OH], %w[Oklahoma OK], %w[Oregon OR], %w[Pennsylvania PA], ['Puerto Rico', 'PR'], ['Rhode Island', 'RI'], ['South Carolina', 'SC'], ['South Dakota', 'SD'], %w[Tennessee TN], %w[Texas TX], %w[Utah UT], %w[Vermont VT], %w[Virginia VA], %w[Washington WA], ['West Virginia', 'WV'], %w[Wisconsin WI], %w[Wyoming WY]]
   end
 
+  # Append a parameter to a URL string
   def append_param_to_url(param, val, url)
     url + (url.include?('?') ? '&' : '?') + param + '=' + val
   end

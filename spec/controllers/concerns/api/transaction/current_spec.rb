e9848348a3817793410179df47a3ee84b5cc5b09
Eric Schultz <eric@commitchange.com>
2021-11-03 13:57:52 -0500

    Rename the id param in APIs to transaction_id and create a `current_transaction` concern

---
+++ spec/controllers/concerns/api/transaction/current_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
+require 'rails_helper'
+
+describe 'Controllers::Api::Transaction::Current', type: :controller do
+	let(:transaction) { force_create(:transaction_for_donation) }
+	let(:nonprofit) { transaction.nonprofit }
+
+	controller(Api::ApiController) do
+		include Controllers::Api::Transaction::Current
+
+		def index
+			render json: {
+				transaction: current_transaction.id
+			}
+		end
+	end
+
+	it 'gets transaction if found' do
+		get :index, params: { nonprofit_id: nonprofit.id, transaction_id: transaction.id }
+		expect(JSON.parse(response.body)).to eq(
+			{
+				'transaction' => transaction.id
+			}
+		)
+	end
+
+	it 'throw RecordNotFound if not found' do
+		expect do
+			get :index, params: { nonprofit_id: nonprofit.id, transaction_id: 124_124_905 }
+		end.to raise_error(ActiveRecord::RecordNotFound)
+	end
+end

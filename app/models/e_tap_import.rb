dd616071f7265b13b5e52e77920c21e1bd408e1f
Eric Schultz <eric@commitchange.com>
2021-12-08 16:11:38 -0600

    ETap Import

---
+++ app/models/e_tap_import.rb
@@ -0,0 +1,37 @@
+class ETapImport < ActiveRecord::Base
+
+  attr_accessible :nonprofit
+  belongs_to :nonprofit
+  has_many :e_tap_import_journal_entries do 
+    def create_from_csv(filename)
+      CSV.read(filename, headers:true).each do |row|
+        create(row: row.to_h)
+      end
+    end
+  end
+  has_many :e_tap_import_contacts do 
+    def create_from_csv(filename)
+      CSV.read(filename, headers:true).each do |row|
+        create(row: row.to_h)
+      end
+    end
+  end
+
+  def self.create_import(nonprofit, journal_file, contacts_file)
+
+    transaction do
+      e_tap_import = create(nonprofit:nonprofit)
+      e_tap_import.e_tap_import_contacts.create_from_csv(contacts_file)
+      e_tap_import.e_tap_import_journal_entries.create_from_csv(journal_file)
+    end
+  end
+
+
+  def process(user)
+    e_tap_import_journal_entries.order('e_tap_import_journal_entries.id ASC').each do |entry|
+      if entry.unprocessed?
+        entry.to_wrapper.process(user)
+      end
+    end
+  end
+end
---
+++ spec/factories/e_tap_import_journal_entries.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :e_tap_import_journal_entry do
+    
+  end
+end

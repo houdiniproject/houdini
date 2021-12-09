e48f35234ca026e62fcb516d3a4c5e3c43728f42
Eric Schultz <eric@commitchange.com>
2021-12-09 15:54:58 -0600

    Update msw-storybook-addon to 1.5.0

--- .storybook/react/preview.js
+++ .storybook/react/preview.js
@@ -3,9 +3,11 @@ import intlDecorate from '../../app/javascript/components/tests/intl';
 import cssBaseline from '../../app/javascript/components/tests/decorators/baseline'
 import clearSessionStorage from '../../app/javascript/components/tests/decorators/clearSessionStorage'
 import '../../app/javascript/components/tests/decorators/roboto'
-import { initializeWorker, mswDecorator } from 'msw-storybook-addon';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 
-initializeWorker();
+initialize({
+	onUnhandledRequest: 'bypass' // we need this because otherwise HMR and other parts of Storybook loading wouldn't work
+});
 
 
-export const decorators = [ clearSessionStorage(), mswDecorator(), intlDecorate(), cssBaseline()]
+export const decorators = [ clearSessionStorage(), mswDecorator, intlDecorate(), cssBaseline()]

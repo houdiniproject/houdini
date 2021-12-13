9cc8107a2a6ab828994036a15d36f9e23996f2ce
Eric Schultz <eric@commitchange.com>
2021-12-13 11:11:26 -0600

    Update to React 17

--- app/javascript/hooks/useIsReadyForSubmission.ts
+++
@@ -1,4 +0,0 @@
-// License: LGPL-3.0-or-later
-export default function useIsReadyForSubmission(isValid: boolean, showProgressAndSuccess: boolean, formState: string): boolean {
-	return !(!isValid || !showProgressAndSuccess && !(formState === 'canSubmit'));
-}

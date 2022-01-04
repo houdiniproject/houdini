0bacdcf39c19b27a3c105fba84eeb4a85c21bdd6
Eric Schultz <eric@commitchange.com>
2022-01-04 16:47:03 -0600

    Remove empty QueryCharges

--- lib/query/query_charges.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
-# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
-require 'psql'
-module QueryCharges
-end

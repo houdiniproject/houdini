6d08ea31b41805b2683ff2ba0fcbe14f3c74a34c
Eric Schultz <eric@commitchange.com>
2022-01-04 16:46:32 -0600

    Remove unused htp.rb file

--- lib/htp.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
-# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
-# Hamster Table Print
-
-def htp(data)
-  tp Hamster.to_ruby(data)
-end

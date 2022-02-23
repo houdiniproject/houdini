0e315afe1ea351a0bc8f5a768bfc752dc2bedfd2
KalebeLopes <calbkalebe@gmail.com>
2022-02-22 21:45:56 -0300

    feat: create router for roles nonprofit
    Co-authored-by: alan-ms <alan.sousa@protonmail.com>
    Co-authored-by: JoaoVitorFarias <jvlopesfarias@gmail.com>

---
+++ app/controllers/api/roles_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
+class Api::RolesController < Api::ApiController
+	include Controllers::Nonprofit::Current
+	include Controllers::Nonprofit::Authorization
+
+	# before_action :authenticate_nonprofit_user!, except: %i[new create]
+
+	# get /nonprofit/:nonprofit_id/roles
+	def index
+		@roles = current_nonprofit.roles
+	end
+end

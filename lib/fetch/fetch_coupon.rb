6f096b4110cce089c8f5b9f64a1e2803d1ce2444
Eric Schultz <eric@wwahammy.com>
2022-03-10 14:01:06 -0600

    Remove unused Coupon

--- lib/fetch/fetch_coupon.rb
+++
@@ -1,6 +0,0 @@
-# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-module FetchCoupon
-	def self.page params
-		return params[:name].gsub('-','_') if params[:name]
-	end
-end

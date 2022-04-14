5fad9207c61199d1243b917871bc5f24dbddf92f
Eric Schultz <eric@commitchange.com>
2022-04-14 13:35:44 -0500

    Add monkeypatch for ActionController::TestResponse on Ruby 2.6

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,21 @@ module ActionController::TestCase::Behavior
   end
 end
 
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
 require 'spec_helper'
 require 'rspec/rails'
 require 'devise'

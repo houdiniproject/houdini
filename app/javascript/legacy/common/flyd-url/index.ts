e82a7c07284a7a1d188a32b50c5f82af6e38e7d3
Eric Schultz <eric@commitchange.com>
2022-07-05 18:15:45 -0500

    Vendor flyd-url and convert it to URL

---
+++ app/javascript/legacy/common/flyd-url/index.ts
@@ -0,0 +1,22 @@
+// License: LGPL-3.0-or-later
+// from: https://github.com/jayrbolton/flyd-url/blob/master/index.es6
+import flyd from 'flyd';
+
+let href = location.href;
+const stream = flyd.stream(new URL(href));
+let target = Date.now();
+const dur = 50;
+
+const poll = () => {
+	if(stream.end()) return;
+	const now = Date.now();
+	target += dur;
+	if(href !== location.href) {
+		stream(new URL(location.href));
+		href = location.href;
+	}
+	setTimeout(poll, target - now);
+};
+poll();
+
+module.exports = stream;

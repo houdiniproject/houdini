df45ec56ea463822a8790b38be70185f279ae030
Eric Schultz <eric@commitchange.com>
2022-07-10 16:59:21 -0500

    Migrate brand-fonts to Typescript

--- app/javascript/legacy/common/brand-fonts.js
+++
@@ -1,8 +0,0 @@
-// License: LGPL-3.0-or-later
-module.exports = {
-	helvetica: {family: "'Helvetica Neue', Helvetica, Arial, sans-serif", name: 'Helvetica'},
-	futura: {family: "'Futura', Arial, sans-serif", name: 'Futura'},
-	open: {family: "Open Sans, 'Helvetica Neue', Arial, sans-serif", name: 'Open Sans'},
-	georgia: {family: "Georgia, serif", name: 'Georgia'},
-	bitter: {family: "'Bitter', serif", name: 'Bitter'}
-}
---
+++ app/javascript/legacy/common/brand-fonts.ts
@@ -0,0 +1,8 @@
+// License: LGPL-3.0-or-later
+export default {
+	helvetica: { family: "'Helvetica Neue', Helvetica, Arial, sans-serif", name: 'Helvetica' },
+	futura: { family: "'Futura', Arial, sans-serif", name: 'Futura' },
+	open: { family: "Open Sans, 'Helvetica Neue', Arial, sans-serif", name: 'Open Sans' },
+	georgia: { family: "Georgia, serif", name: 'Georgia' },
+	bitter: { family: "'Bitter', serif", name: 'Bitter' },
+};

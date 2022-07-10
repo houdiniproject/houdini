ba02e27e71b7936cff6cdf43775a33eddc9545eb
Eric Schultz <eric@commitchange.com>
2022-07-10 16:00:59 -0500

    Remove unused format.toSentence

--- app/javascript/legacy/common/format.js
+++ app/javascript/legacy/common/format.js
@@ -51,13 +51,6 @@ format.capitalize = function (string) {
     .join(' ')
 }
 
-format.toSentence = function(arr) {
-	if(arr.length < 2) return arr
-	if(arr.length === 2) return arr[0] + ' and ' + arr[1]
-	var last = arr.length - 1
-	return arr.slice(0, last).join(', ') + ', and ' + arr[last]
-}
-
 function zeroPad(num, size) {
 	return (num + "").padStart(size, "0")
 }

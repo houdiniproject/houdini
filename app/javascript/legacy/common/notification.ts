3a428140d4945487a038b71eba6e77f3f7d02e2c
Eric Schultz <eric@commitchange.com>
2022-07-11 16:20:30 -0500

    Migrate legacy-legacy notification to typescript

---
+++ app/javascript/legacy/common/notification.ts
@@ -0,0 +1,13 @@
+// License: LGPL-3.0-or-later
+
+export default function notification(msg: string, err: unknown):void {
+	const el = document.getElementById('js-notification');
+	if (err) { el.className = 'show error'; }
+	else { el.className = 'show'; }
+	el.innerText = msg;
+	window.setTimeout(function () {
+		el.className = '';
+		el.innerText = '';
+	}, 7000);
+}
+

3f280a79590f51a283eb4d3b5752db89c17e1659
Eric Schultz <eric@commitchange.com>
2022-07-12 16:49:39 -0500

    Use the common name_and_id routes

--- app/javascript/legacy/common/ajax/get_campaign_and_event_names_and_ids.js
+++ app/javascript/legacy/common/ajax/get_campaign_and_event_names_and_ids.js
@@ -1,9 +1,12 @@
 // License: LGPL-3.0-or-later
 var request = require('../client').default
 
+const campaign = require('../../../routes/nonprofits/campaigns').default
+const events = require('../../../routes/nonprofits/events').default
+
 module.exports = function(npo_id) {
-  var campaignsPath = '/nonprofits/' + npo_id + '/campaigns/name_and_id'
-  var eventsPath = '/nonprofits/' + npo_id + '/events/name_and_id'
+  const campaignsPath = campaign.nameAndIdNonprofitCampaigns(npo_id)
+  const eventsPath = events.nameAndIdNonprofitEvents(npo_id)
 
   request.get(campaignsPath).end(function(err, resp){
     var dataResponse = []

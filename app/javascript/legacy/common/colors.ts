83ae8190f7cbd699a5c5a7a463dc712d47993d52
Eric Schultz <eric@commitchange.com>
2022-07-12 11:38:39 -0500

    Migrate app/javascript/legacy/common/colors to TS

--- app/javascript/legacy/common/colors.ts
+++ app/javascript/legacy/common/colors.ts
@@ -1,59 +1,59 @@
 // License: LGPL-3.0-or-later
-module.exports = {
-  // BLUES
-  '$dark-turquoise': "#306563"
-, '$turquoise': "#a6d5d3"
-, '$sea-foam': "#669092"
-, '$light-sea-foam': "#5FA6AA"
-, '$sky': "#97c2c4"
-, '$faded-sky': "#B6D3D4"
-, '$light-logo-blue': "#69B4CF"
-, '$logo-blue': "#42B3DF"
-, '$dark-logo-blue': "#479EBE"
-, '$baby-blue': "#E2F8F8"
-, '$blue-grey': "rgba(136, 148, 152, 1)"
-, '$cloudy': "rgba(67, 164, 202, 0.3)"
+export default {
+	// BLUES
+	'$dark-turquoise': "#306563"
+	, '$turquoise': "#a6d5d3"
+	, '$sea-foam': "#669092"
+	, '$light-sea-foam': "#5FA6AA"
+	, '$sky': "#97c2c4"
+	, '$faded-sky': "#B6D3D4"
+	, '$light-logo-blue': "#69B4CF"
+	, '$logo-blue': "#42B3DF"
+	, '$dark-logo-blue': "#479EBE"
+	, '$baby-blue': "#E2F8F8"
+	, '$blue-grey': "rgba(136, 148, 152, 1)"
+	, '$cloudy': "rgba(67, 164, 202, 0.3)"
 
-  // GREENS
-, '$light-grass': "#589E73"
-, '$bluegrass': "#5FB88D"
-, '$bluegrass--light': "lighten($bluegrass, 10)"
-, '$grass': "#2d8f60"
-, '$dark-grass': "#35865E"
-, '$sage': "#E9F5E8"
-, '$sage--dark': "#D5E4D4"
-, '$thankYou-green': "#7EC981"
-, '$mint': "#DEEFE7"
+	// GREENS
+	, '$light-grass': "#589E73"
+	, '$bluegrass': "#5FB88D"
+	, '$bluegrass--light': "lighten($bluegrass, 10)"
+	, '$grass': "#2d8f60"
+	, '$dark-grass': "#35865E"
+	, '$sage': "#E9F5E8"
+	, '$sage--dark': "#D5E4D4"
+	, '$thankYou-green': "#7EC981"
+	, '$mint': "#DEEFE7"
 
-  // YELLOWS - ORANGES
-, '$manila': "#FFFCE3"
-, '$pollen': "#F0CD6C"
-, '$light-pollen': "#FFE397"
-, '$oj': "#FDC785"
-, '$looseleaf': "lighten(#FFFEF6, 0.5)"
+	// YELLOWS - ORANGES
+	, '$manila': "#FFFCE3"
+	, '$pollen': "#F0CD6C"
+	, '$light-pollen': "#FFE397"
+	, '$oj': "#FDC785"
+	, '$looseleaf': "lighten(#FFFEF6, 0.5)"
 
-  // PINKS, REDS, PURPLES
-, '$watermelon': "#EE8480"
-, '$lavender': "#A57B9E"
-, '$blush': "rgba(253, 168, 133, 0.1)"
-, '$red': "#FF4F4F"
+	// PINKS, REDS, PURPLES
+	, '$watermelon': "#EE8480"
+	, '$lavender': "#A57B9E"
+	, '$blush': "rgba(253, 168, 133, 0.1)"
+	, '$red': "#FF4F4F"
 
-   // NEUTRALS
-, '$charcoal': "#494949"
-, '$charcoal--light': "lighten(#494949, 10)"
-, '$grey': "rgb(128, 128, 128)"
-, '$lightGrey': "lighten($grey, 20)"
-, '$sepia': "rgba(65, 65, 65, 0.7)"
-, '$shark': "rgb(162, 162, 162)"
-, '$fog': "#fbfbfb"
-, '$shade': "rgba(0,0,0,0.02)"
+	// NEUTRALS
+	, '$charcoal': "#494949"
+	, '$charcoal--light': "lighten(#494949, 10)"
+	, '$grey': "rgb(128, 128, 128)"
+	, '$lightGrey': "lighten($grey, 20)"
+	, '$sepia': "rgba(65, 65, 65, 0.7)"
+	, '$shark': "rgb(162, 162, 162)"
+	, '$fog': "#fbfbfb"
+	, '$shade': "rgba(0,0,0,0.02)"
 
-, '$trans': "rgba(255,255,255,0)"
+	, '$trans': "rgba(255,255,255,0)"
 
-, '$defaultShadow': "0 0 4px 1px rgba($grey, 0.5)"
+	, '$defaultShadow': "0 0 4px 1px rgba($grey, 0.5)"
 
-  // SOCIAL
-, '$facebook': "#236094"
-, '$twitter': "#3199cb"
-, '$google': "#dd4b39"
-}
+	// SOCIAL
+	, '$facebook': "#236094"
+	, '$twitter': "#3199cb"
+	, '$google': "#dd4b39",
+} as const;

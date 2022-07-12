ca8b82a70fa35b0c9f06eb351ef692d3071d66ac
Eric Schultz <eric@commitchange.com>
2022-07-12 16:49:39 -0500

    Use the common name_and_id routes

--- app/javascript/legacy/common/ajax/check_campaign_or_event_name.js
+++ app/javascript/legacy/common/ajax/check_campaign_or_event_name.js
@@ -1,9 +1,15 @@
 // License: LGPL-3.0-or-later
 var R = require('ramda')
 var request = require('../client').default
+const campaign = require('../../../routes/nonprofits/campaigns').default;
+const event = require('../../../routes/nonprofits/events').default;
 
 module.exports = function(name, event_or_campaign, callback) {
-  request.get(`/nonprofits/${app.nonprofit_id}/${event_or_campaign}s/name_and_id`)
+  const url = event_or_campaign == "event" ? 
+    campaign.nameAndIdNonprofitCampaigns(app.nonprofit_id) :
+    event.nameAndIdNonprofitEvents(app.nonprofit_id);
+
+  request.get(url)
     .end(function(err, resp){
       var names = resp.body.map(x => x.name)
       if(R.contains(name, names)) {

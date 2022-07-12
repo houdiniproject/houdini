134a46a349c163d29bb22efe99cc19fd95811cbf
Eric Schultz <eric@commitchange.com>
2022-07-12 11:54:09 -0500

    Migrate on-change-sanitize-slug

--- app/javascript/legacy/nonprofits/edit/page.js
+++ app/javascript/legacy/nonprofits/edit/page.js
@@ -1,6 +1,6 @@
 // License: LGPL-3.0-or-later
 require('../../common/image_uploader')
-require('../../common/on-change-sanitize-slug')
+require('../../common/on-change-sanitize-slug').default
 var url = "/nonprofits/" + app.nonprofit_id
 
 appl.def('remove_this_image', function() {

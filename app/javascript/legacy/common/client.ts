3967f10d422317f6f754a4ccd44a7bb2a20eb562
Eric Schultz <eric@commitchange.com>
2022-07-15 17:11:38 -0500

    Simplify interface for client.ts

--- app/javascript/legacy/common/client.ts
+++ app/javascript/legacy/common/client.ts
@@ -1,23 +1,22 @@
 // License: LGPL-3.0-or-later
 // superapi wrapper with our api defaults
 
-import request, { Response, SuperAgentRequest } from 'superagent';
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type CallbackHandlerFromSuperAgent = (err: any, res: Response) => void;
+import request, { SuperAgentRequest } from 'superagent';
 
 declare const window: Window & { _csrf: string };
+
 const wrapper = {
 
-	post: function (...args: [string, CallbackHandlerFromSuperAgent?]): SuperAgentRequest {
-		return (request.post.apply(this, args) as SuperAgentRequest).set('X-CSRF-Token', window._csrf).type('json');
+	post: function (path:string, ...args:unknown[]): SuperAgentRequest {
+		return (request.post.apply(this, [path, ...args]) as SuperAgentRequest).set('X-CSRF-Token', window._csrf).type('json');
 	},
 
-	put: function (...args: [string, CallbackHandlerFromSuperAgent?]): SuperAgentRequest {
-		return (request.put.apply(this, args) as SuperAgentRequest).set('X-CSRF-Token', window._csrf).type('json');
+	put: function (path:string, ...args:unknown[]): SuperAgentRequest {
+		return (request.put.apply(this, [path, ...args]) as SuperAgentRequest).set('X-CSRF-Token', window._csrf).type('json');
 	},
 
-	del: function (...args: [string, CallbackHandlerFromSuperAgent?]): SuperAgentRequest {
-		return (request.del.apply(this, args) as SuperAgentRequest).set('X-CSRF-Token', window._csrf).type('json');
+	del: function (path:string, ...args:unknown[]): SuperAgentRequest {
+		return (request.del.apply(this, [path, ...args]) as SuperAgentRequest).set('X-CSRF-Token', window._csrf).type('json');
 	},
 
 	get: function (path: string): SuperAgentRequest {

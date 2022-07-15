1e0a80ce34deea6df83329220373b699bbb0fc29
Eric Schultz <eric@commitchange.com>
2022-07-15 17:25:25 -0500

    Migrate app/javascript/legacy/ticket_levels/get_totals.js to TS

--- app/javascript/legacy/ticket_levels/get_totals.ts
+++ app/javascript/legacy/ticket_levels/get_totals.ts
@@ -1,11 +1,10 @@
+import ticket_levels from "../../routes/nonprofits/events/ticket_levels";
 // License: LGPL-3.0-or-later
 // Retrieve the total attendee (ticket) counts for every ticket level for a given event
-var request = require("../common/super-agent-promise").default
+import request from "../common/super-agent-promise";
 
-module.exports = get_totals
-
-function get_totals(nonprofit_id, event_id) {
-	return request.get('/nonprofits/' + nonprofit_id + '/events/' + event_id + '/ticket_levels')
-		.perform()
+export default function get_totals(nonprofit_id:string, event_id:string):Promise<unknown> {
+	return request.get(ticket_levels.nonprofitEventTicketLevels.path({nonprofitId: nonprofit_id, id: event_id}))
+		.perform();
 }
 

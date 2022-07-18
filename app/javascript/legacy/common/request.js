f77ce7a06270b6848b37ac0c786a3729053d1a18
Eric Schultz <eric@commitchange.com>
2022-07-18 15:26:15 -0500

    Remove ramda from app/javascript/legacy/common/request.js

--- app/javascript/legacy/common/request.js
+++ app/javascript/legacy/common/request.js
@@ -1,11 +1,11 @@
 // License: LGPL-3.0-or-later
-const R = require('ramda')
 const request = require('flyd-ajax')
 
 module.exports = options => {
-  options.headers = R.merge({
-    'Content-Type': 'application/json'
-  , 'X-CSRF-Token': window._csrf
-  }, options.headers || {})
+  options.headers = {
+    'Content-Type': 'application/json',
+    'X-CSRF-Token': window._csrf,
+    ...(options.headers || {})
+  };
   return request(options)
 }

dc378693ef0b23a2f030c7f65299740755462c28
Eric Schultz <eric@commitchange.com>
2022-07-19 17:56:57 -0500

    Migrate TransactionTitle partial to a ReactComponent

--- app/javascript/components/common/TransactionTitle/TransactionTabs.tsx
+++ app/javascript/components/common/TransactionTitle/TransactionTabs.tsx
@@ -6,27 +6,25 @@ import Nonprofit from '../../../legacy/app_data/Nonprofit';
 import payments from '../../../routes/nonprofits/payments';
 import recurring from '../../../routes/nonprofits/recurring_donations';
 import payouts from '../../../routes/nonprofits/payouts';
-
+import classnames from 'classnames';
 export interface TransactionTabsProps {
 	active: 'payments' | 'recurring' | 'payouts';
 	nonprofit: Nonprofit;
 }
 
-
-
 export default function TransactionTabs(props: TransactionTabsProps): JSX.Element {
 
 	return (<div className='pageTabs'>
 		<a href={payouts.nonprofitsPayouts.path(props.nonprofit)}
-			className={`tour-payouts ${props.active == 'payouts' ? 'is-active' : ''}`}>
+			className={classnames('tour-payouts', {'is_active': props.active == 'payouts'})}>
 			Payouts
 		</a>
 		<a href={recurring.nonprofitsRecurringDonations.path(props.nonprofit)}
-			className={props.active == 'recurring' ? 'is-active' : ''}>
+			className={classnames({'is-active': props.active == 'recurring'})}>
 			Recurring
 		</a>
 		<a href={payments.nonprofitsPayments.path(props.nonprofit)}
-			className={props.active == 'payments' ? 'is-active' : ''}>
+			className={classnames({'is-active': props.active == 'payments'})}>
 			History
 		</a >
 	</div >);
--- app/javascript/packs/page__nonprofits__payouts__index.js
+++ app/javascript/packs/page__nonprofits__payouts__index.js
@@ -1,9 +1,10 @@
 const ReactOnRails = require('react-on-rails').default;
 const SideNav = require('../components/common/SideNav/SideNavRenderFunc').default;
-
+const TransactionTitle = require('../components/common/TransactionTitle/TransactionTitleRenderFunc').default;
 // This is how react_on_rails can see SignInPage in the browser.
 ReactOnRails.register({
 	SideNav,
+	TransactionTitle,
 });
 require('../../../client/css/global/page.css')
 require('../legacy/page.js')

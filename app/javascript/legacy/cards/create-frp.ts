100911e5dfe7aadff80a1fd49e5e34df26b7443d
Eric Schultz <eric@commitchange.com>
2022-08-07 23:29:40 -0500

    Migrate app/javascript/legacy/cards/create-frp.js to TS

--- app/javascript/legacy/cards/create-frp.ts
+++ app/javascript/legacy/cards/create-frp.ts
@@ -1,11 +1,12 @@
 // License: LGPL-3.0-or-later
-const flyd = require('flyd')
-const flyd_flatMap = require('flyd/module/flatmap')
-
+import flyd from 'flyd';
+import { Token } from '@stripe/stripe-js';
+import type {Stripe as StripeJS} from '../stripe_wrapper';
+declare const Stripe: StripeJS;
 // Given an object of card data, return a stream of stripe tokenization responses
-module.exports = obj => {
-	var $ = flyd.stream()
-	Stripe.card.createToken(obj, (status, resp) => $(resp))
-	return $
+export default function createFrp(obj:unknown) : flyd.Stream<Token> {
+	const $ = flyd.stream<Token>();
+	Stripe.card.createToken(obj, (_status, resp) => $(resp));
+	return $;
 }
 

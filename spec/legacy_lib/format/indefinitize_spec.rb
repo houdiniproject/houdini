8a7346d0c1fcfd02b0b71631665c85c5452d02af
Eric Schultz <eric@commitchange.com>
2022-08-08 12:47:06 -0500

    Speed up format/indefinitize specs

--- spec/legacy_lib/format/indefinitize_spec.rb
+++ spec/legacy_lib/format/indefinitize_spec.rb
@@ -2,8 +2,7 @@
 
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
-require 'spec_helper'
-require 'format/indefinitize'
+require_relative '../../../app/legacy_lib/format/indefinitize'
 
 describe Format::Indefinitize do
   describe '#article' do

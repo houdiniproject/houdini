33ace1664e9c1c0a5deee0bcedcb68e0eab83a9b
Eric Schultz <eric@commitchange.com>
2022-08-08 18:20:21 -0500

    Migrate app/javascript/legacy/common/confirmation.js to TS

--- app/javascript/legacy/common/confirmation.ts
+++ app/javascript/legacy/common/confirmation.ts
@@ -1,46 +1,51 @@
 // License: LGPL-3.0-or-later
-var confirmation = function(msg, success_cb) {
-	var $confirm_modal = $('#confirmation-modal')
-	var $msg = $confirm_modal.find('.msg')
-	if(msg && msg.length > 15) $msg.css('font-size', '16px')
-	var cb = {
-		confirmed: function() {},
-		denied: function() {}
-	}
-	var $previousModal = $('.modal.inView')
-	$('.modal').removeClass('inView')
-	var $body = $('body')
-	$body.addClass('is-showingModal')
+import noop from 'lodash/noop';
 
-	function hide_confirmation_and_show_previous(){
-		$('#confirmation-modal').removeClass('inView')
-		if ($previousModal.length){
-				$previousModal.addClass('inView')
-				$body.addClass('is-showingModal')
-			}
+interface ConfirmationCallbacks {
+	confirmed: (event:JQuery.ClickEvent) => void;
+	denied: (event:JQuery.ClickEvent) => void;
+}
+
+export default function confirmation(msg?:string, success_cb?:()=> void):  ConfirmationCallbacks {
+	const $confirm_modal = $('#confirmation-modal');
+	const $msg = $confirm_modal.find('.msg');
+	if (msg && msg.length > 15) $msg.css('font-size', '16px');
+	const cb = {
+		confirmed: noop,
+		denied: noop,
+	};
+	const $previousModal = $('.modal.inView');
+	$('.modal').removeClass('inView');
+	const $body = $('body');
+	$body.addClass('is-showingModal');
+
+	function hide_confirmation_and_show_previous() {
+		$('#confirmation-modal').removeClass('inView');
+		if ($previousModal.length) {
+			$previousModal.addClass('inView');
+			$body.addClass('is-showingModal');
+		}
 		else
-			$body.removeClass('is-showingModal')
+			$body.removeClass('is-showingModal');
 	}
 
 	$confirm_modal.addClass('inView')
 		.off('click', '.yes')
 		.off('click', '.no')
 
-		.on('click', '.yes', function(e) {
-			hide_confirmation_and_show_previous()
-      if(success_cb) {
-        success_cb()
-      } else {
-       cb.confirmed(e)
-      }
+		.on('click', '.yes', function (e) {
+			hide_confirmation_and_show_previous();
+			if (success_cb) {
+				success_cb();
+			} else {
+				cb.confirmed(e);
+			}
 		})
-		.on('click', '.no', function(e) {
-			$('#confirmation-modal').removeClass('inView')
-			hide_confirmation_and_show_previous()
-			cb.denied(e)
-		})
-		$msg.text(msg || 'Are you sure?')
-	return cb
+		.on('click', '.no', function (e:JQuery.ClickEvent) {
+			$('#confirmation-modal').removeClass('inView');
+			hide_confirmation_and_show_previous();
+			cb.denied(e);
+		});
+	$msg.text(msg || 'Are you sure?');
+	return cb;
 }
-
-module.exports = confirmation

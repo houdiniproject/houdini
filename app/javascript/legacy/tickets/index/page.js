55e969b0197621c9042038b045d7765e2955c169
Eric Schultz <eric@commitchange.com>
2022-08-08 18:25:43 -0500

    Remove unused is_usa Viewscript function

--- app/javascript/legacy/tickets/index/page.js
+++ app/javascript/legacy/tickets/index/page.js
@@ -6,9 +6,7 @@ const create_donation = require('../../donations/create').default
 var request = require('../../common/super-agent-promise').default
 var get_ticket_levels = require('../../ticket_levels/get_totals').default
 var format_err = require('../../common/format_response_error').default
-var format = require('../../common/format').default
 var confirmation = require('../../common/confirmation')
-appl.def('is_usa', format.geography.isUS)
 require('../../common/restful_resource')
 require('../../components/tables/filtering/apply_filter')('tickets')
 require('./delete-ticket')

72c5331c7b5df76a578a2e63ac58730ff2ebdb25
Eric Schultz <eric@commitchange.com>
2022-08-08 14:26:24 -0500

    Update the good_job_protection_spec for good_job 3

--- spec/requests/good_job_protection_spec.rb
+++ spec/requests/good_job_protection_spec.rb
@@ -10,22 +10,23 @@ describe 'GoodJob protection' do
 		it 'shows the good job dashboard' do
 			user.roles.create(name: 'super_admin')
 			sign_in user
-			get('/good_job')
+			get('/good_job/jobs')
 			expect(response).to have_http_status(:success)
 		end
 	end
 
 	describe 'when not logged in' do
-		it 'is redirected to log in page' do
-			get('/good_job')
+		it 'is redirected to log in page' do  # rubocop:disable RSpec/MultipleExpectations
+			get('/good_job/jobs')
 			expect(response).to have_http_status(:redirect)
+			expect(response.header['location']).to eq 'http://www.example.com/users/sign_in'
 		end
 	end
 
 	describe 'when logged in but is not super_admin' do
 		it 'raises RoutingError' do
 			sign_in user
-			expect { get('/good_job') }.to raise_error(ActionController::RoutingError)
+			expect { get('/good_job/jobs') }.to raise_error(ActionController::RoutingError)
 		end
 	end
 end

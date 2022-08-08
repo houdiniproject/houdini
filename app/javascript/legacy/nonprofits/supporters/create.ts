d5e6917c3cdd7634f6822b038499ac839235b83f
Eric Schultz <eric@commitchange.com>
2022-08-08 17:45:53 -0500

    Move app/javascript/legacy/nonprofits/supporters/create.js to .ts

--- app/javascript/legacy/nonprofits/supporters/create.ts
+++ app/javascript/legacy/nonprofits/supporters/create.ts
@@ -1,11 +1,11 @@
 // License: LGPL-3.0-or-later
-var request = require('../../common/super-agent-promise').default
-const {
+import request from '../../common/super-agent-promise';
+import {
  nonprofitsSupportersPath
-} = require('../../../routes')
-module.exports = create_supporter
+}  from '../../../routes';
 
-function create_supporter(form_obj, ui) {
+
+export default function create_supporter(form_obj, ui) {
 	ui.start()
 	return request.post(nonprofitsSupportersPath(app.nonprofit_id))
 		.send(form_obj).perform()

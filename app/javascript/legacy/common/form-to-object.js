d76038709675b84be6d23bc2b9e7e33f24428674
Eric Schultz <eric@commitchange.com>
2022-08-08 00:10:45 -0500

    Cleanup app/javascript/legacy/common/form-to-object.js

--- app/javascript/legacy/common/form-to-object.js
+++ app/javascript/legacy/common/form-to-object.js
@@ -2,12 +2,11 @@
 // Convert a form to an object literal
 module.exports = function(form) {
 	if(form === undefined) throw new Error("form is undefined")
-	var result = {}
-	var fields = toArr(form.querySelectorAll('input, textarea, select'))
+	const result = {}
+	toArr(form.querySelectorAll('input, textarea, select'))
   .filter(function(n) { return n.hasAttribute('name') })
   .map(function(n) {
-    var name = n.getAttribute('name')
-    var keys = n.getAttribute('name').split('.')
+    const keys = n.getAttribute('name').split('.')
     if(n.value && n.value.toString().length) { // won't set empty strings for empty vals
       if(n.getAttribute('type') === 'checkbox') {
         deepSet(keys, n.checked, result)
@@ -27,9 +26,9 @@ function toArr(x) { return Array.prototype.slice.call(x) }
 // Given an array of nested keys, a value, and a target object:
 // Set the value into the object at the last nested key
 function deepSet(keys, val, obj, options) {
-	var exceptLast = keys.slice(0, keys.length-1)
-	var last = keys[keys.length-1]
-	var nested = exceptLast.reduce(function(nestedObj, key) {
+	const exceptLast = keys.slice(0, keys.length-1)
+	const last = keys[keys.length-1]
+	const nested = exceptLast.reduce(function(nestedObj, key) {
 		if(nestedObj[key] === undefined) {
 			nestedObj[key] = {}
 			return nestedObj[key]

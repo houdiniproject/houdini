558a7a872ed002cae476bed8ed4ac1194f2f4ca1
Eric Schultz <eric@commitchange.com>
2022-08-11 19:26:27 -0500

    Cleanup app/javascript/legacy/gift_options/index.js

--- app/javascript/legacy/gift_options/index.js
+++ app/javascript/legacy/gift_options/index.js
@@ -1,14 +1,17 @@
 // License: LGPL-3.0-or-later
 require('../common/restful_resource')
+const {
+	nonprofitCampaignPath,
+} = require('../../routes');
 
 appl.def('gift_options', {
 	resource_name: 'campaign_gift_options',
-	path_prefix: '/nonprofits/' + app.nonprofit_id + '/campaigns/' + app.campaign_id + '/',
+	path_prefix:nonprofitCampaignPath(app.nonprofit_id, app.campaign_id) + '/',
 })
 
 appl.def('ajax_gift_options.index', function() {
 	appl.ajax.index('gift_options').then(function(resp) {
-		var data = resp.body.data
+		const data = resp.body.data
 		appl.def('gift_options.data', supplementData(data))
 		checkForQuantity(data)
 	})
@@ -17,7 +20,7 @@ appl.def('ajax_gift_options.index', function() {
 function supplementData(data) {
 	return data.map(function(x) {
 		if(x.quantity) {
-			var remaining =  x.quantity - x.total_gifts
+			const remaining =  x.quantity - x.total_gifts
 			x.remaining = remaining > 0 ? remaining : 0
 		}
 		return x

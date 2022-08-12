52a8c8c85655ccdb6fe908ddb18c834fbb9a257f
Eric Schultz <eric@commitchange.com>
2022-08-11 19:29:07 -0500

    Cleanup app/javascript/legacy/nonprofits/button/designations.js

--- app/javascript/legacy/nonprofits/button/designations.js
+++ app/javascript/legacy/nonprofits/button/designations.js
@@ -1,13 +1,13 @@
 // License: LGPL-3.0-or-later
-var flyd = require("flyd")
-var h = require("virtual-dom/h")
+const flyd = require("flyd")
+const h = require("virtual-dom/h")
 
-var footer = require('./footer')
-var radioAndLabelWrapper = require('../../components/radio-and-label-wrapper')
+const footer = require('./footer')
+const radioAndLabelWrapper = require('../../components/radio-and-label-wrapper')
 
-var nameStream = flyd.stream()
-var countStream = flyd.stream()
-var inputStream = flyd.stream()
+const nameStream = flyd.stream()
+const countStream = flyd.stream()
+const inputStream = flyd.stream()
 
 
 flyd.map(function(keyup){
@@ -15,7 +15,7 @@ flyd.map(function(keyup){
 }, inputStream)
 
 
-var namePrefix = 'settings.designations.'
+const namePrefix = 'settings.designations.'
 
 module.exports = {
 	root: root,
@@ -37,7 +37,7 @@ function root(state) {
 }
 
 function body(state){
-	var desigs = state.settings.designations
+	const desigs = state.settings.designations
 	return [menu(),
 	input(desigs),
 	inputs(desigs)]
@@ -63,13 +63,13 @@ function input(desigs){
 }
 
 function inputs(desigs){
-	var prompt = [h('p.pastelBox--green.u-padding--10.u-marginY--10', 'If you would like to add a custom prompt to your donors, \
+	const prompt = [h('p.pastelBox--green.u-padding--10.u-marginY--10', 'If you would like to add a custom prompt to your donors, \
 		please enter it below. Example: "Which radio show would you like to donate to?".  The default prompt is "Please select a designation".'),
 			h('input.u-marginTop--10.input--400',
 			{placeholder: 'Prompt to donors', attributes: {'maxlength': 50}, name: namePrefix + 'prompt', onkeyup: inputStream})
 		]
-	var inputs = []
-	for(var i = 0; i < desigs.count; i++) {
+	const inputs = []
+	for(const i = 0; i < desigs.count; i++) {
 		inputs.push(h('li', h('input.input--400', {attributes: {'maxlength': 50}, placeholder: 'Designation name', name: namePrefix + 'multiples.' + i, onchange: inputStream})))
 	}
 	return h('div', {style: {display: desigs.name === 'multiple' ? 'block' : 'none'}}, [

3bc865a8deaee7106857963ed80a17fff0ee79c5
Eric Schultz <eric@commitchange.com>
2022-08-11 19:28:23 -0500

    Cleanup app/javascript/legacy/nonprofits/button/appearance.js

--- app/javascript/legacy/nonprofits/button/appearance.js
+++ app/javascript/legacy/nonprofits/button/appearance.js
@@ -1,13 +1,13 @@
 // License: LGPL-3.0-or-later
-var flyd = require("flyd")
-var h = require("virtual-dom/h")
+const flyd = require("flyd")
+const h = require("virtual-dom/h")
 
-var footer = require('./footer')
-var radioAndLabelWrapper = require('../../components/radio-and-label-wrapper')
+const footer = require('./footer')
+const radioAndLabelWrapper = require('../../components/radio-and-label-wrapper')
 const donateElephantPng = require('../../../../assets/images/graphics/donate-elephant.png')
 const miniAmountStepPng = require('../../../../assets/images/graphics/mini-amount-step.png')
 
-var appearanceStream = flyd.stream()
+const appearanceStream = flyd.stream()
 
 module.exports = {
 	root: root,
@@ -39,15 +39,15 @@ function contentWrapper(title, content) {
 	return [title, h('div.u-paddingTop--15', content)]
 }
 
-var color = app.nonprofit.brand_color ? app.nonprofit.brand_color : '#42B3DF'
-var font = app.nonprofit.brand_font ? app.nonprofit.brand_font : 'inherit'
-var buttonStyles = {background: color, 'font-family': font}
+const color = app.nonprofit.brand_color ? app.nonprofit.brand_color : '#42B3DF'
+const font = app.nonprofit.brand_font ? app.nonprofit.brand_font : 'inherit'
+const buttonStyles = {background: color, 'font-family': font}
 
-var namePrefix = 'settings.appearance.'
+const namePrefix = 'settings.appearance.'
 
 function defaultButton(){
-	var title = 'Default button'
-	var content = [ h('p.branded-donate-button', {style: buttonStyles}, 'Donate'),
+	const title = 'Default button'
+	const content = [ h('p.branded-donate-button', {style: buttonStyles}, 'Donate'),
 			brandedButtonMessage()]
 	function brandedButtonMessage(){
 		if(app.nonprofit.brand_color){return}
@@ -61,33 +61,33 @@ function defaultButton(){
 }
 
 function fixedButton(){
-	var title = 'Fixed position button'
-	var content = [h('p.branded-donate-button.is-fixed', {style: buttonStyles}, 'Donate')]
+	const title = 'Fixed position button'
+	const content = [h('p.branded-donate-button.is-fixed', {style: buttonStyles}, 'Donate')]
 	return h('td', [radioAndLabelWrapper('radio-fixed',  namePrefix + 'name', {'value': 'fixed'},
 		contentWrapper(title, content), appearanceStream)])
 }
 
 function embeddedButton(){
-	var title = 'Embed directly on page'
-	var content = [ h('img', {src: miniAmountStepPng, title: title})]
+	const title = 'Embed directly on page'
+	const content = [ h('img', {src: miniAmountStepPng, title: title})]
 	return h('td', [radioAndLabelWrapper('radio-embedded', namePrefix + 'name', {'value': 'embedded'},
 		contentWrapper(title, content), appearanceStream)])
 }
 
 function imageButton(state){
-	var title = 'Custom image'
+	const title = 'Custom image'
 	const defaultImg = donateElephantPng
-	var imgUrl = state.settings.appearance.customImg ? state.settings.appearance.customImg : defaultImg
-	var content = [ h('img', {src: imgUrl, title: title}),
+	const imgUrl = state.settings.appearance.customImg ? state.settings.appearance.customImg : defaultImg
+	const content = [ h('img', {src: imgUrl, title: title}),
 		h('input', {type: 'text', name: namePrefix + 'customImg', placeholder: 'Add your image URL here', onkeyup: appearanceStream})]
 	return h('td', [radioAndLabelWrapper('radio-custom-image', namePrefix + 'name', {'value': 'custom image'},
 		contentWrapper(title, content), appearanceStream)])
 }
 
 function customText(state) {
-	var text = state.settings.appearance.customText ? state.settings.appearance.customText : 'Donate'
-	var title = 'Custom text'
-	var content = [
+	const text = state.settings.appearance.customText ? state.settings.appearance.customText : 'Donate'
+	const title = 'Custom text'
+	const content = [
 		h('a.customText-text', text),
 		h('input', {type: 'text', name: namePrefix + 'customText', placeholder: 'Type here to change text', onkeyup: appearanceStream})
 	]

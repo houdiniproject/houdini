0e432dbaad34c5c43114182388f0b0ebb164b716
Eric Schultz <eric@commitchange.com>
2022-08-22 17:05:59 -0500

    Remove PostNonprofit

--- app/javascript/legacy_react/api/model/models.ts
+++ app/javascript/legacy_react/api/model/models.ts
@@ -1,6 +1,3 @@
 export * from './Nonprofit';
-export * from './PostNonprofit';
-export * from './PostNonprofitNonprofit';
-export * from './PostNonprofitUser';
 export * from './ValidationError';
 export * from './ValidationErrors';

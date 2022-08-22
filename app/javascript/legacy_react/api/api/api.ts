031b5440fb9296359325a40ec0f0fead2811cfe1
Eric Schultz <eric@commitchange.com>
2022-08-22 17:04:00 -0500

    Remove UsersApi

--- app/javascript/legacy_react/api/api/api.ts
+++ app/javascript/legacy_react/api/api/api.ts
@@ -1,5 +1,3 @@
 export * from './NonprofitsApi';
-export * from './UsersApi';
 import { NonprofitsApi } from './NonprofitsApi';
-import { UsersApi } from './UsersApi';
-export const APIS = [NonprofitsApi, UsersApi];
+export const APIS = [NonprofitsApi];
--- app/javascript/legacy_react/api/model/models.ts
+++ app/javascript/legacy_react/api/model/models.ts
@@ -2,6 +2,5 @@ export * from './Nonprofit';
 export * from './PostNonprofit';
 export * from './PostNonprofitNonprofit';
 export * from './PostNonprofitUser';
-export * from './PostUser';
 export * from './ValidationError';
 export * from './ValidationErrors';

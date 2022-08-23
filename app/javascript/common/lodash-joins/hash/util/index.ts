2ee7968261be70c68c945f5e4934b175ded7f9bf
Eric Schultz <eric@commitchange.com>
2022-08-23 11:19:28 -0500

    Remove vendored lodash-joins

--- app/javascript/common/lodash-joins/hash/util/index.ts
+++
@@ -1,7 +0,0 @@
-// License: LGPL-3.0-or-later
-// from https://github.com/mtraynham/lodash-joins/blob/c252b462981562451d85d1e09c8f273ce7fe06c5/lib/hash/util/index.ts
-import toStringAccessor from './toStringAccessor';
-
-export {
-    toStringAccessor
-};

09c74e1e2fc2dcce0e69baece123d1a1a4a7bf6c
Eric Schultz <eric@commitchange.com>
2022-08-23 13:15:12 -0500

    Remove deprecated getSidFromNodeUrlQS

--- app/javascript/legacy/nonprofits/supporters/index/sidepanel/backport-query-string.ts
+++ app/javascript/legacy/nonprofits/supporters/index/sidepanel/backport-query-string.ts
@@ -1,23 +1,11 @@
 // License: LGPL-3.0-or-later
-import {UrlWithStringQuery as NodeUrl} from 'url';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const queryString = require('query-string');
-
-/**
- * @deprecated
- */
-
-export function getSidFromNodeUrlQS(url:NodeUrl| URL): string | undefined {
-	return queryString.parse(url.search).sid as string | undefined;
-}
 
 /**
  *
  * @param url
  * @return The value if it exists or undefined (to be consistent with the previous implementation)
  */
-export function getSidFromNodeUrl(url:NodeUrl | URL ): string | undefined {
+export function getSidFromNodeUrl(url: URL ): string | undefined {
 	const result = new URLSearchParams(url.search).get("sid");
 	return result === null ? undefined : result;
 }

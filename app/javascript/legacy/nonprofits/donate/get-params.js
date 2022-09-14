4e0edea900ce05e8e2a7b1342bfc8cf082e68e65
Eric Schultz <eric@commitchange.com>
2022-09-14 11:20:24 -0500

    Completely removed Ramda from get-params

--- app/javascript/legacy/nonprofits/donate/get-params.js
+++ app/javascript/legacy/nonprofits/donate/get-params.js
@@ -1,24 +1,26 @@
 // License: LGPL-3.0-or-later
-const R = require('ramda');
-const split = require('lodash/split');
 
 const splitParam = str =>
-  split(str, /[_;,]/)
+  str?.split(/[_;,]/)
 
 module.exports = params => {
   const defaultAmts = '10,25,50,100,250,500,1000'
   // Set defaults
-  const merge = R.merge({
-    custom_amounts: ''
-  })
-  // Preprocess data
-  const evolve = R.evolve({
-    multiple_designations: splitParam
-  , custom_amounts: amts => R.compose(R.map(Number), splitParam)(amts || defaultAmts)
-  , custom_fields: fields => R.map(f => {
-      const [name, label] = R.map(R.trim, R.split(':', f))
+  // const merge = R.merge({
+  //   custom_amounts: ''
+  // })
+  params = {
+    ...params,
+    custom_amounts: params?.custom_amounts || defaultAmts
+  }
+
+  return {
+    ...params,
+    multiple_designations: splitParam(params?.multiple_designations),
+    custom_amounts: splitParam(params?.custom_amounts).map((i) => Number(i)),
+    custom_fields: params?.custom_fields?.split(',')?.map(f => {
+      const [name, label] = f.split(':').map((i) => i.trim())
       return {name, label: label ? label : name}
-    }, R.split(',',  fields))
-  })
-  return R.compose(evolve, merge)(params)
+    })
+  }
 }

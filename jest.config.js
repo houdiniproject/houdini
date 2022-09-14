d345f1893823bdd43bc199e92bcb4c16e96b4f2c
Eric Schultz <eric@commitchange.com>
2022-09-14 12:54:15 -0500

    Move jest config into jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  collectCoverage: false,
+  modulePathIgnorePatterns: [
+    "<rootDir>/vendor", // don't go to the gems vendor folder. EVER.
+    "<rootDir>/tmp",
+    "<rootDir>/vendor",
+    "<rootDir>/storage",
+    "<rootDir>/log",
+    "<rootDir>/.vscode",
+  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFiles: ["<rootDir>/setupTests.js", "jest-date-mock"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  testEnvironmentOptions: {
+    enzymeAdapter: "react16",
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/config/webpack/test.js",
+    "<rootDir>/vendor/",
+    "<rootDir>/tmp/",
+    "<rootDir>/public/",
+    "<rootDir>/storage/",
+    "<rootDir>/log/",
+    "<rootDir>/coverage/",
+    "<rootDir>/.vscode/",
+  ],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!lodash-joins/)",
+    "<rootDir>/config/webpack/test.js",
+    "<rootDir>/vendor/",
+    "<rootDir>/tmp/",
+    "<rootDir>/public/",
+    "<rootDir>/storage/",
+    "<rootDir>/log/",
+    "<rootDir>/coverage/",
+    "<rootDir>/.vscode/",
+  ],
+};

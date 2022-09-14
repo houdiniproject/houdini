d36d6e1ebb3ab47985c0340514835da846536ddc
Eric Schultz <eric@commitchange.com>
2022-09-14 11:43:50 -0500

    get-params converted to TS

--- app/javascript/legacy/nonprofits/donate/get-params.ts
+++ app/javascript/legacy/nonprofits/donate/get-params.ts
@@ -1,26 +1,31 @@
 // License: LGPL-3.0-or-later
 
-const splitParam = str =>
-  str?.split(/[_;,]/)
+function splitParam(str: string) {
+	return str.split(/[_;,]/);
+}
 
-module.exports = params => {
-  const defaultAmts = '10,25,50,100,250,500,1000'
-  // Set defaults
-  // const merge = R.merge({
-  //   custom_amounts: ''
-  // })
-  params = {
-    ...params,
-    custom_amounts: params?.custom_amounts || defaultAmts
-  }
+interface InputParams {
+  custom_amounts?: string;
+  custom_fields?: string;
+  multiple_designations?: string;
+}
 
-  return {
-    ...params,
-    multiple_designations: splitParam(params?.multiple_designations),
-    custom_amounts: splitParam(params?.custom_amounts).map((i) => Number(i)),
-    custom_fields: params?.custom_fields?.split(',')?.map(f => {
-      const [name, label] = f.split(':').map((i) => i.trim())
-      return {name, label: label ? label : name}
-    })
-  }
+interface OutputParams {
+  custom_amounts: number[];
+  custom_fields?: { label: string, name: string }[];
+  multiple_designations?: string[];
+}
+
+export default function getParams<TInput extends InputParams>(params?: TInput): OutputParams & Record<string, unknown> {
+	const defaultAmts = '10,25,50,100,250,500,1000';
+
+	return {
+		...params,
+		multiple_designations: params?.multiple_designations ? splitParam(params.multiple_designations) : undefined,
+		custom_amounts: splitParam(params?.custom_amounts || defaultAmts).map((i) => Number(i)),
+		custom_fields: params?.custom_fields?.split(',')?.map(f => {
+			const [name, label] = f.split(':').map((i) => i.trim());
+			return { name, label: label ? label : name };
+		}),
+	};
 }

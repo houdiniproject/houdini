fbb3a986f6818e1066651a894ef4ada1db0656b9
Eric Schultz <eric@commitchange.com>
2022-09-14 12:57:37 -0500

    Alphabetize the properties in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
-module.exports =  {
-	"testEnvironmentOptions": {
-		"enzymeAdapter": "react17",
-	},
-	"globals": {
+// License: LGPL-3.0-or-later
+module.exports = {
+	collectCoverage: false,
+	globals: {
 		"ts-jest": {
-			"babelConfig": false,
-			"isolatedModules": true,
+			babelConfig: false,
+			isolatedModules: true,
 		},
 	},
-	"collectCoverage": false,
-	"modulePathIgnorePatterns": [
+	modulePathIgnorePatterns: [
 		"<rootDir>/vendor", // don't go to the gems vendor folder. EVER.
 		"<rootDir>/tmp",
 		"<rootDir>/vendor",
@@ -17,35 +15,20 @@ module.exports =  {
 		"<rootDir>/log",
 		"<rootDir>/.vscode",
 	],
-	"moduleFileExtensions":["ts", "tsx", "js", "json", "jsx", "node"],
-	"moduleNameMapper": {
-		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/app/javascript/__mocks__/fileMock.js",
+	moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx", "node"],
+	moduleNameMapper: {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+			"<rootDir>/app/javascript/__mocks__/fileMock.js",
 		"\\./regenerate.js": "<rootDir>/app/javascript/__mocks__/erbMock.js",
 	},
-	"setupFiles": [
-		"<rootDir>/setupTests.js",
-	],
-	"setupFilesAfterEnv": [
-		"<rootDir>/setupTestsAfterEnvLoaded.ts",
-	],
-	"snapshotSerializers": [
-		"enzyme-to-json/serializer",
-	],
-	"transform": {
-		"^.+\\.tsx?$": "babel-jest",
+	setupFiles: ["<rootDir>/setupTests.js"],
+	setupFilesAfterEnv: ["<rootDir>/setupTestsAfterEnvLoaded.ts"],
+	snapshotSerializers: ["enzyme-to-json/serializer"],
+	testEnvironmentOptions: {
+		enzymeAdapter: "react17",
 	},
-	"transformIgnorePatterns": [
-		"<rootDir>/node_modules/(?!lodash-joins/)",
-		"<rootDir>/config/webpack/test.js",
-		"<rootDir>/vendor/",
-		"<rootDir>/tmp/",
-		"<rootDir>/public/",
-		"<rootDir>/storage/",
-		"<rootDir>/log/",
-		"<rootDir>/coverage/",
-		"<rootDir>/.vscode/",
-	],
-	"testPathIgnorePatterns": [
+	testMatch: ["<rootDir>/app/**/?(*.)+(spec|test).[jt]s?(x)"],
+	testPathIgnorePatterns: [
 		"<rootDir>/node_modules/",
 		"<rootDir>/config/webpack/test.js",
 		"<rootDir>/vendor/",
@@ -56,5 +39,18 @@ module.exports =  {
 		"<rootDir>/coverage/",
 		"<rootDir>/.vscode/",
 	],
-	"testMatch":[ "<rootDir>/app/**/?(*.)+(spec|test).[jt]s?(x)" ],
+	transform: {
+		"^.+\\.tsx?$": "babel-jest",
+	},
+	transformIgnorePatterns: [
+		"<rootDir>/node_modules/(?!lodash-joins/)",
+		"<rootDir>/config/webpack/test.js",
+		"<rootDir>/vendor/",
+		"<rootDir>/tmp/",
+		"<rootDir>/public/",
+		"<rootDir>/storage/",
+		"<rootDir>/log/",
+		"<rootDir>/coverage/",
+		"<rootDir>/.vscode/",
+	],
 };

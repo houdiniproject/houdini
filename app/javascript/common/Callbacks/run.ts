02a9f2ea4af7385b32a8b28dacc35f1acb12b1ed
Eric Schultz <eric@commitchange.com>
2022-10-18 15:00:07 -0500

    Improve documentation and simplify the property types in Callbacks/run.ts

--- app/javascript/common/Callbacks/run.ts
+++ app/javascript/common/Callbacks/run.ts
@@ -1,13 +1,15 @@
 // License: LGPL-3.0-or-later
-import Callback from "./Callback";
+import type { CallbackClass } from "./types";
 
 
 /**
  * A very simple function for conditionally running callbacks. Move into own file because we can mock it for CallbackController
  * @param input The input properties to every callback
- * @param callbacks callbacks to be run
+ * @param callbacks callbacks as classes to be run
+ * @template TCallbackProps the properties to be passed into the constructor of each Callback
+ * @returns {Promise<void>} a promise resolving on completion of all of the callbacks. Doesn't currently ever reject.
  */
-export default async function run<TCallbackInput>(input: TCallbackInput, callbacks: ReadonlyArray<{ new(input: TCallbackInput): Callback<TCallbackInput> }>): Promise<void> {
+export default async function run<TCallbackProps>(input: TCallbackProps, callbacks: readonly CallbackClass<TCallbackProps>[]): Promise<void> {
 	try {
 		for (const callback of callbacks) {
 			const obj = new callback(input);

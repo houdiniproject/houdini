690debc270dec259a5e350c21404fdedc75dc4f1
Eric Schultz <eric@commitchange.com>
2022-11-17 15:56:26 -0600

    Remove unused, legacy `script/test.sh`

--- script/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-npm ci --unsafe-perm && rake db:create db:structure:load db:migrate && RAILS_ENV=test rake db:create db:structure:load test:prepare && rake spec && npm run ci-build-all && npx jest

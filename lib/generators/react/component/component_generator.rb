df3c102ae639aace7965a6dafe166a463bd7bb51
Eric Schultz <eric@commitchange.com>
2022-11-18 10:06:06 -0600

    Remove legacy React generators

--- lib/generators/react/component/component_generator.rb
+++
@@ -1,9 +0,0 @@
-# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-class React::ComponentGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
-
-  def copy_file_to_component
-    template 'component.tsx.erb', File.join("javascripts/src/components", *(class_path + ["#{file_name.camelize}.tsx"]))
-    template 'component.spec.tsx.erb', File.join("javascripts/src/components", *(class_path + ["#{file_name.camelize}.spec.tsx"]))
-  end
-end
--- lib/generators/react/lib/lib_generator.rb
+++
@@ -1,7 +0,0 @@
-class React::LibGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
-  def copy_file_to_lib
-    template 'module.ts.erb', File.join("javascripts/src/lib/", *(class_path + ["#{file_name.underscore}.ts"]))
-    template 'module.spec.ts.erb', File.join("javascripts/src/lib/", *(class_path + ["#{file_name.underscore}.spec.ts"]))
-  end
-end

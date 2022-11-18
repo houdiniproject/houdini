b6852a89648298b870c7431be4828267b67ddb83
Eric Schultz <eric@commitchange.com>
2022-11-18 10:04:51 -0600

    Remove legacy job generators

--- lib/generators/generic_job/generic_job_generator.rb
+++
@@ -1,9 +0,0 @@
-# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-class GenericJobGenerator < Rails::Generators::NamedBase
-  argument :attribs, :type => :array
-  source_root File.expand_path('../templates', __FILE__)
-  def copy_file_to_lib
-    template 'generic_job_template.erb', "lib/job_types/#{name.underscore}.rb"
-    template 'generic_job_spec_template.erb', "spec/lib/job_types/#{name.underscore}_spec.rb"
-  end
-end

b9b2f1cbe3456bac3db7bc55e60e8d7f4f00de8b
Eric Schultz <eric@commitchange.com>
2022-11-18 10:05:18 -0600

    Remove legacy libmodule generators

--- lib/generators/libmodule/libmodule_generator.rb
+++
@@ -1,9 +0,0 @@
-# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-class LibmoduleGenerator < Rails::Generators::NamedBase
-  argument :mod_type, :type => :string
-  source_root File.expand_path('../templates', __FILE__)
-  def copy_file_to_lib
-    template 'libmodule_template.erb', "lib/#{mod_type.underscore}/#{mod_type.underscore}_#{name.underscore}.rb"
-    template 'libmodule_spec_template.erb', "spec/lib/#{mod_type.underscore}/#{mod_type.underscore}_#{name.underscore}_spec.rb"
-  end
-end

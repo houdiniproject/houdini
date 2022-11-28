83530cbff4c147b2759f555fc2e5ec2d76ee67a3
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2022-11-28 19:41:17 -0300

    [RESOLVE] Create a seeds task to have initial data on the db for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,43 @@
 
 # License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
 # Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
-bp = BillingPlan.new
-bp.name = 'Default billing plan'
-bp.amount = 0
-bp.percentage_fee = 0
-bp.save!
+
+ActiveRecord::Base.transaction do 
+
+  DEFAULT_PASSWORD = '123123'
+
+ 	puts("[seeds] Loading Billing Plan")
+
+	bp = BillingPlan.find_or_create_by(
+		name: 'Default billing plan',
+		amount: 0,
+		percentage_fee: 0,
+	)
+	bp.save!
+
+ 	puts("[seeds] Loading admin user")
+
+	system_admin = User.find_or_create_by(
+		name: "Admin", 
+		email: "admin@gmail.com", 
+		state_code: "AZ", 
+		city: "Phoenix", 
+		confirmed_at: Time.now.utc
+	)
+	system_admin.password = DEFAULT_PASSWORD
+	system_admin.save!
+
+ 	puts("[seeds] Loading nonprofit informations")
+	 
+	nonprofit_admin = Nonprofit.find_or_create_by(
+		 name: "Admin Nonprofit",
+		 email: "admin@gmail.com", 
+		 state_code: "AZ", 
+		 city: "Phoenix",
+		)
+	nonprofit_admin.user_id = system_admin.id
+	nonprofit_admin.save!
+	
+	puts("[seeds] initial information completed successfully")
+	
+end

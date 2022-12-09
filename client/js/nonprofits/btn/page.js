80e07f8c5bf0f9d71adbfd95bea3486271204e01
Eric Schultz <eric@commitchange.com>
2022-12-09 17:11:12 -0600

    Shrink btn down more

--- client/js/nonprofits/btn/page.js
+++ client/js/nonprofits/btn/page.js
@@ -1,15 +1,20 @@
 // License: LGPL-3.0-or-later
- var Font = require('../../common/brand-fonts'),
-     utils = require('../../common/utilities'),
-	$brandedButton = document.querySelectorAll('.branded-donate-button')
+ const Font = require('../../common/brand-fonts'),
+$brandedButton = document.querySelectorAll('.branded-donate-button');
 
-  if(utils.get_param('fixed')){
+	// from utils because utils is huge
+	function get_param(name) {
+		const param = decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [null])[1])
+		return (param == 'undefined') ? undefined : param
+	}
+
+  if(get_param('fixed')){
 	  $brandedButton.forEach((elem) => elem.classList.add('is-fixed'))
 	
 	document.querySelectorAll('.centered').forEach((elem) => elem.style.paddingTop = '5px')
   }
 
-  var $logoBlue = '#42B3DF',
+  const $logoBlue = '#42B3DF',
 	brandColor = app.nonprofit.brand_color || $logoBlue,
 	brandFont = Font[app.nonprofit.brand_font] || Font.bitter
 

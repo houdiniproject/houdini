d08b06c724fa6061d9449da5ac123084ef729324
Eric Schultz <eric@commitchange.com>
2023-01-05 17:38:18 -0600

    Add tests for `BillingPlan.create_cache_key`

--- spec/models/billing_plan_spec.rb
+++ spec/models/billing_plan_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe BillingPlan, type: :model do
     it {is_expected.to have_many(:billing_subscriptions)}
   end
   
+  describe 'Caching' do
+    describe '.create_cache_key' do
+      it 'clears the proper key when id is an integer' do
+        expect(described_class.create_cache_key(1)).to eq "billing_plan_nonprofit_id_1"
+      end
+
+      it 'clears the proper key when id is a nonprofit' do
+        np = create(:nonprofit)
+        expect(described_class.create_cache_key(np)).to eq "billing_plan_nonprofit_id_#{np.id}"
+      end
+    end
+  end
 end

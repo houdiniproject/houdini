207ace91f279fe04b455d268fcb63a2fec3d9dd0
Eric Schultz <eric@commitchange.com>
2023-01-05 17:52:22 -0600

    Add specs for `BillingSubscription.clear_cache`

--- spec/models/billing_subscription_spec.rb
+++ spec/models/billing_subscription_spec.rb
@@ -4,6 +4,19 @@ require 'rails_helper'
 RSpec.describe BillingSubscription, type: :model do
 
   describe 'Caching' do
+    describe '.clear_cache' do
+      it 'clears the cache when an id is passed' do
+        expect(Rails.cache).to receive(:delete).with("billing_subscription_nonprofit_id_1")
+        described_class.clear_cache(1)
+      end
+
+      it 'clears the cache when an nonprofit is passed' do
+        np = create(:nonprofit)
+        expect(Rails.cache).to receive(:delete).with("billing_subscription_nonprofit_id_#{np.id}")
+        described_class.clear_cache(np)
+      end
+    end
+
     describe '.create_cache_key' do
       it 'clears the proper key when id is an integer' do
         expect(described_class.create_cache_key(1)).to eq "billing_subscription_nonprofit_id_1"

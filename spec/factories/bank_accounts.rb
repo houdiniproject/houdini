d89abf85fa2b27b78c2f6cedcb86728f63ac44a1
Eric Schultz <eric@commitchange.com>
2023-02-14 17:25:59 -0600

    Use faker for generated `BankAccount` names

--- spec/factories/bank_accounts.rb
+++ spec/factories/bank_accounts.rb
@@ -1,6 +1,6 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 FactoryBot.define do
   factory :bank_account do
-    
+    bank_name { Faker::Bank.name }
   end
 end

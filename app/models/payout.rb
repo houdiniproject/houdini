269134d6dedb26e3c3ff7dfeceae3862eea2c722
Eric Schultz <eric@commitchange.com>
2023-02-22 15:58:58 -0600

    Adds warning to top of `Payout` (#589)

--- app/models/payout.rb
+++ app/models/payout.rb
@@ -4,6 +4,7 @@
 #
 # These are tied to Stripe transfers
 
+# Unless you're sure, DO NOT CREATE THESE USING STANDARD ACTIVERECORD METHODS. Use `InsertPayout.with_stripe` instead.
 class Payout < ApplicationRecord
 
 	attr_accessible \

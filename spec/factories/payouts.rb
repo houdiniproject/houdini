1937f8d04b6e51311fbd66e23000c83e31be0bb4
Eric Schultz <eric@commitchange.com>
2023-02-24 10:34:23 -0600

    Add jbuilder template for `Payout` (#582)

--- spec/factories/payouts.rb
+++ spec/factories/payouts.rb
@@ -1,6 +1,9 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 FactoryBot.define do
   factory :payout do
-    
+    nonprofit
+    net_amount { 10000 }
+    stripe_transfer_id { "tr_3532o5nfcih"}
+    email { "email@email.com"}
   end
 end
---
+++ spec/views/api_new/payouts/show.json.jbuilder_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
+require 'rails_helper'
+
+RSpec.describe '/api_new/payouts/show.json.jbuilder', type: :view do
+  let(:payout) { build(:payout) }
+  subject(:json) do
+    view.lookup_context.prefixes = view.lookup_context.prefixes.drop(1)
+    assign(:payout, payout)
+    render
+    rendered
+  end
+
+  it do
+    is_expected.to include_json(id: payout.houid, 
+        created: payout.created_at.to_i,
+        net_amount: {
+          cents: payout.net_amount_as_money.cents,
+          currency: 'usd',
+        },
+        object: 'payout'
+    )  
+  end
+end

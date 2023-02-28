af131c65acaebf0223280cc6ca87bdb5cd904fd6
Eric Schultz <eric@commitchange.com>
2023-02-27 19:22:04 -0600

    Add specs for `Users::Sessionscontroller#create`

---
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -0,0 +1,38 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+require 'rails_helper'
+
+describe Users::SessionsController, :type => :controller do
+  before(:each) do 
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+  describe '#create' do
+    describe 'basic auth' do
+      it 'accepts a correct password' do
+        user = create(:user, :confirmed)
+
+        post :create, {user: {email: user.email, password: user.password}, format: :json}
+
+        expect(response).to have_http_status(200)
+
+      end
+
+      it 'rejects an invalid password' do
+        user = create(:user, :confirmed)
+
+        post :create, {user: {email: user.email, password: "not valid"}, format: :json}
+
+        expect(response).to have_http_status(401)
+
+      end
+
+      it 'throw an error if format is not :json' do
+
+        user = create(:user, :confirmed)
+
+        expect do
+          post :create, {user: {email: user.email, password: user.password}}
+        end.to raise_error(ActionController::UnknownFormat)
+      end
+    end
+  end
+end

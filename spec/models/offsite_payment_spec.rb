c8c0d15f655054a8c9dce1ffbc085fc797e7a078
Eric Schultz <eric@commitchange.com>
2023-03-17 09:49:42 -0500

    Add initial tests to OffsitePayment

--- spec/models/offsite_payment_spec.rb
+++ spec/models/offsite_payment_spec.rb
@@ -2,5 +2,13 @@
 require 'rails_helper'
 
 RSpec.describe OffsitePayment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {is_expected.to have_db_column(:gross_amount).of_type(:integer)}
+  it {is_expected.to have_db_column(:kind).of_type(:string)}
+  it {is_expected.to have_db_column(:date).of_type(:datetime)}
+  it {is_expected.to have_db_column(:check_number).of_type(:string)}
+
+  it { is_expected.to belong_to(:payment) }
+  it { is_expected.to belong_to(:donation) }
+  it { is_expected.to belong_to(:nonprofit) }
+  it { is_expected.to belong_to(:supporter) }
 end

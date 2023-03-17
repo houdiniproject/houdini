64e835c472fa0a9de5e6dfef13384bcd11d660c2
Eric Schultz <eric@commitchange.com>
2023-03-17 09:42:02 -0500

    Ran `bin/rails g rspec:model OffsitePayment`

---
+++ spec/models/offsite_payment_spec.rb
@@ -0,0 +1,6 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+require 'rails_helper'
+
+RSpec.describe OffsitePayment, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end

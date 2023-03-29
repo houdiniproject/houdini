381ee9fecc11a79d51879e06cdfcc131be9374ef
Eric Schultz <eric@commitchange.com>
2023-03-29 17:24:54 -0500

    Hardcode `QueryBillingSubscriptions.plan_tier` to return 2 because that's we don't have plan tiers and 2 is the all in plan_tier

--- app/legacy_lib/query_billing_subscriptions.rb
+++ app/legacy_lib/query_billing_subscriptions.rb
@@ -6,8 +6,8 @@ module QueryBillingSubscriptions
 
   def self.plan_tier(np_id)
     sub = BillingSubscription::find_via_cached_np_id(np_id)
-    return 2 if sub && sub['status'] != 'inactive'
-    return 0
+    # hard coding to 2 because that's the all in plan tier and that's all we have.
+    return 2
   end
 end
 

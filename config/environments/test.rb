3ff1333425d91bf79c592f8820d59f42642e8f6e
Eric Schultz <eric@commitchange.com>
2023-04-04 11:53:52 -0500

    Use unlogged Postgres tables in testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,5 +61,8 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
+  
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.create_unlogged_tables = true
+  end
 end

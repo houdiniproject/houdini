333eeed7d244ce3618f919482bf20592c4de5c4d
Eric Schultz <eric@commitchange.com>
2023-04-05 17:29:10 -0500

    Convert dateRange to TS

---
+++ app/javascript/legacy/components/date-range.ts
@@ -0,0 +1,15 @@
+// License: LGPL-3.0-or-later
+import * as Moment from 'moment';
+import { extendMoment } from 'moment-range';
+const moment = extendMoment(Moment);
+
+// returns an array of moments
+// timeSpan is one of 'day, 'week', 'month', 'year' (see moment.js docs)
+
+export function dateRange(startDate:moment.MomentInput, 
+  endDate: moment.MomentInput, 
+  timeSpan:'day'|'week'|'month'|'year'): moment.Moment[] { 
+    return Array.from(moment.range(moment(startDate), moment(endDate)).by(timeSpan))
+}
+
+

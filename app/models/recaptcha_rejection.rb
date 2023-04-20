3f0db28c79f2a1daf66a8fc4ddd22648ff7c0480
Eric Schultz <eric@commitchange.com>
2023-04-20 16:46:35 -0500

    Convert RecaptchaRejection.details into jsonb column

--- app/models/recaptcha_rejection.rb
+++ app/models/recaptcha_rejection.rb
@@ -1,11 +1,3 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 class RecaptchaRejection < ActiveRecord::Base
-
-  def details_json
-    JSON.parse(self.details)
-  end
-
-  def details_json= json
-    self.details = JSON.generate(json)
-  end
 end

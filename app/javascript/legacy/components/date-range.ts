ea0d5b73796379d63749ee35804592c02d00d288
Eric Schultz <eric@commitchange.com>
2023-04-21 18:54:39 -0500

    Correct eslint in data-range.ts

--- app/javascript/legacy/components/date-range.ts
+++ app/javascript/legacy/components/date-range.ts
@@ -6,10 +6,10 @@ const moment = extendMoment(Moment);
 // returns an array of moments
 // timeSpan is one of 'day, 'week', 'month', 'year' (see moment.js docs)
 
-export function dateRange(startDate:moment.MomentInput, 
-  endDate: moment.MomentInput, 
-  timeSpan:'day'|'week'|'month'|'year'): moment.Moment[] { 
-    return Array.from(moment.range(moment(startDate), moment(endDate)).by(timeSpan))
+export function dateRange(startDate:moment.MomentInput,
+	endDate: moment.MomentInput,
+	timeSpan:'day'|'week'|'month'|'year'): moment.Moment[] {
+	return Array.from(moment.range(moment(startDate), moment(endDate)).by(timeSpan));
 }
 
 

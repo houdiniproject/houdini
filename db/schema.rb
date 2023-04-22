22631f9ca33e1f67418bf0c02268ed883a1bde78
Eric Schultz <eric@commitchange.com>
2023-04-21 19:13:24 -0500

    Remove unused sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_234918) do
+ActiveRecord::Schema.define(version: 2023_04_22_001251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -814,15 +814,6 @@ ActiveRecord::Schema.define(version: 2023_04_21_234918) do
     t.string "host_type", limit: 255
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "sessions", id: :serial, force: :cascade do |t|
-    t.string "session_id", limit: 255, null: false
-    t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id"
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "source_tokens", id: false, force: :cascade do |t|

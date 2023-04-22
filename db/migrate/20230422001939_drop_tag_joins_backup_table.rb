73183fe46f75b1a20aa0738b5a7a5629c36582da
Eric Schultz <eric@commitchange.com>
2023-04-21 19:31:44 -0500

    Remove unused tag_joins_backup table

---
+++ db/migrate/20230422001939_drop_tag_joins_backup_table.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
+
+class DropTagJoinsBackupTable < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :tag_joins_backup
+  end
+end

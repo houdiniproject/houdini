836f9378553dce81f548e97a25e7eb0d52b1d568
Eric Schultz <eric@commitchange.com>
2023-04-25 16:02:33 -0500

    Correct incorrect schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230421234415) do
+ActiveRecord::Schema.define(version: 20230424163001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_stat_statements"

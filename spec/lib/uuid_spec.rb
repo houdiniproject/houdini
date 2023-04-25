e2a0474892e0f5ccf51269dbd3a6ac0194780096
Eric Schultz <eric@commitchange.com>
2023-04-25 12:16:13 -0500

    Speed up uuid_spec by removing rails_helper

--- spec/lib/uuid_spec.rb
+++ spec/lib/uuid_spec.rb
@@ -1,5 +1,7 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-require 'rails_helper'
+require 'securerandom'
+require_relative '../../app/legacy_lib/uuid'
+
 describe UUID do
   describe '::Regex' do
     it 'rejects nil' do

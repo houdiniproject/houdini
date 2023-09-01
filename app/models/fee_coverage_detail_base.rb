8904b3b895c83f3189bdae5b67ce995944c78c3a
Eric Schultz <eric@commitchange.com>
2023-09-01 13:45:39 -0500

    Add missing license headers

--- app/models/fee_coverage_detail_base.rb
+++ app/models/fee_coverage_detail_base.rb
@@ -1,3 +1,4 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 # A FeeCoverageDetailBase describes the base amount for calculating fee coverage during a fee era.
 # Notably, this entity only deals with the amount added from the fee_era itself for Stripe,
 # NOT the amount added for Nonprofit's portion of the fees

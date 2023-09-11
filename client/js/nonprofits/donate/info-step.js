8978dbc6d30263ac5ed346841f099ac3e04e90b0
Eric Schultz <eric@commitchange.com>
2023-09-11 13:52:51 -0500

    Move formatFormData into its own file

--- client/js/nonprofits/donate/info-step.js
+++ client/js/nonprofits/donate/info-step.js
@@ -10,6 +10,7 @@ const serialize = require('form-serialize')
 const request = require('../../common/request')
 const format = require('../../common/format')
 const { default: customFields } = require('./components/info-step/customFields')
+const { formatFormData } = require('./components/info-step/utils');
 
 const sepaTab = 'sepa'
 const cardTab = 'credit_card'
@@ -56,10 +57,6 @@ function init(donation$, parentState) {
   return state
 }
 
-const formatFormData = form => {
-  const data = serialize(form, {hash: true})
-  return R.evolve({customFields: R.toPairs}, data)
-}
 
 const postSupporter = supporter =>
   flyd.map(

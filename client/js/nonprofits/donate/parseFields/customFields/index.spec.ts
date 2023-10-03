cb46cf53d103620563b06129b9e6e4d592f21d68
Eric Schultz <eric@commitchange.com>
2023-10-03 18:12:22 -0500

    Add tests for .parseCustomFields for JSON fields

--- client/js/nonprofits/donate/parseFields/customFields/index.spec.ts
+++ client/js/nonprofits/donate/parseFields/customFields/index.spec.ts
@@ -9,4 +9,12 @@ describe('.parseCustomFields', () => {
   it('when label provided, label is set too', () => {
     expect(parseCustomFields(" Custom Supp Level  :     Supporter Tier ")).toStrictEqual([{name: "Custom Supp Level", label: "Supporter Tier"}]);
   });
+
+  it('when passed an array looking thing, it treats a standard label', () => {
+    expect(parseCustomFields(" [Custom Supp Level]  :     Supporter Tier ")).toStrictEqual([{name: "[Custom Supp Level]", label: "Supporter Tier"}]);
+  });
+
+  it('when passed an array of json, it parses it', () => {
+    expect(parseCustomFields("[{name: 'name', label: 'labeled'}] ")).toStrictEqual([{name: "name", label: "labeled"}]);
+  });
 });

397a530e6f706721e1b697e27669eac1bb8236b3
Eric Schultz <eric@commitchange.com>
2023-10-06 20:51:44 -0500

    Cleanup client/js/cards/create.js

--- client/js/cards/create.js
+++ client/js/cards/create.js
@@ -7,8 +7,8 @@
 
 // Not namespacing card_form; only show one card form on the page at any time
 
-var request = require('../common/super-agent-promise')
-var format_err = require('../common/format_response_error')
+const request = require('../common/super-agent-promise')
+const format_err = require('../common/format_response_error')
 const grecaptcha_during_payment = require('../../../javascripts/src/lib/grecaptcha_during_payment').default
 
 module.exports = create_card

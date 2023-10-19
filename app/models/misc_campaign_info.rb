69636440a862cc604c9e907ecfea5c2dba2876e9
Eric Schultz <eric@commitchange.com>
2023-10-19 13:34:55 -0500

    Fix missing license header

--- app/models/misc_campaign_info.rb
+++ app/models/misc_campaign_info.rb
@@ -1,3 +1,4 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 class MiscCampaignInfo < ActiveRecord::Base
   belongs_to :campaign
   attr_accessible :manual_cover_fees, :paused

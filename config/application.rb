c6f5f233805d10b5490b6434f9e7e23b980e1b29
Eric Schultz <eric@commitchange.com>
2023-10-20 17:07:02 -0500

    Update config/environment.rb

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,11 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 
-require File.expand_path('../boot', __FILE__)
-
+require_relative 'boot'
 require 'rails/all'
 
-Bundler.require *Rails.groups(:assets) if defined?(Bundler)
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module Commitchange
 	class Application < Rails::Application

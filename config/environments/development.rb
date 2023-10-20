7c33a0ce17a2f4c0c756b870a98929a8dce358f3
Eric Schultz <eric@commitchange.com>
2023-10-20 17:37:44 -0500

    Update config/environment/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,40 +1,41 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-CarrierWave.configure do |config|
-  config.ignore_integrity_errors = false
-  config.ignore_processing_errors = false
-  config.ignore_download_errors = false
-end
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
 
-Commitchange::Application.configure do
-	# Settings specified here will take precedence over those in config/application.rb
-
-	# In the development environment your application's code is reloaded on
-	# every request. This slows down response time but is perfect for development
-	# since you don't have to restart the web server when you make code changes.
-	config.eager_load  = false
-	config.cache_classes = false
-  config.cache_store = :mem_cache_store, 'localhost:11211', {:expires_in => 5.hours, :compress => true, pool_size: 5 }
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+ 
+  # Do not eager load code on boot.
+  config.eager_load = false
 
 	# Log error messages when you accidentally call methods on nil.
 	config.whiny_nils = true
 
-	# Show full error reports and disable caching
-	config.consider_all_requests_local = true
-	config.action_controller.perform_caching = false
+  # Show full error reports
+  config.consider_all_requests_local = true
 
-	config.session_store :cookie_store, key: '_commitchange_session'
+  # Enable/disable caching. By default caching is disabled.
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
 
-  # You can uncomment the following to test our real AWS email server on localhost:
-	# creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_ACCESS_KEY'])
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
 
-	# Aws::Rails.add_action_mailer_delivery_method(
-	# 	:ses,
-	# 	credentials: creds,
-	# 	region: 'us-east-1'
-	# )
-	# config.action_mailer.delivery_method = :ses
+    config.cache_store = :null_store
+  end
 
-	config.action_mailer.default_url_options = { host: 'localhost', port: 5000}
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000}
 	config.action_mailer.delivery_method = Settings.mailer.delivery_method.to_sym
 	config.action_mailer.smtp_settings = { address: Settings.mailer.address, port: Settings.mailer.port }
         config.action_mailer.smtp_settings['user_name']= Settings.mailer.username if Settings.mailer.username
@@ -49,24 +50,34 @@ Commitchange::Application.configure do
 	# )
 	# config.action_mailer.delivery_method = :ses
 	config.action_mailer.default_url_options = { host: 'localhost', port: 5000}
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
 
-	# Print deprecation notices to the Rails logger
-	config.active_support.deprecation = :log
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
 
-	# Raise exception on mass assignment protection for Active Record models
+  	# Raise exception on mass assignment protection for Active Record models
 	config.active_record.mass_assignment_sanitizer = :strict
 
 	# Do not compress assets
 	config.assets.compress = false
 
-	# Expands the lines which load the assets
-	config.assets.debug = true
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 
-	config.assets.quiet = true
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
 
-	config.log_level = :debug
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 
-	config.dependency_loading = true if $rails_rake_task
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.dependency_loading = true if $rails_rake_task
 	# Turn this on if you want to mess with code inside /node_modules
 	# config.browserify_rails.evaluate_node_modules = true
 

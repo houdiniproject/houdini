69ce63708d5c2edd39893064bc8c44fb56c924b6
Eric Schultz <eric@commitchange.com>
2023-10-20 17:40:44 -0500

    Move some session_store config into config/initializers/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 12.hours
+if Rails.env.development?
+  Rails.application.config.session_store :cookie_store, key: '_commitchange_session'
+else
+  Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 12.hours
+end

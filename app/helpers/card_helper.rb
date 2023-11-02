cdec0e4ee8485881efe5e96b99f9a40fcc8477c3
Eric Schultz <eric@commitchange.com>
2023-11-02 18:30:24 -0500

    Remove unused code from CardHelper

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -1,22 +1,6 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 module CardHelper
 
-	def brand_file(brand)
-		if brand == 'Visa' || brand == 'visa' || brand == 'VISA'
-			'visa'
-		elsif brand == 'American Express' || brand == 'amex'
-			'amex'
-		elsif brand == 'Discover' || brand == 'Discover Card' || brand == 'discover'
-			'discover'
-		elsif brand == 'MasterCard' || brand == 'Mastercard' || brand == 'mastercard'
-			'mastercard'
-		end
-	end
-
-	def current_card
-		current_user && current_user.profile.card
-	end
-
 	def expiration_years
 		(0..15).map{|n| (Date.today + n.years).year}
 	end

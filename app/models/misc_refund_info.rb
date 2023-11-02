db7675dbaa3ad6f403a96c2df92d0ced89fe5087
Eric Schultz <eric@commitchange.com>
2023-11-02 16:04:07 -0500

    Remove attr_accessible from MiscRefundInfo

--- app/models/misc_refund_info.rb
+++ app/models/misc_refund_info.rb
@@ -1,7 +1,4 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 class MiscRefundInfo < ApplicationRecord
-  attr_accessible :is_modern,
-    :stripe_application_fee_refund_id
-
   belongs_to :refund
 end

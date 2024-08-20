75840e0e021dc5b49a21e9aac0ac693dbf8baf29
Eric Schultz <eric@commitchange.com>
2024-08-20 16:34:22 -0500

    Correct heroku download command

--- script/restore_from_heroku.sh
+++ script/restore_from_heroku.sh
@@ -2,5 +2,5 @@
 set -e
 
 
-curl -o latest.dump `heroku pg:backups public-url -a commitchange`
+curl -o latest.dump `heroku pg:backups:url -a commitchange`
 script/pg_restore_local_from_production.sh

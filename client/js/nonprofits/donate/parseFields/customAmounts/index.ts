d93ce162d5431b4624ee73f46052fe5619c9e2af
Ben Hejkal <ben@softwareforgood.com>
2024-10-30 09:39:17 -0500

    refactor to separate parseNumberAmounts

--- client/js/nonprofits/donate/parseFields/customAmounts/index.ts
+++ client/js/nonprofits/donate/parseFields/customAmounts/index.ts
@@ -2,17 +2,16 @@
 import JsonStringParser from './JsonStringParser';
 import { getDefaultAmounts } from '../../custom_amounts';
 import { CustomAmount } from '../customAmount';
-import { splitParam } from '..';
+import parseNumberAmounts from './parseNumberAmounts';
 
 export default function parseCustomAmounts(amountsString: string): CustomAmount[] {
   const defaultAmts = getDefaultAmounts().join();
 
   if (amountsString.includes('{')) {
-    return new JsonStringParser(`[${amountsString}]`).results;
+    if (!amountsString.startsWith('[')) amountsString = `[${amountsString}`;
+    if (!amountsString.endsWith(']')) amountsString = `${amountsString}]`;
+    return new JsonStringParser(amountsString).results;
   } else {
-    const commaParams = splitParam(amountsString || defaultAmts)
-      .map(Number)
-      .join(',');
-    return new JsonStringParser(`[${commaParams}]`).results;
+    return parseNumberAmounts(amountsString || defaultAmts);
   }
 }

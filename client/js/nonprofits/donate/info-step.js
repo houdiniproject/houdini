969fd4451cee4220bddf619b66bf44a7e7568a3a
Ben Hejkal <ben@softwareforgood.com>
2024-11-12 11:59:43 -0500

    dedication link: more styling in css, fix wrapping

--- client/js/nonprofits/donate/info-step.js
+++ client/js/nonprofits/donate/info-step.js
@@ -168,9 +168,11 @@ const dedicationLink = state => {
   if(state.params$().hide_dedication) return ''
   return h('label.u-centered.u-marginTop--10', [
     h('small', [
-      h('a', {
+      h('a.dedication-link', {
         on: { click: [state.showDedicationForm$, true] },
-        style: {'border-width': 'thin', 'border-style': 'solid', padding: '2px', 'border-color': `${app.nonprofit.brand_color || '#4DAE7F'}`, 'border-radius': '2px', 'box-shadow': `3px 3px ${app.nonprofit.brand_color || '#4DAE7F'}7F`}
+        style: {
+          'border-color': `${app.nonprofit.brand_color || '#4DAE7F'}`
+        , 'box-shadow': `3px 3px ${app.nonprofit.brand_color || '#4DAE7F'}7F`}
       }, state.dedicationData$() && state.dedicationData$().first_name
       ? [h('i.fa.fa-check'), I18n.t('nonprofits.donate.info.dedication_saved') + `${state.dedicationData$().first_name || ''} ${state.dedicationData$().last_name || ''}`]
       : [I18n.t('nonprofits.donate.info.dedication_link')]

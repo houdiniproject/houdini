d27bcc94d5b367de70c59beb61dfb9240360cc1e
Thena Seer <138702673+thena-seer-sfg@users.noreply.github.com>
2024-12-11 10:53:44 -0800

    Update script/pg_restore_local_from_production.sh
    Co-authored-by: Eric Schultz <eric@commitchange.com>

--- script/pg_restore_local_from_production.sh
+++ script/pg_restore_local_from_production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-pg_restore --verbose --clean --no-acl --no-owner -h ${DATABASE_HOST:-localhost} -U admin -d commitchange_development_legacy ${HOUDINI_DUMP_PATH:-"latest.dump"}
+pg_restore --verbose --clean --no-acl --no-owner -h ${DATABASE_HOST:-localhost} -U admin -d commitchange_development_legacy ${CC_PROD_DUMP_PATH:-"latest.dump"}

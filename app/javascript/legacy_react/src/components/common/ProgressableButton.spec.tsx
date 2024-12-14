b442e7f838e9c769a2b91809189f5348f08674cf
Eric Schultz <eric@commitchange.com>
2024-12-13 22:18:03 -0600

    Update ProgressableButton test

--- app/javascript/legacy_react/src/components/common/ProgressableButton.spec.tsx
+++ app/javascript/legacy_react/src/components/common/ProgressableButton.spec.tsx
@@ -3,17 +3,27 @@ import * as React from 'react';
 import ProgressableButton from './ProgressableButton'
 import toJson from 'enzyme-to-json';
 import {mount, shallow} from 'enzyme';
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('ProgressableButton', () => {
-  test('Basic title button works', () => {
-    let output = shallow(
-      <ProgressableButton onClick={() => console.log('alert!')} buttonText={"nothing"} data-label="button"/>)
-    expect(toJson(output)).toMatchSnapshot()
+  test('Basic title button works', async () => {
+    const clicked = jest.fn()
+    let output = render(
+      <ProgressableButton onClick={clicked} buttonText={"nothing"} data-label="button"/>)
+
+    userEvent.click(screen.getByText("nothing"))
+    expect(clicked).toBeCalled();
+
+    expect(output.baseElement.outerHTML).toMatchSnapshot()
+
+
   })
 
   test('Progress means we change the title, dont disable and do turn on spinner', () => {
-    let output = mount(
-      <ProgressableButton onClick={() => console.log('alert!')}
+    const clicked = jest.fn()
+    let output = render(
+      <ProgressableButton onClick={clicked}
                           buttonText={"nothing"}
                           data-label="button"
                           buttonTextOnProgress={"onProgress"}
@@ -21,7 +31,9 @@ describe('ProgressableButton', () => {
                           disableOnProgress={false}
 
                           />)
-    expect(toJson(output)).toMatchSnapshot()
+    userEvent.click(screen.getByText("onProgress"))
+    expect(clicked).toBeCalled();
+    expect(output.baseElement.outerHTML).toMatchSnapshot()
   })
 
   test('Title is kept on progress if no titleOnProgress is set', () => {

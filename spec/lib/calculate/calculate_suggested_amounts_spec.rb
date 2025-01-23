00d8dcc6ee2dc89dd853867ce8e43b9fddf8d97b
Eric Schultz <eric@commitchange.com>
2025-01-23 17:36:07 -0600

    Add missing rails_helper require in the CalculateSuggestAmounts spec

--- spec/lib/calculate/calculate_suggested_amounts_spec.rb
+++ spec/lib/calculate/calculate_suggested_amounts_spec.rb
@@ -1,4 +1,5 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+require 'rails_helper'
 describe CalculateSuggestedAmounts do
   describe '.calculate' do
     it 'param validation' do

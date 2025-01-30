19e837d940a85dea0f586bd6008a7fe15b621085
Eric Schultz <eric@commitchange.com>
2025-01-30 16:10:44 -0600

    Fix events controller test quirk

--- spec/controllers/events_spec.rb
+++ spec/controllers/events_spec.rb
@@ -9,7 +9,7 @@ describe EventsController, :type => :controller do
       include_context :open_to_event_editor, :post, :create, nonprofit_id: :__our_np, id: :__our_event
     end
     describe 'update' do
-      include_context :open_to_event_editor, :put, :update, nonprofit_id: :__our_np, id: :__our_event, with_status: 200
+      include_context :open_to_event_editor, :put, :update, nonprofit_id: :__our_np, id: :__our_event
     end
     describe 'duplicate' do
       include_context :open_to_event_editor, :post, :duplicate, nonprofit_id: :__our_np, id: :__our_event

7016e1412ffeb85bd818784489e5b2e4f61f7343
Eric Schultz <eric@commitchange.com>
2025-02-18 10:44:45 -0600

    Move session_store info into initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,10 @@
 
 if Rails.env.development?
   Rails.application.config.session_store :cookie_store, key: '_commitchange_session'
+elsif Rails.env.staging? || Rails.env.production?
+  Rails.application.config.session_store :redis_store, servers: [ENV['OPENREDIS_URL']], 
+    expire_after: 12.hours,
+    namespace: "_#{Rails.application.class.parent_name.downcase}_session"
 else
   Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 12.hours
 end

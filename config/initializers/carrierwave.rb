c07a4aa4b61aab90a4e3927fd83574158907be27
Eric Schultz <eric@commitchange.com>
2025-02-19 14:55:01 -0600

    Switch tabs to spaces in carrierwave init

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,22 +5,22 @@ if Rails.env.test?
     config.enable_processing = false
   end
 elsif Rails.env.development?
-	CarrierWave.configure do |config|
-		config.ignore_integrity_errors = false
-		config.ignore_processing_errors = false
-		config.ignore_download_errors = false
-	end
+  CarrierWave.configure do |config|
+    config.ignore_integrity_errors = false
+    config.ignore_processing_errors = false
+    config.ignore_download_errors = false
+  end
 else
-	CarrierWave.configure do |config|
-		config.storage    = :aws
-		config.aws_bucket = Settings.aws.bucket
-		config.aws_acl    = :public_read
-		config.asset_host = Settings.image&.host || "https://#{Settings.aws.bucket}.s3.amazonaws.com"
-		config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
-		config.aws_credentials = {
-			access_key_id:     Settings.aws.access_key_id,
-			secret_access_key: Settings.aws.secret_access_key,
-			region: Settings.aws.region
-		}
-	end
+  CarrierWave.configure do |config|
+    config.storage    = :aws
+    config.aws_bucket = Settings.aws.bucket
+    config.aws_acl    = :public_read
+    config.asset_host = Settings.image&.host || "https://#{Settings.aws.bucket}.s3.amazonaws.com"
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+    config.aws_credentials = {
+      access_key_id:     Settings.aws.access_key_id,
+      secret_access_key: Settings.aws.secret_access_key,
+      region: Settings.aws.region
+    }
+  end
 end

dbbc024082db9b6cf1b6476203977e7edbb7e905
Eric Schultz <eric@commitchange.com>
2025-02-24 12:56:10 -0600

    Cleanup a few importers

--- app/legacy_lib/import_onecause_event_donations.rb
+++ app/legacy_lib/import_onecause_event_donations.rb
@@ -61,7 +61,7 @@ module ImportOnecauseEventDonations
             'supporter_id': supporter.id,
             'event_id': event.id,
             'offsite_payment': {}
-          }.to_unsafe_h.with_indifferent_access)
+          }.with_indifferent_access)
         end
 
 

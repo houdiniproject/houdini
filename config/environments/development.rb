2477a40f27bdea034976e6a85a69b5cd59bdcff5
Eric Schultz <eric@commitchange.com>
2025-03-07 19:49:41 -0600

    Update development for Rails 6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -31,8 +32,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-   # Store uploaded files on the local file system (see config/storage.yml for options)
-   config.active_storage.service = :local
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
 
 	config.action_mailer.delivery_method = :letter_opener
 	config.action_mailer.perform_deliveries = true
@@ -80,7 +81,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,

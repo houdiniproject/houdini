d5016fabd464380b8c4ce135636a311bdaae6807
Eric Schultz <eric@commitchange.com>
2025-03-11 18:17:29 -0500

    Add zeitwerk autoloader

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Commitchange
 	class Application < Rails::Application
 		# Initialize configuration defaults for originally generated Rails version.
 		config.load_defaults 5.2
+		config.autoloader = :zeitwerk
 
 		# Settings in config/environments/* take precedence over those specified here.
 		# Application configuration should go into files in config/initializers

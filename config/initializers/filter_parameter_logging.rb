63396bd77d2a3964dfe9713a1e4c45af17060ced
Eric Schultz <eric@commitchange.com>
2025-03-12 16:52:12 -0500

    Update filter parameter logging and add license

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,8 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  :password,
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+]

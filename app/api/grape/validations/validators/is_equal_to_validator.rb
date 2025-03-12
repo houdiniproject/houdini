ce1ce0665c8bb53dedb3674f01a6ee973fa70410
Eric Schultz <eric@commitchange.com>
2025-03-12 11:02:50 -0500

    Move the IsEqualToValidator to the place Grape expects it.

--- app/api/grape/validations/validators/is_equal_to_validator.rb
+++ app/api/grape/validations/validators/is_equal_to_validator.rb
@@ -1,5 +1,5 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-class Houdini::V1::Validators::IsEqualTo < Grape::Validations::Validators::Base
+class Grape::Validations::Validators::IsEqualToValidator < Grape::Validations::Validators::Base
   def validate_param!(attr_name, params)
     if params[attr_name] != params[@option]
       fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name), @scope.full_name(@option)], message: message(:is_equal_to)

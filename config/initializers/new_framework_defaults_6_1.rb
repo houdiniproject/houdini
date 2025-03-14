9e87954b3442858dd0944a3a9ebe8c3ce5b10098
Eric Schultz <eric@commitchange.com>
2025-03-14 18:34:15 -0500

    We don't use ActionMailbox so we can safely turn on their defaults

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -54,10 +54,10 @@ Rails.application.config.active_storage.queues.analysis = nil
 Rails.application.config.active_storage.queues.purge = nil
 
 # Set the default queue name for the incineration job to the queue adapter default.
-# Rails.application.config.action_mailbox.queues.incineration = nil
+Rails.application.config.action_mailbox.queues.incineration = nil
 
 # Set the default queue name for the routing job to the queue adapter default.
-# Rails.application.config.action_mailbox.queues.routing = nil
+Rails.application.config.action_mailbox.queues.routing = nil
 
 # Set the default queue name for the mail deliver job to the queue adapter default.
 # Rails.application.config.action_mailer.deliver_later_queue_name = nil

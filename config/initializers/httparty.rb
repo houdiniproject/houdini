ded65cf62391426d4bb962217e9e6ab1b81d4db4
Eric Schultz <eric@commitchange.com>
2025-03-28 19:38:59 -0500

    Get httparty working properly in initializers

--- config/initializers/httparty.rb
+++ config/initializers/httparty.rb
@@ -1,4 +1,8 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 require 'httparty'
-::HTTParty::Logger.add_formatter(:mailchimp, ::Httparty::Logger::MailchimpLogger)
-::HTTParty::Logger.add_formatter(:full_contact, ::Httparty::Logger::FullContactLogger)
+
+# runs once on boot but won't reload the classes when they change
+Rails.application.config.after_initialize do
+  ::HTTParty::Logger.add_formatter(:mailchimp, ::Httparty::Logger::MailchimpLogger)
+  ::HTTParty::Logger.add_formatter(:full_contact, ::Httparty::Logger::FullContactLogger)
+end

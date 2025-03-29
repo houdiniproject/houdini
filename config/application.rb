3414c97cfae2b15aa85361d1084a253010a95746
Eric Schultz <eric@commitchange.com>
2025-03-29 12:12:56 -0500

    Set active_support.cache_format_version = 7.0

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Commitchange
 		# Initialize configuration defaults for originally generated Rails version.
 		config.load_defaults 6.1
 
+		config.active_support.cache_format_version = 7.0 # needed until line above is 7.0
+
 		# Settings in config/environments/* take precedence over those specified here.
 		# Application configuration should go into files in config/initializers
 		# -- all .rb files in that directory are automatically loaded.

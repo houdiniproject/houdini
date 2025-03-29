416e4fd064204d075348a45329db08492126d624
Eric Schultz <eric@commitchange.com>
2025-03-28 19:09:52 -0500

    Update schema for Rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_09_011057) do
+ActiveRecord::Schema[6.1].define(version: 2024_02_09_011057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"

d0a1c94dec060fbea4c7739dc6c0979b537b518c
Eric Schultz <eric@commitchange.com>
2025-03-29 12:15:28 -0500

    Set active_support.disable_to_s_conversion = true

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Commitchange
 		config.load_defaults 6.1
 
 		config.active_support.cache_format_version = 7.0 # needed until line above is 7.0
+		config.active_support.disable_to_s_conversion = true # needed until line above is 7.0
 
 		# Settings in config/environments/* take precedence over those specified here.
 		# Application configuration should go into files in config/initializers

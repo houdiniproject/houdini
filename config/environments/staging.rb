6db82678a21fd577eb0ecf8f3ba0941d3bd41964
Eric Schultz <eric@commitchange.com>
2025-04-04 13:46:56 -0500

    Undo change to staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,9 +4,8 @@ require_relative './production'
 Rails.application.configure do
 	# Settings specified here will take precedence over those in config/application.rb and config/environments/production.rb
 
-	config.action_mailer.default_url_options = { host: 'commitchange-test.herokuapp.com' }
 	config.action_mailer.delivery_method = Settings.mailer.delivery_method.to_sym
-
+	config.action_mailer.default_url_options = { host: 'commitchange-test.herokuapp.com' }
 	# we want to be able to show mailer previews
 	config.action_mailer.show_previews = true
 end

338a42e9667307e01d55c107b6bc4f600fc173a1
Eric Schultz <eric@commitchange.com>
2025-04-04 13:47:45 -0500

    Correct change to test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'localhost:8080' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr

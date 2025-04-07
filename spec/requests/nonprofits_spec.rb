6474de1c7243fc86720f717c281ce6284c545716
Eric Schultz <eric@commitchange.com>
2025-04-07 15:20:53 -0500

    A little cleanup of the document

--- spec/requests/nonprofits_spec.rb
+++ spec/requests/nonprofits_spec.rb
@@ -1,8 +1,6 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 require 'rails_helper'
-# require 'support/contexts/shared_donation_charge_context'
 describe NonprofitsController, type: :request do
-  
   let(:nonprofit) { create(:nonprofit_base, :with_default_billing_subscription)}
   let(:user) { create(:user_as_nonprofit_associate, nonprofit: nonprofit)  }
 

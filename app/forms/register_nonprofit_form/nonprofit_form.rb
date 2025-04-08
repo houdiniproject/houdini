250d42863534116ff4efbf3123e61ed0424d731c
Casey Helbling <casey@softwareforgood.com>
2025-04-07 21:33:45 -0500

    I ran standrdrb on the new form classes and specs in an attempt to preemptively standardize them. As it was, the extra whitespace and linting things were distracting and making it hard to review the substance of the PR.

--- app/forms/register_nonprofit_form/nonprofit_form.rb
+++ app/forms/register_nonprofit_form/nonprofit_form.rb
@@ -1,22 +1,19 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+
 class RegisterNonprofitForm::NonprofitForm < ApplicationForm
   attribute :name, :string, default: "" # needed because OnboardAccounts is goofy
 
-  attr_accessor :city, 
-    :email,
-    :state_code,
-    :phone,
-    :website,
-    :zip_code
+  attr_reader :website
+  attr_accessor :city, :email, :state_code, :phone, :zip_code
 
   validates :zip_code, presence: true
-  validates :website, format: { with: URI.regexp }, if: Proc.new { |form| form.website.present? }
+  validates :website, format: {with: URI::RFC2396_PARSER.make_regexp}, if: proc { |form| form.website.present? }
   # validates :email, format: {with:  URI::MailTo::EMAIL_REGEXP }, if: Proc.new { |form| form.email.present? }
 
-  def initialize(attributes={})
-    super(attributes)
+  def initialize(attributes = {})
+    super
     @models = [
-      nonprofit,
+      nonprofit
     ]
   end
 
@@ -24,31 +21,31 @@ class RegisterNonprofitForm::NonprofitForm < ApplicationForm
     @nonprofit ||= Nonprofit.new(OnboardAccounts.set_nonprofit_defaults(
       city: city,
       email: email,
-      name: name, 
+      name: name,
       phone: phone,
-      state_code: state_code, 
-      website: website, 
-      zip_code: zip_code,
+      state_code: state_code,
+      website: website,
+      zip_code: zip_code
     ))
   end
 
   def website=(url)
     nudged_url = url
-    unless (url =~ /\Ahttp:\/\/.*/i || url =~ /\Ahttps:\/\/.*/i)
-      nudged_url = 'http://'+ url
+    unless url =~ /\Ahttp:\/\/.*/i || url =~ /\Ahttps:\/\/.*/i
+      nudged_url = "http://" + url
     end
     @website = nudged_url
   end
 
   def valid?(context = nil)
-    result = super(context)
+    result = super
     unless result
       if nonprofit.errors.of_kind?(:slug, :taken)
         begin
           slug = ::SlugNonprofitNamingAlgorithm.new(nonprofit.state_code_slug, nonprofit.city_slug).create_copy_name(nonprofit.slug)
           nonprofit.slug = slug
           errors.delete(:slug, :taken)
-          result = super(context) # try again!
+          result = super # try again!
         rescue UnableToCreateNameCopyError
           errors.delete(:slug, :taken)
           errors.add(:name, :invalid, message: "has an invalid slug. Contact support for help.")

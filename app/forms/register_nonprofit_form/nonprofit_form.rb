ed6bd80e0ed336e46c64645dd0309fb87f78e0c9
Casey Helbling <casey@softwareforgood.com>
2025-04-08 19:30:49 -0500

    Revert rubcop recommendation

--- app/forms/register_nonprofit_form/nonprofit_form.rb
+++ app/forms/register_nonprofit_form/nonprofit_form.rb
@@ -11,7 +11,7 @@ class RegisterNonprofitForm::NonprofitForm < ApplicationForm
     :zip_code
 
   validates :zip_code, presence: true
-  validates :website, format: {with: URI::RFC2396_Parser.make_regexp}, if: proc { |form| form.website.present? }
+  validates :website, format: {with: URI.regexp}, if: proc { |form| form.website.present? }
   # validates :email, format: {with:  URI::MailTo::EMAIL_REGEXP }, if: Proc.new { |form| form.email.present? }
 
   def initialize(attributes = {})

aa53b51c5dd54dbc050160b4bb4b6b5316d7085c
Eric Schultz <eric@commitchange.com>
2025-04-10 13:06:03 -0500

    Update assets.rb for Rails 7.1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,8 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path

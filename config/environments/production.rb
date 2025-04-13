e513679d63abaf8201bae403ca02915f8e4f1d9d
Eric Schultz <eric@commitchange.com>
2025-04-12 19:55:06 -0500

    Update uglifier to latest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.static_cache_control = "public, max-age=#{10.minutes.seconds.to_i}"
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.

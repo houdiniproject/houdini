9e7860d1a5e016c67972ef2c26d98b336d945715
Eric Schultz <eric@commitchange.com>
2025-04-15 10:51:37 -0500

    Cleanup events factory spec

--- spec/factory_specs/events_spec.rb
+++ spec/factory_specs/events_spec.rb
@@ -1,17 +1,16 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 require "rails_helper"
-require 'webmock/rspec'
+require "webmock/rspec"
 
-describe 'events factory' do
+describe "events factory" do
+  let!(:stub_request_to_google_maps) { stub_request(:get, %r{https://maps.googleapis.com/maps/api/geocode/json.*}) }
 
-  let!(:stub_request_to_google_maps) { stub_request(:get, %r{https://maps.googleapis.com/maps/api/geocode/json.*})}
-  
-  it 'defaults to return nil for geocode' do
+  it "defaults to return nil for geocode" do
     create(:event_base)
     expect(stub_request_to_google_maps).to have_not_been_made
   end
 
-  it 'defaults to return nil for geocode' do
+  it "contacts Google Maps api when perform_geocode is set" do
     create(:event_base, perform_geocode: true)
 
     expect(stub_request_to_google_maps).to have_been_made

c42cd2062ab447a375dece57e61f214124fc2ad7
Eric Schultz <eric@commitchange.com>
2025-04-16 13:46:51 -0500

    Move redundant `:password` from filter_parameters

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,6 +3,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
-  :password,
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
 ]

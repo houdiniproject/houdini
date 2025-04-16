d0daed2e6690d46737ddd10592c9199ae129323b
Eric Schultz <eric@commitchange.com>
2025-04-16 14:16:19 -0500

    Update docs on monkeypatch

--- config/initializers/1_strict_loading_monkeypatch.rb
+++ config/initializers/1_strict_loading_monkeypatch.rb
@@ -1,5 +1,7 @@
 # License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
 # from https://github.com/rails/rails/issues/48617#issuecomment-2205940350
+
+# This should be removed approximately a week after Rails 7 is deployed to prod
 module ActiveRecordBaseMonkeyPatch
   def strict_loading_mode
     @strict_loading_mode || :all

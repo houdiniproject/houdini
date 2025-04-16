c45aa1b46579362e336fd4d4fb96a34401ce9e9f
Eric Schultz <eric@commitchange.com>
2025-04-16 13:59:29 -0500

    Add temp strict loading monkey patch

---
+++ config/initializers/1_strict_loading_monkeypatch.rb
@@ -0,0 +1,9 @@
+# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
+# from https://github.com/rails/rails/issues/48617#issuecomment-2205940350
+module ActiveRecordBaseMonkeyPatch
+  def strict_loading_mode
+    @strict_loading_mode || :all
+  end
+end
+
+ActiveSupport.on_load(:active_record) { prepend ActiveRecordBaseMonkeyPatch }

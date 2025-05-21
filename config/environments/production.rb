cda73a5d8e4b27868e59125bf67750b597470492
Eric Schultz <eric@commitchange.com>
2025-05-21 11:14:29 -0500

    Readd deleted AWS credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,8 @@ Rails.application.configure do
   config.asset_host = cdn_url
 
   config.action_mailer.perform_caching = false
-
+  
+  creds = Aws::Credentials.new(ENV["AWS_ACCESS_KEY"], ENV["AWS_SECRET_ACCESS_KEY"])
   Aws::Rails.add_action_mailer_delivery_method(
     :ses,
     credentials: creds,

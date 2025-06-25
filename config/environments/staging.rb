2e7389155d200d5327009fb770b602a5ac1ff9d8
Eric Schultz <eric@commitchange.com>
2025-06-25 15:49:13 -0500

    Configure Mailtrap for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb and config/environments/production.rb
 
   config.action_mailer.delivery_method = Settings.mailer.delivery_method.to_sym
+  config.action_mailer.smtp_settings = Settings.mailer.smtp_settings.to_h
   config.action_mailer.default_url_options = {host: "commitchange-test.herokuapp.com"}
   # we want to be able to show mailer previews
   config.action_mailer.show_previews = true

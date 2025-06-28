e00500ad400b3ce13f06b72a794a34e4616e3615
Eric Schultz <eric@commitchange.com>
2025-06-27 19:17:40 -0500

    Remove unused `Hash.with_defaults_unless_nil` method

--- app/legacy_lib/hash.rb
+++
@@ -1,7 +0,0 @@
-# License: AGPL-3.0-or-later WITH Web-Template-Output-Additional-Permission-3.0-or-later
-class Hash
-  # Prefills a new Hash from the values in other_hash. If a value in other_hash is nil, then it isn't copied over.
-  def self.with_defaults_unless_nil(other_hash)
-    try_convert(other_hash.compact)
-  end
-end

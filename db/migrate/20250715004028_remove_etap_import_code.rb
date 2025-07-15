5622d3ba6d3bb84096409cb0d5f84bceebb8e2d6
Eric Schultz <eric@commitchange.com>
2025-07-15 10:01:24 -0500

    Silence Rails/ReversibleMigration warnings

--- db/migrate/20250715004028_remove_etap_import_code.rb
+++ db/migrate/20250715004028_remove_etap_import_code.rb
@@ -1,10 +1,12 @@
 class RemoveEtapImportCode < ActiveRecord::Migration[7.1]
   def change
+    # standard:disable Rails/ReversibleMigration
     drop_table :e_tap_imports
     drop_table :e_tap_import_contacts
     drop_table :e_tap_import_journal_entries
     drop_table :journal_entries_to_items
     drop_table :payment_dupe_statuses
     drop_table :reassignments
+    # standard:enable Rails/ReversibleMigration
   end
 end

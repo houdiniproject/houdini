cd28eea9d72a8147483214431adae14f5f2e68b3
Eric Schultz <eric@commitchange.com>
2025-08-27 19:41:29 -0500

    Rollback Maintenance task for schema

--- db/schema.rb
+++ db/schema.rb
@@ -560,27 +560,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_26_154514) do
     t.string "item_type"
   end
 
-  create_table "maintenance_tasks_runs", force: :cascade do |t|
-    t.string "task_name", null: false
-    t.datetime "started_at", precision: nil
-    t.datetime "ended_at", precision: nil
-    t.float "time_running", default: 0.0, null: false
-    t.bigint "tick_count", default: 0, null: false
-    t.bigint "tick_total"
-    t.string "job_id"
-    t.string "cursor"
-    t.string "status", default: "enqueued", null: false
-    t.string "error_class"
-    t.string "error_message"
-    t.text "backtrace"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "arguments"
-    t.integer "lock_version", default: 0, null: false
-    t.text "metadata"
-    t.index ["task_name", "status", "created_at"], name: "index_maintenance_tasks_runs", order: { created_at: :desc }
-  end
-
   create_table "manual_balance_adjustments", id: :serial, force: :cascade do |t|
     t.integer "gross_amount", default: 0
     t.integer "fee_total", default: 0
